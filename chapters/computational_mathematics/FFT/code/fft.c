17483518553904f624a8f0a923ab7cc1961a5b25
Gathros <ultim8killers@gmail.com>
2017-12-30 11:20:52 +0000

    adding comments to fft.c and fft.cpp

--- chapters/computational_mathematics/FFT/code/fft.c
+++ chapters/computational_mathematics/FFT/code/fft.c
@@ -1,13 +1,18 @@
+// written by Gathros.
+
 #include <complex.h>
 #include <math.h>
-#include <stdio.h>
+
+// These headers are for presentation not for the algorithm.
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 #define PI 3.1415926535897932384626
 
 void cooley_tukey(double complex *X, const size_t N){
 	if(N >= 2){
+		// Splits the array, so the top half are the odd elements and the bottom half are the even ones.
 		double complex tmp [N/2];
 		for(size_t i = 0; i < N/2; ++i){
 			tmp[i] = X[2*i + 1];
@@ -17,9 +22,11 @@ void cooley_tukey(double complex *X, const size_t N){
 			X[i + N/2] = tmp[i];
 		}
 
+		// Recursion.
 		cooley_tukey(X, N/2);
 		cooley_tukey(X + N/2, N/2);
 
+		// Combine.
 		for(size_t i = 0; i < N/2; ++i){
 			X[i + N/2] = X[i] - cexp(-2.0*I*PI*i/N)*X[i + N/2];
 			X[i] -= (X[i + N/2]-X[i]);
@@ -28,12 +35,12 @@ void cooley_tukey(double complex *X, const size_t N){
 }
 
 void bit_reverse(double complex *X, size_t N){
-        double complex temp;
+        // Bit reverses the array X[] but only if the size of the array is less then 2^32.
+		double complex temp;
         unsigned int b;
 
         for(unsigned int i = 0; i < N; ++i){
                 b = i;
-                // Reverse bits
                 b = (((b & 0xaaaaaaaa) >> 1) | ((b & 0x55555555) << 1));
                 b = (((b & 0xcccccccc) >> 2) | ((b & 0x33333333) << 2));
                 b = (((b & 0xf0f0f0f0) >> 4) | ((b & 0x0f0f0f0f) << 4));
@@ -51,8 +58,10 @@ void iterative_cooley_tukey(double complex *X, size_t N){
 	int stride;
 	double complex v,w;
 
+	// Bit reverse the array.
 	bit_reverse(X, N);
 
+	// Preform the butterfly on the array.
 	for(int i = 1; i <= log2((double)N); ++i){
 		stride = pow(2, i);
 		w = cexp(-2.0*I*PI/stride);
@@ -68,12 +77,14 @@ void iterative_cooley_tukey(double complex *X, size_t N){
 }
 
 void approx(double complex *X, double complex *Y, size_t N){
+	// This is to show that the arrays are approximate.
 	for(size_t i = 0; i < N; ++i){
 		printf("%f\n", cabs(X[i]) - cabs(Y[i]));
 	}
 }
 
 int main(){
+	// Initalizing the arrays for FFT.
 	srand(time(NULL));
 	const size_t N = 64;
 	double complex x[N], y[N], z[N];
@@ -83,9 +94,11 @@ int main(){
 		z[i] = x[i];
 	}
 
+	// Preform FFT.
 	cooley_tukey(y, N);
 	iterative_cooley_tukey(z, N);
 
+	// Check if the different methods are approximate.
 	approx(y, z, N);
 
 	return 0;
--- chapters/computational_mathematics/FFT/code/fft.cpp
+++ chapters/computational_mathematics/FFT/code/fft.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
-#include <iomanip>
+// written by Gathros, modernized by Nicole Mazzuca.
+
 #include <complex>
 #include <vector>
-#include <random>
 #include <array>
 #include <cstdint>
 
-/* TODO: add some comments here before actually merging it */
-/* TODO: in general, more commenting is good */
+// These headers are for presentation not for the algorithm.
+#include <random>
+#include <iostream>
+#include <iomanip>
+
+// Create Range and span to simplify the code for splitting and creating arrays.
 struct range {
   std::size_t begin;
   std::size_t end;
@@ -41,6 +44,7 @@ constexpr T pi = 3.14159265358979323846264338327950288419716;
 void cooley_tukey(span<c64> original, std::vector<c64>&& temp = {}) {
   auto size = original.size();
   if (size >= 2) {
+    // Splits the array, so the top half are the odd elements and the bottom are the even ones.
     temp.reserve(size / 2);
     for (std::size_t i = 0; i < size / 2; ++i) {
       temp[i] = original[i * 2 + 1];
@@ -50,9 +54,11 @@ void cooley_tukey(span<c64> original, std::vector<c64>&& temp = {}) {
       original[i + size / 2] = temp[i];
     }
 
+    // Recursion.
     cooley_tukey(original[range{0, size / 2}], std::move(temp));
     cooley_tukey(original[range{size / 2, size}], std::move(temp));
 
+    // Combine.
     for (std::size_t k = 0; k < size / 2; ++k) {
       auto w = std::exp(c64(0, -2.0 * pi<double> * k / size));
       original[k + size / 2] = original[k] - w * original[k + size / 2];
@@ -62,11 +68,11 @@ void cooley_tukey(span<c64> original, std::vector<c64>&& temp = {}) {
 }
 
 void bit_reverse(span<c64> original) {
+  // Bit reverses the array X[] but only if the size of the array is less then 2^32.
   auto size = original.size();
 
   for (std::uint32_t i = 0; i < size; ++i) {
     auto b = i;
-    // Reverse bits
     b = (((b & 0xaaaaaaaa) >> 1) | ((b & 0x55555555) << 1));
     b = (((b & 0xcccccccc) >> 2) | ((b & 0x33333333) << 2));
     b = (((b & 0xf0f0f0f0) >> 4) | ((b & 0x0f0f0f0f) << 4));
@@ -79,8 +85,10 @@ void bit_reverse(span<c64> original) {
 }
 
 void iterative_cooley_tukey(span<c64> original) {
+  // Bit reverse the array.
   bit_reverse(original);
 
+  //Preform the butterfly on the array.
   auto size = original.size();
   for (std::size_t stride = 2; stride <= size; stride *= 2) {
     auto w = exp(c64(0, -2.0 * pi<double> / stride));
@@ -97,6 +105,7 @@ void iterative_cooley_tukey(span<c64> original) {
 }
 
 int main() {
+  // Initalizing the FFT inputs.
   auto random_number_generator = std::mt19937_64();
   auto generate_random_double = [&]() {
     auto rn = random_number_generator();
@@ -112,9 +121,11 @@ int main() {
   auto recursive = initial;
   auto iterative = initial;
 
+  // Preform an FFT on the arrays.
   cooley_tukey(recursive);
   iterative_cooley_tukey(iterative);
 
+  // Check if the arrays are approximate.
   std::cout
     << std::right
     << std::setw(16) << "idx"
