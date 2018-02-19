2a026671570bcbb0133740b55d721f2bbded9292
Nicole Mazzuca <npmazzuca@gmail.com>
2018-02-19 09:32:33 -0800

    modify the C++ to give it nicer output
    also change it slightly to make it more similar to other c++ code

--- chapters/computational_physics/euler/code/c++/euler.cpp
+++ chapters/computational_physics/euler/code/c++/euler.cpp
@@ -2,40 +2,68 @@
 #include <cmath>
 #include <vector>
 #include <cstddef>
+#include <iterator>
+#include <algorithm>
+#include <utility>
+
+using std::begin;
+using std::end;
 
 using std::size_t;
 
-std::vector<double> solve_euler(double timestep, size_t size){
-    if(size != 0){
-        auto result = std::vector<double>(size);
-        result[0] = 1.0;
-        for(size_t i = 1; i < size; ++i){
-            result[i] = result[i-1] - 3.0*result[i-1]*timestep;
-        }
-        return result;
-    }
+std::vector<double> solve_euler(double timestep, size_t size) {
+  std::vector<double> result;
+  double current = 1.0;
+  std::generate_n(
+    std::back_inserter(result),
+    size,
+    [&] { return std::exchange(current, current - 3.0 * current * timestep); });
+  return result;
 }
 
-bool check_result(std::vector<double>result, double threshold,
-        double timestep){
-
-    bool is_approx = true;
-    for(size_t i = 0; i < result.size(); ++i){
-        double solution = std::exp(-3.0*i* timestep);
-        if(std::fabs(result[i] - solution) > threshold){
-            std::cout << result[i] << "     " << solution << std::endl;
-            is_approx = false;
-        }
+/*
+  check_result takes an iterator over doubles,
+  and returns whether any value is outside the passed threshold.
+*/
+template <typename Iter>
+bool check_result(Iter first, Iter last, double threshold, double timestep) {
+  auto it = first;
+  for (size_t idx = 0; it != last; ++idx, ++it) {
+    double solution = std::exp(-3.0 * idx * timestep);
+    if (std::abs(*it - solution) > threshold) {
+      std::cout
+        << "We found a value outside the threshold; the "
+        << idx
+        << "-th value was "
+        << *it
+        << ", but the expected solution was "
+        << solution
+        << '\n';
+      std::cout
+        << "(the threshold was "
+        << threshold
+        << " and the difference was "
+        << std::abs(*it - solution)
+        << ")\n";
+      return true;
     }
-    return is_approx;
+  }
+  return false;
 }
 
 int main(){
-    double threshold = 0.01;
-    double timestep = 0.01;
+  double threshold = 0.01;
+  double timestep = 0.01;
 
-    std::vector<double> result = solve_euler(timestep, 100);
-    std::cout << check_result(result, threshold, timestep) << std::endl;
+  auto result = solve_euler(timestep, 100);
+  auto outside_threshold =
+    check_result(begin(result), end(result), threshold, timestep);
+  auto msg = outside_threshold ? "yes :(" : "no :D";
 
-    return 0;
+  std::cout
+    << "Were any of the values outside of the threshold ("
+    << threshold
+    << ")? "
+    << msg
+    << '\n';
 }
