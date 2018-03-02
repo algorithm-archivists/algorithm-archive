57504e9e234ce3d2f8de145db1b7031179884220
nicole mazzuca <npmazzuca@gmail.com>
2018-03-01 16:49:18 -0800

    modernize convolutions.cpp
    I don't know if this is correct, but it's as correct as the old one

--- chapters/mathematical_background/convolutions/code/c++/convolutions.cpp
+++ chapters/mathematical_background/convolutions/code/c++/convolutions.cpp
@@ -1,12 +1,22 @@
+#include <algorithm>
 #include <array>
+#include <cassert>
 #include <complex>
 #include <cstdint>
+#include <iterator>
 #include <vector>
 
 // These headers are for presentation not for the algorithm.
 #include <iomanip>
 #include <iostream>
 #include <random>
+
+using std::begin;
+using std::end;
+using std::swap;
+
+using std::ptrdiff_t;
+using std::size_t;
 
 using c64 = std::complex<double>;
 template <typename T>
@@ -15,141 +25,106 @@ constexpr T pi() {
 }
 
 // This section is not a part of the algorithm
-
-template <typename Iter, typename Iter_end>
-void fft(Iter start, Iter_end end) {
-  auto size = end - start;
+template <typename Iter>
+void fft(Iter const first, Iter const last) {
+  auto const size = last - first;
   if (size >= 2) {
-    // Splits the array, so the top half are the odd elements and the bottom are
-    // the even ones.
     auto temp = std::vector<c64>(size / 2);
-    for (std::size_t i = 0; i < size / 2; ++i) {
-      temp[i] = start[i * 2 + 1];
-      start[i] = start[i * 2];
+    for (ptrdiff_t i = 0; i < size / 2; ++i) {
+      temp[i] = first[i * 2 + 1];
+      first[i] = first[i * 2];
     }
-    for (std::size_t i = 0; i < size / 2; ++i) {
-      start[i + size / 2] = temp[i];
+    for (ptrdiff_t i = 0; i < size / 2; ++i) {
+      first[i + size / 2] = temp[i];
     }
 
-    // Recursion.
-    fft(start, start + size / 2);
-    fft(start + size / 2, end);
+    auto const split = first + size / 2;
+    fft(first, split);
+    fft(split, last);
 
-    // Combine.
-    for (std::size_t k = 0; k < size / 2; ++k) {
+    for (ptrdiff_t k = 0; k < size / 2; ++k) {
       auto w = std::exp(c64(0, -2.0 * pi<double>() * k / size));
-      start[k + size / 2] = start[k] - w * start[k + size / 2];
-      start[k] -= (start[k + size / 2] - start[k]);
+
+      auto& bottom = first[k];
+      auto& top = first[k + size / 2];
+      top = bottom - w * top;
+      bottom -= top - bottom;
     }
   }
 }
 
-template <typename Iter, typename Iter_end>
-void ifft(Iter start, Iter_end end) {
-  auto size = end - start;
-  for (std::size_t i = 0; i < size; i++) {
-    start[i] = std::conj(start[i]);
-  }
+template <typename Iter>
+void inverse_fft(Iter const first, Iter const last) {
+  std::for_each(first, last, [](auto& it) { it = std::conj(it); });
 
-  fft(start, end);
+  fft(first, last);
 
-  for (std::size_t i = 0; i < size; i++) {
-    start[i] = std::conj(start[i]) / static_cast<c64>(size);
-  }
+  auto const size = static_cast<c64>(last - first);
+  std::for_each(first, last, [&](auto& it) { it = std::conj(it) / size; });
 }
 
 // This section is a part of the algorithm
 
-template <typename Iter, typename Iter_end>
+template <typename S1, typename S2, typename Out>
 void conv(
-    Iter signal1start,
-    Iter_end signal1end,
-    Iter signal2start,
-    Iter_end signal2end,
-    Iter outstart,
-    Iter_end outend) {
+    S1 const s1,
+    S1 const s1_last,
+    S2 const s2,
+    S2 const s2_last,
+    Out const out) {
+  auto const size1 = s1_last - s1;
+  auto const size2 = s2_last - s2;
+  auto const size = size1 + size2;
 
-  auto size1 = (signal1end - signal1start);
-  auto size2 = (signal2end - signal2start);
-  c64 sum = 0;
-
-  for (std::size_t i = 0; i < (size1 < size2 ? size2 : size1); i++) {
-    for (std::size_t j = 0; j < i; j++) {
+  for (ptrdiff_t i = 0; i < size; ++i) {
+    c64 sum = 0;
+    for (ptrdiff_t j = 0; j < i; ++j) {
       if (j < size1) {
-        sum += signal1start[j] * signal2start[i - j];
+        sum += s1[j] * s2[i - j];
       }
     }
-    outstart[i] = sum;
-    sum = 0;
+    out[i] = sum;
   }
 }
 
-template <typename Iter, typename Iter_end>
-void conv_fft(
-    Iter signal1start,
-    Iter_end signal1end,
-    Iter signal2start,
-    Iter_end signal2end,
-    Iter outstart,
-    Iter_end outend) {
-  fft(signal1start, signal1end);
-  fft(signal2start, signal2end);
+template <typename S1, typename S2, typename Out>
+void conv_fft(S1 const s1, S1 const s1_last, S2 const s2, Out const out) {
+  auto const size = s1_last - s1;
 
-  for (std::size_t i = 0; i < (signal1end - signal1start); i++) {
-    outstart[i] = signal1start[i] * signal2start[i];
+  fft(s1, s1_last);
+  fft(s2, s2 + size);
+
+  auto s1_it = s1;
+  auto s2_it = s2;
+  auto out_it = out;
+  for (; s1_it != s1_last; ++s1_it, ++s2_it, ++out_it) {
+    *out_it = *s1_it * *s2_it;
   }
 
-  ifft(outstart, outend);
+  inverse_fft(out, out_it);
 }
 
 int main() {
-  std::array<c64, 64> signal1, signal2;
-  std::array<c64, 128> signal3, signal4, out1, out2;
+  auto signal1 = std::array<c64, 64>();
+  std::fill(begin(signal1) + 16, begin(signal1) + 48, 1);
 
-  for (std::size_t i = 0; i < 128; i++) {
-    if (i >= 16 && i < 48) {
-      signal1[i] = 1.0;
-      signal2[i] = 1.0;
-      signal3[i] = 1.0;
-      signal4[i] = 1.0;
-      out1[i] = 0.0;
-      out2[i] = 0.0;
-    } else if (i >= 64) {
-      signal3[i] = 0.0;
-      signal4[i] = 0.0;
-      out1[i] = 0.0;
-      out2[i] = 0.0;
-    } else {
-      signal1[i] = 0.0;
-      signal2[i] = 0.0;
-      signal3[i] = 0.0;
-      signal4[i] = 0.0;
-      out1[i] = 0.0;
-      out2[i] = 0.0;
-    }
-  }
+  auto signal2 = signal1;
 
-  conv(
-      signal1.begin(),
-      signal1.end(),
-      signal2.begin(),
-      signal2.end(),
-      out1.begin(),
-      out1.end());
-  conv_fft(
-      signal3.begin(),
-      signal3.end(),
-      signal4.begin(),
-      signal4.end(),
-      out2.begin(),
-      out2.end());
+  auto out1 = std::array<c64, 128>();
+  conv(begin(signal1), end(signal1), begin(signal2), end(signal2), begin(out1));
+
+  auto signal3 = std::array<c64, 128>();
+  std::copy(begin(signal1), end(signal1), begin(signal3));
+  auto signal4 = signal3;
+
+  auto out2 = std::array<c64, 128>();
+  conv_fft(begin(signal3), end(signal3), begin(signal4), begin(out2));
 
   std::cout << std::right << std::setw(16) << "i" << std::setw(16)
             << "subtracted" << '\n';
-  for (int i = 0; i < signal1.size(); ++i) {
+
+  for (size_t i = 0; i < signal1.size(); ++i) {
     std::cout << std::setw(16) << i << std::setw(16)
               << (std::abs(out1[i]) - std::abs(out2[i])) << '\n';
   }
-
-  return 0;
 }
