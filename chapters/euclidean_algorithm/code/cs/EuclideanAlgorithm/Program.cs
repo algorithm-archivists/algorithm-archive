f8c7f6838946c6f166711ebe0bb47e9700997d5f
Julian Schacher <jspp@posteo.net>
2018-04-15 23:18:33 +0200

    Add Program.cs for EuclideanAlgorithm and EuclideanAlgorithmMdAdditional.

---
+++ chapters/euclidean_algorithm/code/cs/EuclideanAlgorithm/Program.cs
@@ -0,0 +1,18 @@
+﻿// submitted by Julian Schacher (jspp)
+using System;
+
+namespace EuclideanAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EuclideanAlgorithm");
+            int check = EuclideanAlgorithm.EuclidMod(64 * 67, 64 * 81);
+            int check2 = EuclideanAlgorithm.EuclidSub(128 * 12, 128 * 77);
+
+            Console.WriteLine(check);
+            Console.WriteLine(check2);
+        }
+    }
+}
---
+++ chapters/euclidean_algorithm/code/cs/EuclideanAlgorithmMdAdditional/Program.cs
@@ -0,0 +1,18 @@
+﻿// submitted by Julian Schacher (jspp)
+using System;
+
+namespace EuclideanAlgorithmMdAdditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EuclideanAlgorithmMdAdditional");
+            int checkMdAdditional = EuclideanAlgorithmMdAdditional.EuclidMod(64 * 67, 64 * 81);
+            int checkMdAdditional2 = EuclideanAlgorithmMdAdditional.EuclidSub(128 * 12, 128 * 77);
+
+            Console.WriteLine(checkMdAdditional);
+            Console.WriteLine(checkMdAdditional2);
+        }
+    }
+}
