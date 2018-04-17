9204e4258cd183986492a3c807daaf7babdeb899
Gathros <6323830+Gathros@users.noreply.github.com>
2018-04-17 21:09:09 +0100

    using size_t

--- chapters/sorting_searching/bogo/code/c/bogo_sort.c
+++ chapters/sorting_searching/bogo/code/c/bogo_sort.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stddef.h>
 
-bool is_sorted(int *array, int n) {
+bool is_sorted(int *array, size_t n) {
     while (--n >= 1) {
         if (array[n] < array[n - 1]) {
             return false;
@@ -12,8 +13,8 @@ bool is_sorted(int *array, int n) {
     return true;
 }
 
-void shuffle(int *array, int n) {
-    for (int i = 0; i < n; ++i) {
+void shuffle(int *array, size_t n) {
+    for (size_t i = 0; i < n; ++i) {
         int t = array[i];
         int r = rand() % n;
         array[i] = array[r];
@@ -21,14 +22,14 @@ void shuffle(int *array, int n) {
     }
 }
 
-void bogo_sort(int *array, int n) {
+void bogo_sort(int *array, size_t n) {
     while (!is_sorted(array, n)) {
         shuffle(array, n);
     }
 }
 
 int main() {
-    int array[] = { 1, 3654, 78, 654, -234, -12, 4, 3, -6, -100};
+    int array[10] = {1, 3654, 78, 654, -234, -12, 4, 3, -6, -100};
 
     printf("Unsorted array:\n");
     for (int i = 0; i < 10; ++i) {
