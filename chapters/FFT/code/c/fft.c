5d65985f1bcaf8809e3d35b999d8725b8b77b53f
The Gathros <6323830+Gathros@users.noreply.github.com>
2018-04-18 12:46:23 +0100

    C cleaning code (#79)
    * Fixing all the C code.
    
    * Updating tree_traversal.md
    
    * Making some changes
    
    * using size_t
    
    * fixed the for loops

--- chapters/FFT/code/c/fft.c
+++ chapters/FFT/code/c/fft.c
@@ -1,52 +1,45 @@
-// written by Gathros.
-
 #include <complex.h>
 #include <math.h>
-
-// These headers are for presentation not for the algorithm.
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 #define PI 3.1415926535897932384626
 
-void cooley_tukey(double complex *X, const size_t N){
-	if(N >= 2){
-		// Splits the array, so the top half are the odd elements and the bottom half are the even ones.
-		double complex tmp [N/2];
-		for(size_t i = 0; i < N/2; ++i){
-			tmp[i] = X[2*i + 1];
-			X[i] = X[2*i];
-		}
-		for(size_t i = 0; i < N/2; ++i){
-			X[i + N/2] = tmp[i];
-		}
+void cooley_tukey(double complex *X, const size_t N) {
+    if (N >= 2) {
+        double complex tmp [N / 2];
+        for (size_t i = 0; i < N / 2; ++i) {
+            tmp[i] = X[2*i + 1];
+            X[i] = X[2*i];
+        }
+        for (size_t i = 0; i < N / 2; ++i) {
+            X[i + N / 2] = tmp[i];
+        }
 
-		// Recursion.
-		cooley_tukey(X, N/2);
-		cooley_tukey(X + N/2, N/2);
+        cooley_tukey(X, N / 2);
+        cooley_tukey(X + N / 2, N / 2);
 
-		// Combine.
-		for(size_t i = 0; i < N/2; ++i){
-			X[i + N/2] = X[i] - cexp(-2.0*I*PI*i/N)*X[i + N/2];
-			X[i] -= (X[i + N/2]-X[i]);
-		}
-	}
+        for (size_t i = 0; i < N / 2; ++i) {
+            X[i + N / 2] = X[i] - cexp(-2.0 * I * PI * i / N) * X[i + N / 2];
+            X[i] -= (X[i + N / 2]-X[i]);
+        }
+    }
 }
 
-void bit_reverse(double complex *X, size_t N){
-        // Bit reverses the array X[] but only if the size of the array is less then 2^32.
-		double complex temp;
+void bit_reverse(double complex *X, size_t N) {
+        double complex temp;
         unsigned int b;
 
-        for(unsigned int i = 0; i < N; ++i){
+        for (unsigned int i = 0; i < N; ++i) {
                 b = i;
                 b = (((b & 0xaaaaaaaa) >> 1) | ((b & 0x55555555) << 1));
                 b = (((b & 0xcccccccc) >> 2) | ((b & 0x33333333) << 2));
                 b = (((b & 0xf0f0f0f0) >> 4) | ((b & 0x0f0f0f0f) << 4));
                 b = (((b & 0xff00ff00) >> 8) | ((b & 0x00ff00ff) << 8));
-                b = ((b >> 16) | (b << 16)) >> (32 - (unsigned int) log2((double)N));
-                if(b > i){
+                b = ((b >> 16) | (b << 16)) >>
+                        (32 - (unsigned int) log2((double)N));
+                if (b > i) {
                         temp = X[b];
                         X[b] = X[i];
                         X[i] = temp;
@@ -54,52 +47,42 @@ void bit_reverse(double complex *X, size_t N){
         }
 }
 
-void iterative_cooley_tukey(double complex *X, size_t N){
-	int stride;
-	double complex v,w;
+void iterative_cooley_tukey(double complex *X, size_t N) {
+    bit_reverse(X, N);
 
-	// Bit reverse the array.
-	bit_reverse(X, N);
-
-	// Preform the butterfly on the array.
-	for(int i = 1; i <= log2((double)N); ++i){
-		stride = pow(2, i);
-		w = cexp(-2.0*I*PI/stride);
-		for(size_t j = 0; j < N; j += stride){
-			v = 1.0;
-			for(size_t k = 0; k < stride/2; k++){
-				X[k + j + stride/2] = X[k + j] - v*X[k + j + stride/2];
-				X[k + j] -= (X[k + j + stride/2] - X[k + j]);
-				v *= w;
-			}
-		}
-	}
+    for (int i = 1; i <= log2((double)N); ++i) {
+        int stride = pow(2, i);
+        double complex w = cexp(-2.0 * I * PI / stride);
+        for (size_t j = 0; j < N; j += stride) {
+            double complex v = 1.0;
+            for (size_t k = 0; k < stride / 2; ++k) {
+                X[k + j + stride / 2] = X[k + j] - v * X[k + j + stride / 2];
+                X[k + j] -= (X[k + j + stride / 2] - X[k + j]);
+                v *= w;
+            }
+        }
+    }
 }
 
-void approx(double complex *X, double complex *Y, size_t N){
-	// This is to show that the arrays are approximate.
-	for(size_t i = 0; i < N; ++i){
-		printf("%f\n", cabs(X[i]) - cabs(Y[i]));
-	}
+void approx(double complex *X, double complex *Y, size_t N) {
+    for (size_t i = 0; i < N; ++i) {
+        printf("%f\n", cabs(X[i]) - cabs(Y[i]));
+    }
 }
 
-int main(){
-	// Initalizing the arrays for FFT.
-	srand(time(NULL));
-	const size_t N = 64;
-	double complex x[N], y[N], z[N];
-	for(size_t i = 0; i < N; ++i){
-		x[i] = rand() / (double) RAND_MAX;
-		y[i] = x[i];
-		z[i] = x[i];
-	}
+int main() {
+    srand(time(NULL));
+    double complex x[64], y[64], z[64];
+    for (size_t i = 0; i < 64; ++i) {
+        x[i] = rand() / (double) RAND_MAX;
+        y[i] = x[i];
+        z[i] = x[i];
+    }
 
-	// Preform FFT.
-	cooley_tukey(y, N);
-	iterative_cooley_tukey(z, N);
+    cooley_tukey(y, 64);
+    iterative_cooley_tukey(z, 64);
 
-	// Check if the different methods are approximate.
-	approx(y, z, N);
+    approx(y, z, 64);
 
-	return 0;
+    return 0;
 }
--- chapters/euclidean_algorithm/code/c/euclidean_example.c
+++ chapters/euclidean_algorithm/code/c/euclidean_example.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 
-int euclid_mod(int a, int b)
-{
+int euclid_mod(int a, int b) {
     a = abs(a);
     b = abs(b);
 
-    while (b != 0){
+    while (b != 0) {
         int temp = b;
-        b = a%b;
+        b = a % b;
         a = temp;
     }
 
     return a;
 }
 
-int euclid_sub(int a, int b)
-{
+int euclid_sub(int a, int b) {
     a = abs(a);
     b = abs(b);
 
     while (a != b) {
         if (a > b) {
             a -= b;
-        }
-        else {
+        } else {
             b -= a;
         }
     }
@@ -32,12 +29,12 @@ int euclid_sub(int a, int b)
     return a;
 }
 
-int main()
-{
-    int check1 = euclid_mod(64*67, 64*81);
-    int check2 = euclid_sub(128*12, 128*77);
+int main() {
+    int check1 = euclid_mod(64 * 67, 64 * 81);
+    int check2 = euclid_sub(128 * 12, 128 * 77);
 
     printf("%d\n", check1);
     printf("%d\n", check2);
-}
 
+    return 0;
+}
