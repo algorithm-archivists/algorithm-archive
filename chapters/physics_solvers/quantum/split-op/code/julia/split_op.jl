239a60f61f8354f5bf51ba9ace53d7509bbbf318
leios <jrs.schloss@gmail.com>
2018-05-19 05:39:07 +0900

    adding smallscale changes to spacing in split_op.jl

--- chapters/physics_solvers/quantum/split-op/code/julia/split_op.jl
+++ chapters/physics_solvers/quantum/split-op/code/julia/split_op.jl
@@ -12,9 +12,10 @@ struct Param
     dk::Float64
     k::Vector{Float64}
 
-    Param() = new(10.0, 512, 0.05, 1000, 2*10.0 / 512,
-                  Vector{Float64}(-10.0+10.0/512:20.0/512:10.0), pi / 10.0,
-                  Vector{Float64}(vcat(0:512/2-1, -512/2:-1)*pi/10.0))
+    Param() = new(10.0, 512, 0.05, 1000, 2 * 10.0/512,
+                  Vector{Float64}(-10.0 + 10.0/512 : 20.0/512 : 10.0),
+                   pi / 10.0,
+                  Vector{Float64}(vcat(0:512/2 - 1, -512/2 : -1) * pi/10.0))
     Param(xmax::Float64, res::Int64, dt::Float64, timesteps::Int64) = new(
               xmax, res, dt, timesteps, 
               2*xmax/res, Vector{Float64}(-xmax+xmax/res:2*xmax/res:xmax),
@@ -33,7 +34,7 @@ end
 # Function to initialize the wfc and potential
 function init(par::Param, voffset::Float64, wfcoffset::Float64)
     V = 0.5 * (par.x - voffset).^2
-    wfc = 3* exp.(-(par.x-wfcoffset).^2/2)
+    wfc = 3 * exp.(-(par.x - wfcoffset).^2/2)
     PE = exp.(-0.5*im*V*par.dt)
     KE = exp.(-0.5*im*par.k.^2*par.dt)
 
@@ -45,25 +46,25 @@ function split_op(par::Param, opr::Operators)
 
     for i = 1:par.timesteps
         # Half-step in real space
-        opr.wfc = opr.wfc.*opr.PE
+        opr.wfc = opr.wfc .* opr.PE
 
         # fft to phase space
         opr.wfc = fft(opr.wfc)
 
         # Full step in phase space
-        opr.wfc = opr.wfc.*opr.KE
+        opr.wfc = opr.wfc .* opr.KE
 
         # ifft back
         opr.wfc = ifft(opr.wfc)
 
         # final half-step in real space
-        opr.wfc = opr.wfc.*opr.PE
+        opr.wfc = opr.wfc .* opr.PE
 
         # plotting density and potential
         density = abs2.(opr.wfc)
 
         plot([density, real(opr.V)])
-        savefig("density"*string(lpad(i, 5, 0))*".png")
+        savefig("density" * string(lpad(i, 5, 0)) * ".png")
         println(i)
     end
 end
