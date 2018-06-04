0af4201732069109a8c05b4ccde6e9f79103c0e3
Gathros <6323830+Gathros@users.noreply.github.com>
2018-06-04 20:17:12 +0100

    naming fix for monte_carlo.c

--- chapters/monte_carlo/code/c/monte_carlo.c
+++ chapters/monte_carlo/code/c/monte_carlo.c
@@ -8,19 +8,19 @@ bool in_circle(double x, double y, double radius) {
 }
 
 void monte_carlo(int samples, double radius) {
-    int cnts = 0.0;
+    int count = 0;
 
     for (int i = 0; i < samples; ++i) {
         double x = (double)rand() * 2.0 / RAND_MAX - 1.0;
         double y = (double)rand() * 2.0 / RAND_MAX - 1.0;
 
         if (in_circle(x, y, radius)) {
-            cnts += 1;
+            count += 1;
         }
     }
 
     printf("the estimate of pi is %f\n",
-           4.0 * cnts / (samples * radius * radius));
+           4.0 * count / (samples * radius * radius));
 }
 
 int main() {
