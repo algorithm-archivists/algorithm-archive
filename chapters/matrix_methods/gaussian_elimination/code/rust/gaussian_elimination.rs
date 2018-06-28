bf264c20b2f539da794637ddf45c11221b078e38
Jess 3Jane <me@jess.coffee>
2018-06-28 10:51:08 -0400

    The gaussian elimination part works now but back substitution is completely broken

--- chapters/matrix_methods/gaussian_elimination/code/rust/gaussian_elimination.rs
+++ chapters/matrix_methods/gaussian_elimination/code/rust/gaussian_elimination.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::cmp::min;
 
 pub struct Matrix {
     rows: usize,
@@ -34,23 +35,85 @@ impl fmt::Display for Matrix {
             if row != 0 { writeln!(f, "")?; }
             for col in 0..self.cols {
                 if col != 0 { write!(f, " ")?; }
-                write!(f, "{}", self.get(row, col))?;
+                write!(f, "{:3}", self.get(row, col))?;
             }
         }
         Ok(())
     }
 }
 
-fn main() {
-    let mut mat = Matrix::new(3,3);
+fn gaussian_elimination(a: &mut Matrix) {
+    for k in 0..min(a.cols, a.rows) {
+        // Step 1: find the maximum element for this kumn
+        let mut max_row = 0;
+        let mut max_value = 0.0;
+        for row in k..a.rows {
+            if max_value < a.get(row, k).abs() {
+                max_value = a.get(row, k).abs();
+                max_row = row;
+            }
+        }
 
-    for i in 0..3 {
-        for j in 0..3 {
-            mat.set(i, j, (i*j) as f64);
+        // Check to make sure the matrix is good
+        if a.get(max_row, k) == 0.0 {
+            println!("Matrix is singular, aborting");
+            return;
+        }
+
+        // Step 2: swap the row with the highest value for this kumn to the top
+        a.swap_rows(k, max_row);
+        println!("{}:\n{}", k, a);
+
+        // Loop over all remaining rows
+        for i in k+1..a.rows {
+            // Step 3: find the fraction
+            let fraction = a.get(i, k)/a.get(k, k);
+
+            // Loop through all columns for that row
+            for j in (k+1)..a.cols {
+                // Step 4: re-evaluate each element
+                let val = a.get(i, j) - a.get(k, j)*fraction;
+                a.set(i, j, val);
+            }
+
+            // Step 5: set lower elements to 0 
+            a.set(i, k, 0.0);
         }
     }
+}
 
-    mat.swap_rows(0, 1);
+fn back_substitution(a: &Matrix) -> Vec<f64> {
+    let mut soln = Vec::with_capacity(a.rows);
+    for _ in 0..a.rows { soln.push(0.0); }
+    
+    soln[a.rows - 1] = a.get(a.rows - 1, a.cols - 1) / a.get(a.rows - 1, a.cols - 2);
 
-    println!("{}", mat);
+    for i in (0..a.rows - 2).rev() {
+        let mut sum = 0.0;
+        for j in (i..a.rows - 1).rev() {
+            sum += soln[j] * a.get(i, j);
+            println!("{}, {}", i, j);
+        }
+        soln[i] = (a.get(i, a.cols - 1) - sum) / a.get(i,i);
+    }
+
+    soln
+}
+
+fn main() {
+    // The example matrix from the text
+    let mut a = Matrix::new(3,4);
+    a.data = vec![2.0,  3.0, 4.0,  6.0,
+                  1.0,  2.0, 3.0,  4.0,
+                  3.0, -4.0, 0.0, 10.0,];
+    
+    println!("Original:");
+    println!("{}", a);
+
+    gaussian_elimination(&mut a);
+    println!("Reduced:");
+    println!("{}", a);
+
+    let soln = back_substitution(&a);
+    println!("Solution: {:?}", soln);
 }
