71ff69280f39707e2066a278db855fcae3b621f2
Julian <jspp@posteo.net>
2018-06-28 23:54:00 +0200

    Enhance the code of TreeTraversal and EuclideanAlgorithm for C# (#120)
    * Add all additional methods from TreeTraversalMdAdditional to TreeTraversal. Fix incorrect ConsoleWriteLine. Use local functions. Create constructor for Node, which takes an id. Remove unnecessary curly braces. Remove redundant CreateTree method and just put the code into the constructor. Remove unnecessary comment. Remove the now redundant TreeTraversalMdAdditional. Sort the methods so that they have the same order as in the text.
    
    * Delete redundant EuclideanAlgorithmMdAdditional.
    
    * Make EuclideanAlgorithm non-static.
    
    * Make file be "ASCII text" instead of "UTF-8 Unicode (with BOM) text, with CRLF line terminators".
    
    * Provide explanation for commented lines of code.

--- chapters/euclidean_algorithm/code/cs/Program.cs
+++ chapters/euclidean_algorithm/code/cs/Program.cs
@@ -8,8 +8,9 @@ namespace EuclideanAlgorithm
         static void Main(string[] args)
         {
             Console.WriteLine("EuclideanAlgorithm");
-            int check = EuclideanAlgorithm.EuclidMod(64 * 67, 64 * 81);
-            int check2 = EuclideanAlgorithm.EuclidSub(128 * 12, 128 * 77);
+            var euclideanAlgorithm = new EuclideanAlgorithm();
+            int check = euclideanAlgorithm.EuclidMod(64 * 67, 64 * 81);
+            int check2 = euclideanAlgorithm.EuclidSub(128 * 12, 128 * 77);
 
             Console.WriteLine(check);
             Console.WriteLine(check2);
