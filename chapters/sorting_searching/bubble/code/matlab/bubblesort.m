5f354cb2045b6c296e1865b4b18d1e077dd5143d
Kjetil Andre Johannessen <kjetijo@gmail.com>
2018-06-29 14:41:42 +0200

    Added Matlab bubblesort

---
+++ chapters/sorting_searching/bubble/code/matlab/bubblesort.m
@@ -0,0 +1,25 @@
+function main()
+    array = floor( rand(1,7)*100 );
+    disp('Before Sorting:')
+    disp(array)
+
+    array = bubble_sort(array);
+    disp('After Sorting')
+    disp(array)
+end
+
+function sorted_array = bubble_sort(array)
+    for i=1:length(array)
+        for j=1:length(array)-i
+            if array(j) > array(j+1)
+                % swap elements in the list
+                temp       = array(j);
+                array(j)   = array(j+1);
+                array(j+1) = temp;
+            end
+        end
+    end
+    sorted_array = array
+end
+
+
