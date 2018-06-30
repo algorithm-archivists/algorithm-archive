d2bab0f332a24d58ee841e3646996e014a5e8bd6
xam4lor <xam4lor@gmail.com>
2018-06-30 10:26:01 +0200

    Changed tabs to spaces

--- chapters/monte_carlo/code/js/monte_carlo.js
+++ chapters/monte_carlo/code/js/monte_carlo.js
@@ -1,22 +1,22 @@
 // submitted by xam4lor
 function inCircle(xPos, yPos, radius) {
-    return xPos * xPos + yPos * yPos < radius * radius;
+  return xPos * xPos + yPos * yPos < radius * radius;
 }
 
 function monteCarlo(n, radius) {
-    let piCount = 0;
+  let piCount = 0;
 
-    for (let i = 0; i < n; i++) {
-        const pointX = Math.random();
-        const pointY = Math.random();
+  for (let i = 0; i < n; i++) {
+    const pointX = Math.random();
+    const pointY = Math.random();
 
-        if (inCircle(pointX, pointY, radius)) {
-            piCount++;
-        }
+    if (inCircle(pointX, pointY, radius)) {
+      piCount++;
     }
+  }
 
-    const piEstimate = 4 * Math.PI / (n * (radius * radius));
-    console.log('Percent error is: %s%', piEstimate);
+  const piEstimate = 4 * Math.PI / (n * (radius * radius));
+  console.log('Percent error is: %s%', piEstimate);
 }
 
 monteCarlo(100000000, 0.5);
