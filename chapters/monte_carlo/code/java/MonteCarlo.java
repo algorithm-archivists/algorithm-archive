16b5ae9eb820a9bfcfc224abe67ea20044c84020
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2018-07-01 16:59:18 +0200

    Set fixed radius to 1

--- chapters/monte_carlo/code/java/MonteCarlo.java
+++ chapters/monte_carlo/code/java/MonteCarlo.java
@@ -4,16 +4,16 @@ import java.util.Random;
 public class MonteCarlo {
 
     public static void main(String[] args){
-        monteCarlo(10_000_000, 0.5);
+        monteCarlo(10_000_000);
     }
 
-    //function to check whether point (x,y) is in circle of radius r
-    private static boolean inCircle(double x, double y, double radius){
-        return x*x + y*y < radius*radius;
+    //function to check whether point (x,y) is in unit circle
+    private static boolean inCircle(double x, double y){
+        return x*x + y*y < 1;
     }
 
     //function to calculate estimation of pi
-    public static void monteCarlo(int samples, double radius){
+    public static void monteCarlo(int samples){
         int piCount = 0;
 
         Random random = new Random();
@@ -21,12 +21,12 @@ public class MonteCarlo {
         for(int i = 0; i < samples; i++){
             double x = random.nextDouble();
             double y = random.nextDouble();
-            if(inCircle(x, y, radius)){
+            if(inCircle(x, y)){
                 piCount++;
             }
         }
 
-        double estimation = 4 * piCount / (samples * radius * radius);
+        double estimation = 4.0 * piCount / (samples);
 
         System.out.println("Estimated pi value: " + estimation);
         System.out.printf("Percent error: %.4f%%",  100*(Math.PI-estimation)/Math.PI);
