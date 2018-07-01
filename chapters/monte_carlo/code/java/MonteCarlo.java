0655f8f55e81e530c052039f90056af28d2b5587
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2018-07-01 22:16:50 +0200

    Corrected code style

--- chapters/monte_carlo/code/java/MonteCarlo.java
+++ chapters/monte_carlo/code/java/MonteCarlo.java
@@ -3,32 +3,33 @@ import java.util.Random;
 
 public class MonteCarlo {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         monteCarlo(10_000_000);
     }
 
     //function to check whether point (x,y) is in unit circle
-    private static boolean inCircle(double x, double y){
-        return x*x + y*y < 1;
+    private static boolean inCircle(double x, double y) {
+        return x * x + y * y < 1;
     }
 
     //function to calculate estimation of pi
-    public static void monteCarlo(int samples){
+    public static void monteCarlo(int samples) {
         int piCount = 0;
 
         Random random = new Random();
 
-        for(int i = 0; i < samples; i++){
+        for (int i = 0; i < samples; i++) {
             double x = random.nextDouble();
             double y = random.nextDouble();
-            if(inCircle(x, y)){
+            if (inCircle(x, y)) {
                 piCount++;
             }
         }
 
-        double estimation = 4.0 * piCount / (samples);
+        double estimation = 4.0 * piCount / samples;
 
         System.out.println("Estimated pi value: " + estimation);
-        System.out.printf("Percent error: %.4f%%",  100*(Math.PI-estimation)/Math.PI);
+        System.out.printf("Percent error: %.4f%%",  
+                          100 * (Math.PI - estimation) / Math.PI);
     }
 }
