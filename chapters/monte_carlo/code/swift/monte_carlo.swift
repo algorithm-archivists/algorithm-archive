d95328416e16e1a14b62dc1d3d7382eb72d336d6
CD Sigma <cdsigma271@gmail.com>
2018-07-01 22:52:21 -0700

    Code Updates
    - Added comment
    - Removed radius as a free parameter for the monte carlo function
    - Moved error testing to main()

--- chapters/monte_carlo/code/swift/monte_carlo.swift
+++ chapters/monte_carlo/code/swift/monte_carlo.swift
@@ -5,50 +5,53 @@ import Foundation
 
 
 public extension Double {
-
+    
     public static var random: Double {
         
-        return Double(arc4random()) / 0xFFFFFFFF
+        return Double(arc4random()) / 0xFFFFFFFF // Returns a random double between 0.0 and 1.0, inclusive.
     }
-
+    
     public static func random(min: Double, max: Double) -> Double {
-
+        
         return Double.random * (max - min) + min
     }
 }
 
 
 func isInCircle(x: Double, y: Double, radius: Double) -> Bool {
-
+    
     return (x*x) + (y*y) < radius*radius
 }
 
 
-func monteCarlo(n: Int, radius: Double) -> Double {
-
+func monteCarlo(n: Int) -> Double {
+    
+    let radius: Double = 1
     var piCount = 0
     var randX: Double
     var randY: Double
-
+    
     for _ in 0...n {
         randX = Double.random(min: 0, max: radius)
         randY = Double.random(min: 0, max: radius)
-
+        
         if(isInCircle(x: randX, y: randY, radius: radius)) {
             piCount += 1
         }
     }
     
     let piEstimate = Double(4 * piCount)/(Double(n))
-    print("Percent error is: \(100*(Double.pi - piEstimate)/Double.pi)%")
-
+    
     return piEstimate
 }
 
 
 func main() {
-    print("Pi estimate is: ", monteCarlo(n: 10000, radius: 50))
+    let piEstimate = monteCarlo(n: 10000)
+    print("Pi estimate is: ", piEstimate)
+    print("Percent error is: \(100*(Double.pi - piEstimate)/Double.pi)%")
 }
 
 
 main()
+
