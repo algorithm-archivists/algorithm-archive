d7f0169199ffbba26a7fe6674afdd39387334708
Garrison Grogan <garrison.grogan@columbia.edu>
2018-07-02 17:07:06 -0400

    style fixes

--- chapters/matrix_methods/thomas/code/python/thomas.py
+++ chapters/matrix_methods/thomas/code/python/thomas.py
@@ -1,7 +1,7 @@
 #Author: gammison
 
 #note this example is inplace and destructive
-def thomas(a:list,b:list,c:list,d:list):
+def thomas(a,b,c,d):
     
     #set the initial elements
     c[0] = c[0] / b[0]
@@ -22,21 +22,19 @@ def thomas(a:list,b:list,c:list,d:list):
         d[i] = d[i] - c[i]*d[i+1]
     
     return d
-    
-    #Complexity:
-    #Two loops,each take O(n), and a few contant time operations => T(n) = 2*O(n)+c*O(1) = O(n)
 
-#example for matrix
-#[1  4  0][x]   [7]
-#[2  3  5][y] = [5]
-#[0  3  6][z]   [3]
-#                [.8666]
-#soln will equal [1.533]
-#                [-.266]
-#note we index a from 1 and c from 0
-a=[0,2,3]
-b=[1,3,6]
-c=[4,5,0]
-d=[7,5,3]
-soln = thomas(a,b,c,d)
-print(soln)
+def main():
+    #example for matrix
+    #[1  4  0][x]   [7]
+    #[2  3  5][y] = [5]
+    #[0  3  6][z]   [3]
+    #                [.8666]
+    #soln will equal [1.533]
+    #                [-.266]
+    #note we index a from 1 and c from 0
+    a=[0,2,3]
+    b=[1,3,6]
+    c=[4,5,0]
+    d=[7,5,3]
+    soln = thomas(a,b,c,d)
+    print(soln)
