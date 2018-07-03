e45339510459458c7b356466419b018f17695f5e
Garrison Grogan <garrison.grogan@columbia.edu>
2018-07-03 13:41:02 -0400

    style fixes

--- chapters/matrix_methods/thomas/code/python/thomas.py
+++ chapters/matrix_methods/thomas/code/python/thomas.py
@@ -1,14 +1,13 @@
 #Author: gammison
 
 #note this example is inplace and destructive
-def thomas(a,b,c,d):
+def thomas(a, b, c, d):
     
     #set the initial elements
     c[0] = c[0] / b[0]
     d[0] = d[0] / b[0]
 
     n = len(d) #number of equations to solve
-    #range is exclusive, loop will run from 1 to n-1
     for i in range(1,n):
         #scale factor for c and d
         scale = 1 / (b[i] - c[i-1]*a[i])
@@ -17,7 +16,7 @@ def thomas(a,b,c,d):
         d[i] = (d[i] -a[i] * d[i-1]) * scale
 
     
-    #do the back substitution, will loop n-2 to 0 inclusive
+    #do the back substitution
     for i in range(n-2,-1,-1):
         d[i] = d[i] - c[i]*d[i+1]
     
@@ -32,10 +31,10 @@ def main():
     #soln will equal [1.533]
     #                [-.266]
     #note we index a from 1 and c from 0
-    a=[0,2,3]
-    b=[1,3,6]
-    c=[4,5,0]
-    d=[7,5,3]
+    a = [0,2,3]
+    b = [1,3,6]
+    c = [4,5,0]
+    d = [7,5,3]
     soln = thomas(a,b,c,d)
     print(soln)
 
