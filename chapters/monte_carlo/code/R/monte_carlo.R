2bd4e50b831875abfbffb1b5a24f431cfbb6c8aa
GuyPozner <guy.pozner@mail.huji.ac.il>
2018-07-03 09:36:40 +0300

    fixed radius hardcoding

--- chapters/monte_carlo/code/R/monte_carlo.R
+++ chapters/monte_carlo/code/R/monte_carlo.R
@@ -1,29 +1,27 @@
 
-in_circle <- function(x, y){
+in_circle <- function(x, y, radius = 1){
         # Return True if the point is in the circle and False otherwise.
-        radius = 1.0
         return((x*x + y*y) < radius*radius)
 }
 
-monte_carlo <- function(n_samples){
+monte_carlo <- function(n_samples, radius = 1){
 # Return the estimate of pi using the monte carlo algorithm.
-        radius = 1.0
         
         # Sample x, y from the uniform distribution
-        x <- runif(n_samples, 0, 1)
-        y <- runif(n_samples, 0, 1)
+        x <- runif(n_samples, 0, radius)
+        y <- runif(n_samples, 0, radius)
 
         # Count the number of points inside the circle
-        in_circle_count <- sum(in_circle(x, y))
+        in_circle_count <- sum(in_circle(x, y, radius))
 
-        # Since we've generated points in upper left quadrant ([0,1], [0,1])
+        # Since we've generated points in upper left quadrant ([0,radius], [0,])
         # We need to multiply the number of points by 4	
-        pi_estimate <- 4 * in_circle_count / (n_samples * radius * radius)
+        pi_estimate <- 4 * in_circle_count / n_samples
 
         return(pi_estimate)
 }
 
-pi_estimate <- monte_carlo(100000)
+pi_estimate <- monte_carlo(10000000)
 percent_error <- abs(pi - pi_estimate)/pi
 
 print(paste("The estimate of pi is: ", formatC(pi_estimate)))
