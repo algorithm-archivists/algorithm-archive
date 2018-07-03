8182fae56459b2a3394a5bd9dfb8cfbea8aa8bd4
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-03 15:52:50 +0900

    rustfmt

--- chapters/differential_equations/euler/code/rust/euler.rs
+++ chapters/differential_equations/euler/code/rust/euler.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut result = [0.0;100];
+    let mut result = [0.0; 100];
     let threshold = 0.01;
     let timestep = 0.01;
 
@@ -7,19 +7,17 @@ fn main() {
     println!("{}", check_result(&result, threshold, timestep));
 }
 
-
 fn solve_euler(timestep: f64, result: &mut [f64]) {
     let n = result.len();
     if n != 0 {
         result[0] = 1.0;
         for i in 1..n {
-            result[i] = result[i-1] - 3.0 * result[i-1] * timestep;
+            result[i] = result[i - 1] - 3.0 * result[i - 1] * timestep;
         }
     }
 }
 
-
-fn check_result(result: &[f64],  threshold: f64,  timestep: f64) -> bool {
+fn check_result(result: &[f64], threshold: f64, timestep: f64) -> bool {
     let mut is_approx: bool = true;
     for (i, val) in result.iter().enumerate() {
         let solution = (-3.0 * i as f64 * timestep).exp();
@@ -31,4 +29,3 @@ fn check_result(result: &[f64],  threshold: f64,  timestep: f64) -> bool {
 
     return is_approx;
 }
-
