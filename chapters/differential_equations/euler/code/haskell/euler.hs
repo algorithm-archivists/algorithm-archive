eb1cd8d0902b67a30fecc25cefc3553077912abf
Unknown <sciencegeek1999@gmail.com>
2018-07-04 16:20:56 -0500

    Add explicit types and apply hindent

--- chapters/differential_equations/euler/code/haskell/euler.hs
+++ chapters/differential_equations/euler/code/haskell/euler.hs
@@ -1,13 +1,20 @@
-solve_euler timestep n = take n $ iterate f 1
-    where f x = x - 3 * x * timestep
+solveEuler :: Double -> Int -> [Double]
+solveEuler timestep n = take n $ iterate f 1
+    where
+        f x = x - 3 * x * timestep
 
-check_result results threshold timestep =
-    and $ zipWith check' results [exp $ -3 * i * timestep | i <- [0..]]
-    where check' result solution = abs (result - solution) < threshold
+checkResult :: [Double] -> Double -> Double -> Bool
+checkResult results threshold timestep =
+    and $ zipWith check' results [exp $ -3 * i * timestep | i <- [0 ..]]
+    where
+        check' result solution = abs (result - solution) < threshold
 
-main = let timestep = 0.01;
-           n = 1;
-           threshold = 0.01;
-        in putStrLn $ if check_result (solve_euler timestep n) threshold timestep
-                      then "All values within threshold"
-                      else "Value(s) not in threshold"
+main :: IO ()
+main =
+    let timestep = 0.01
+        n = 1
+        threshold = 0.01
+    in  putStrLn $
+        if checkResult (solveEuler timestep n) threshold timestep
+        then "All values within threshold"
+        else "Value(s) not in threshold"
