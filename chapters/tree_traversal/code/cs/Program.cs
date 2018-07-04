6d02784f5e59d8a241a82dbcc9fc0337d19362f3
Julian <jspp@posteo.net>
2018-07-04 07:19:41 +0200

    Update Tree Traversal C#. (#209)
    * Restructure code by removing the Node class. Remove CreateAllChildren method and make the constructor do it instead.
    
    * Inline temp variables of DFSSTack and BFSQueue.

--- chapters/tree_traversal/code/cs/Program.cs
+++ chapters/tree_traversal/code/cs/Program.cs
@@ -17,6 +17,7 @@ namespace TreeTraversal
             tree.BFSQueue();
             Console.WriteLine("DFSRecursivePostorder");
             tree.DFSRecursivePostorder();
+
             // Uncommenting the following 2 lines will result in an exception thrown because at least one Node of the Tree has more than 2 children and therefor a DFSRecursiveInorderBinary doesn't work.
             // Console.WriteLine("DFSRecursiveInorder (fail)");
             // tree.DFSRecursiveInorderBinary();
