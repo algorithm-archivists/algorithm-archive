ed1e252bb59f159eb278ce105a139431ed072fdc
chmod777 <oussamabouchareb006@gmail.com>
2018-07-04 20:58:13 +0100

    corrected wrong script

--- chapters/sorting_searching/selection/code/python/selection.py
+++ chapters/sorting_searching/selection/code/python/selection.py
@@ -1,10 +1,7 @@
-def insertion_sort(A):
-	for i in range(1, len(A)):
-		j = i-1
-		while A[j] > A[j+1] and j >= 0:
-			A[j], A[j+1] = A[j+1], A[j]
-			j -= 1
-
-
-A = [5,9,1,2,4,8,6,3,7]
-print(insertion_sort(A))
+def selectionList(lst):
+	for i in range(0, len(lst)):
+		if i < len(lst):
+			lst[i] = i
+	return lst
+t = [1,0,3,2,6,4,5,9,7,8]
+print(selectionList(t))
