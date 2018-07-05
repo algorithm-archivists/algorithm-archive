537be2c43df8f95a94950f7060b8c9e847fe5c51
Unknown <sciencegeek1999@gmail.com>
2018-07-05 13:40:54 -0500

    Generalize solveEuler and checkResults

--- chapters/differential_equations/euler/code/haskell/euler.hs
+++ chapters/differential_equations/euler/code/haskell/euler.hs
@@ -1,20 +1,23 @@
-solveEuler :: Double -> Int -> [Double]
-solveEuler timestep n = take n $ iterate f 1
-    where
-        f x = x - 3 * x * timestep
+solveEuler :: Num a => (a -> a) -> a -> Int -> [a]
+solveEuler func initVal n = take n $ iterate func initVal
 
-checkResult :: [Double] -> Double -> Double -> Bool
-checkResult results threshold timestep =
-    and $ zipWith check' results [exp $ -3 * i * timestep | i <- [0 ..]]
+checkResult :: (Num a, Ord a, Enum a) => [a] -> (a -> a) -> a -> Bool
+checkResult results checkFunc threshold =
+    and $ zipWith check' results (fmap checkFunc [0 ..])
     where
         check' result solution = abs (result - solution) < threshold
+
+kinematics :: Double -> Double -> Double
+kinematics timestep x = x - 3 * x * timestep
 
 main :: IO ()
 main =
     let timestep = 0.01
-        n = 1
+        n = 100
         threshold = 0.01
+        kinematics' = kinematics timestep
+        checkFunc'  = exp . kinematics'
     in  putStrLn $
-        if checkResult (solveEuler timestep n) threshold timestep
+        if checkResult (solveEuler kinematics' 1 n) checkFunc' threshold
         then "All values within threshold"
         else "Value(s) not in threshold"
