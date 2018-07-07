973e4c34ac5e125650ff85f2cbee624ae2621e75
William Boyles <40878829+wmboyles@users.noreply.github.com>
2018-07-06 23:33:19 -0400

    Update jarvits.py

--- chapters/computational_geometry/gift_wrapping/jarvis_march/code/py/jarvits.py
+++ chapters/computational_geometry/gift_wrapping/jarvis_march/code/py/jarvits.py
@@ -3,39 +3,41 @@ def CCW(p1, p2, p3):
     return (p3[1]-p1[1])*(p2[0]-p1[0]) >= (p2[1]-p1[1])*(p3[0]-p1[0])
 
 
-#Find the leftmost point in the list
-def leftmost(gift):
-    leftmost = gift[0]
-    for point in gift[1:]:
-        if point[0]<leftmost[0]: leftmost=point
-    return leftmost
-
-
-#Are two points the same point?
-def equal(p1,p2):
-    return p1[0]==p2[0] and p1[1]==p2[1]
-
-
 def jarvisMarch(gift):
     n = len(gift)  #Number of points in list
-    hull = [(None,None)] * n #The hull, initially empty points
-    pointOnHull = leftmost(gift) #leftmost point is guranteeded to be in hull
+    pointOnHull = min(gift) #leftmost point in gift
+    hull = [pointOnHull] #leftmost point guranteed to be in hull
     
     i = 0
     while True:
-        hull[i] = pointOnHull
         endpoint = gift[0]
         for j in range(1,n):
-            if equal(endpoint,pointOnHull) or not CCW(gift[j],hull[i],endpoint):
+            if endpoint==pointOnHull or not CCW(gift[j],hull[i],endpoint):
                 endpoint = gift[j]
+                
         i+=1
-        pointOnHull = endpoint
-        if equal(hull[0],endpoint):
+        pointOnHull = endpoint        
+                
+        #Check if we have completely wrapped gift
+        if hull[0]==endpoint:
             break
+        else:
+            hull.append(pointOnHull)
         
-    for i in range(n):
-        if hull[-1] == (None,None):
-            del hull[-1]
 
-    hull.append(hull[0])
     return hull
+
+
+def main():
+    testGift = [(5, 7), (5, 7), (-6, -12), (-14, -14), (9, 9), 
+                (-1, -1), (-10, 11), (-6, 15), (-6, -8), (15, -9),
+                (7, -7), (-2, -9), (6, -5), (0, 14), (2, 8)]
+    hull = jarvisMarch(testGift)
+    
+    print("The points in the hull are:")
+    for point in hull:
+        print(point)
+
+        
+if __name__=="__main__":
+    main()
