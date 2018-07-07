0dd8148ed9148debdc6ee2857df598933ab6cfd1
Unknown <sciencegeek1999@gmail.com>
2018-07-07 11:12:39 -0500

    Move timestep over to solveEuler

--- chapters/differential_equations/euler/code/haskell/euler.hs
+++ chapters/differential_equations/euler/code/haskell/euler.hs
@@ -1,21 +1,20 @@
-solveEuler :: Num a => (a -> a) -> a -> [a]
-solveEuler = iterate
+solveEuler :: Num a => (a -> a) -> a -> a -> [a]
+solveEuler f ts = iterate (\x -> x + f x * ts)
 
 checkResult :: (Ord a, Num a, Num t, Enum t) => a -> (t -> a) -> [a] -> Bool
 checkResult thresh check =
     and . zipWith (\i k -> abs (check i - k) < thresh) [0..]
 
-kinematics :: Double -> Double -> Double
-kinematics timestep x = x - 3 * x * timestep
+kinematics :: Double -> Double
+kinematics x = -3 * x
 
 main :: IO ()
 main =
     let timestep = 0.01
         n = 100
         threshold = 0.01
-        kinematics' = kinematics timestep
-        checkResult' = checkResult threshold $ exp . kinematics'
+        checkResult' = checkResult threshold $ exp . (\x -> x - 3 * x * timestep)
     in  putStrLn $
-        if checkResult' (take n $ solveEuler kinematics' 1)
+        if checkResult' (take n $ solveEuler kinematics timestep 1)
         then "All values within threshold"
         else "Value(s) not in threshold"
