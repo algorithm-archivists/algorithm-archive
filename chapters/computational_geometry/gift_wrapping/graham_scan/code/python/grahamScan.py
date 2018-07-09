a3dada2b3eec11f7d39124f6bd8db249aea844f9
William Boyles <40878829+wmboyles@users.noreply.github.com>
2018-07-09 01:10:38 -0400

    More pep8

--- chapters/computational_geometry/gift_wrapping/graham_scan/code/python/grahamScan.py
+++ chapters/computational_geometry/gift_wrapping/graham_scan/code/python/grahamScan.py
@@ -2,25 +2,25 @@ from math import atan2
 
 
 #Is the turn counter clockwise?
-def counter_Clockwise(p1, p2, p3):
+def counter_clockwise(p1, p2, p3):
     return (p3[1]-p1[1])*(p2[0]-p1[0]) >= (p2[1]-p1[1])*(p3[0]-p1[0])
 
 
 #Find the polar angle of a point relative to a reference point
-def polar_Angle(ref, point):
+def polar_angle(ref, point):
     return atan2(point[1]-ref[1],point[0]-ref[0])
 
 
-def graham_Scan(gift):
-    start = min(gift, key=lambda p: (p[1],p[0])) #Must be in hull
+def graham_scan(gift):
+    start = min(gift, key=lambda p: (p[1], p[0])) #Must be in hull
     gift.remove(start)
 
-    S = sorted(gift,key=lambda point: polar_Angle(start,point))
-    hull = [start,S[0],S[1]]
+    s = sorted(gift,key=lambda point: polar_angle(start, point))
+    hull = [start,s[0],s[1]]
 
     #Remove points from hull that make the hull concave
-    for pt in S[2:]:
-        while not counter_Clockwise(hull[-2],hull[-1],pt):
+    for pt in s[2:]:
+        while not counter_clockwise(hull[-2], hull[-1], pt):
             del hull[-1]
         hull.append(pt)
 
@@ -28,10 +28,10 @@ def graham_Scan(gift):
 
 
 def main():
-    test_Gift = [(-5,2),(5,7),(-6,-12),(-14,-14),(9,9),
-                (-1,-1),(-10,11),(-6,15),(-6,-8),(15,-9),
-                (7,-7),(-2,-9),(6,-5),(0,14),(2,8)]
-    hull = graham_Scan(test_Gift)
+    test_gift = [(-5, 2), (5, 7), (-6, -12), (-14, -14), (9, 9),
+                (-1, -1), (-10, 11), (-6, 15), (-6, -8), (15, -9),
+                (7, -7), (-2, -9), (6, -5), (0, 14), (2, 8)]
+    hull = graham_scan(test_gift)
 
     print("The points in the hull are:")
     for point in hull:
