300dd6479e9405cd0614caa1508bf277af655fdd
v.shvetsov <v.shvetsov@insign.ch>
2018-07-10 09:30:20 +0300

    renamed directory to csharp. Moved algorithm to another file

---
+++ chapters/monte_carlo/code/csharp/MonteCarlo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonteCarlo
+{
+    class MonteCarlo
+    {
+        private static Random random = new Random(47);
+        private readonly int radius = 1;
+
+
+        public MonteCarlo(int radius)
+        {
+            this.radius = radius;
+        }
+
+
+        public double Run(int samples)
+        {
+            int piCount = 0;
+
+            for (int i = 0; i < samples; i++)
+            {
+                var randomX = random.NextDouble() * radius;
+                var randomY = random.NextDouble() * radius;
+
+                if (IsInCircle(randomX, randomY)) piCount++;
+            }
+
+            return 4 * piCount / (double)samples;
+        }
+
+
+        private bool IsInCircle(double x, double y) => x * x + y * y < radius * radius;
+    }
+}
---
+++ chapters/monte_carlo/code/csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MonteCarlo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double piEstimate = new MonteCarlo(1).Run(1000000);
+
+            Console.WriteLine("Pi estimate = {0}", piEstimate);
+            Console.WriteLine("Pi error = {0}", 100 * (Math.Abs(Math.PI - piEstimate)) / Math.PI);
+
+            Console.ReadLine();
+        }
+    }
+}
