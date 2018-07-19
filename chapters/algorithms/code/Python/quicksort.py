12898d792d1d4f36c234ee72da53fba12271c6c1
Marco Porracin <marcoporracin@gmail.com>
2018-07-19 09:47:28 -0300

    Create quicksort.py

---
+++ chapters/algorithms/code/Python/quicksort.py
@@ -0,0 +1,17 @@
+def quicksort(array):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            if x == pivot:
+                equal.append(x)
+            if x > pivot:
+                greater.append(x)
+        return sort(less)+equal+sort(greater)
+    else:
+        return array
