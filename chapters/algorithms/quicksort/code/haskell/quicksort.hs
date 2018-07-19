da1614820a980fd2e1f3e954a5b8698326e16ecb
marcoporracin <marcoporracin@gmail.com>
2018-07-19 09:56:59 -0300

    c

---
+++ chapters/algorithms/quicksort/code/haskell/quicksort.hs
@@ -0,0 +1,6 @@
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted 
---
+++ chapters/algorithms/quicksort/code/javascript/quicksort.js
@@ -0,0 +1,22 @@
+function quicksort(origArray) {
+	if (origArray.length <= 1) { 
+		return origArray;
+	} else {
+
+		var left = [];
+		var right = [];
+		var newArray = [];
+		var pivot = origArray.pop();
+		var length = origArray.length;
+
+		for (var i = 0; i < length; i++) {
+			if (origArray[i] <= pivot) {
+				left.push(origArray[i]);
+			} else {
+				right.push(origArray[i]);
+			}
+		}
+
+		return newArray.concat(quicksort(left), pivot, quicksort(right));
+	}
+}
