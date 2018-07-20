d7372751c8a0c5a6db82d4152e592df7561dd0a9
gorzoid <gorzoid@users.noreply.github.com>
2018-07-20 01:15:43 +0100

    Fixed style to conform with guidelines https://github.com/algorithm-archivists/algorithm-archive/issues/18

--- contents/jarvis_march/code/c++/jarvis_march.cpp
+++ contents/jarvis_march/code/c++/jarvis_march.cpp
@@ -22,11 +22,9 @@ std::vector<Point> jarvis_march(ForwardIter start, ForwardIter end)
     // Left most point
     auto first_point_it = std::min_element(start, end, [](const Point& a, const Point& b){ return a.x < b.x; });
 
-
     auto next_point_it = first_point_it;
     do
     {
-
         hull_points.push_back(*next_point_it);
 
         const Point& p1 = hull_points.back();
@@ -35,9 +33,8 @@ std::vector<Point> jarvis_march(ForwardIter start, ForwardIter end)
         next_point_it = std::max_element(
             start,
             end,
-            [p1](const Point& p2, const Point& p3)
-            {
-                return (p1 == p2) || (p2.x - p1.x) * (p3.y - p1.y) > (p3.x - p1.x) * (p2.y - p1.y); // p2 < p3 if p2 == p3 or p3 is left of the line p1 -> p2
+            [p1](const Point& p2, const Point& p3){
+                return (p1 == p2) || (p2.x - p1.x) * (p3.y - p1.y) > (p3.x - p1.x) * (p2.y - p1.y);
             }
         );
     }
@@ -62,8 +59,7 @@ int main() {
 
     std::cout << "Hull points are:" << std::endl;
 
-    for(const Point& point : hull_points)
-    {
+    for(const Point& point : hull_points) {
         std::cout << '(' << point.x << ", " << point.y << ')' << std::endl;
     }
 }
