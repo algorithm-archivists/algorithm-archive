7d3f676b71c8d4ba18eb9b62135256032403fbeb
Eric Berquist <eric.berquist@gmail.com>
2018-07-21 23:14:22 -0400

    Make messages bold; fix duplicate word

--- tools/deploy/update_site_travis.bash
+++ tools/deploy/update_site_travis.bash
@@ -8,20 +8,23 @@ set -o errexit
 # Required environment variables:
 # - DOCS_BRANCH_NAME: name of the remote branch serving the book
 # - GH_REPO_NAME: name of the remote repo
-# - GH_USER: name of the remote repo's owner
 # - GH_TOKEN: [secret] Personal Access Token
+# - GH_USER: name of the remote repo's owner
+
+bold=$(tput bold)
+normal=$(tput sgr0)
 
 if [ -z ${DOCS_BRANCH_NAME+x} ]; then
-    echo "\$DOCS_BRANCH_NAME is not set!"
+    echo "${bold}\$DOCS_BRANCH_NAME is not set!${normal}"
     exit 1
 elif [ -z ${GH_REPO_NAME+x} ]; then
-    echo "\$GH_REPO_NAME is not set!"
+    echo "${bold}\$GH_REPO_NAME is not set!${normal}"
     exit 1
 elif [ -z ${GH_TOKEN+x} ]; then
-    echo "\$GH_TOKEN is not set!"
+    echo "${bold}\$GH_TOKEN is not set!${normal}"
     exit 1
 elif [ -z ${GH_USER+x} ]; then
-    echo "\$GH_USER is not set!"
+    echo "${bold}\$GH_USER is not set!${normal}"
     exit 1
 fi
 
@@ -34,14 +37,14 @@ GH_REPO_REF="github.com/${GH_USER}/${GH_REPO_NAME}.git"
 # inside the same directory as this script.
 
 if [ -d build ]; then
-    echo "Cloning the website repo..."
+    echo "${bold}Cloning the website repo...${normal}"
     git clone -b $DOCS_BRANCH_NAME https://git@${GH_REPO_REF}
     rm -rf ./"${GH_REPO_NAME}"/*
     cp -a ./build/* ./"${GH_REPO_NAME}"
     pushd ./"${GH_REPO_NAME}"
-    echo "Adding changes..."
+    echo "${bold}Adding changes...${normal}"
     git add --all
-    echo "Committing..."
+    echo "${bold}Committing...${normal}"
     # This will return 1 if there are no changes, which should not
     # result in failure.
     git commit \
@@ -51,7 +54,7 @@ if [ -d build ]; then
     popd
 else
     echo "" >&2
-    echo "Warning: The book wasn't found found!" >&2
-    echo "Warning: Not going to push the book to GitHub!" >&2
+    echo "${bold}Warning: The book wasn't found!${normal}" >&2
+    echo "${bold}Warning: Not going to push the book to GitHub!${normal}" >&2
     exit 1
 fi
