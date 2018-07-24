d5d8c48a66bc069afef657f12f491572793cffaa
Eric Berquist <eric.berquist@gmail.com>
2018-07-24 14:18:19 -0400

    Adjust C code for Verlet according to #257

--- contents/verlet_integration/code/c/verlet.c
+++ contents/verlet_integration/code/c/verlet.c
@@ -1,55 +1,60 @@
 #include <stdio.h>
 
-void verlet(double pos, double acc, double dt) {
-    double prev_pos, temp_pos, time;
+void verlet(double *time, double pos, double acc, double dt) {
+    double prev_pos, temp_pos;
     prev_pos = pos;
-    time = 0;
+    *time = 0.0;
 
     while (pos > 0) {
-        time += dt;
+        (*time) += dt;
         temp_pos = pos;
         pos = pos * 2 - prev_pos + acc * dt * dt;
         prev_pos = temp_pos;
     }
-
-   printf("%f\n", time);
 }
 
-void stormer_verlet(double pos, double acc, double dt) {
-    double prev_pos, temp_pos, time, vel;
+void stormer_verlet(double *time, double *vel, double pos, double acc, double dt) {
+    double prev_pos, temp_pos;
     prev_pos = pos;
-    vel = 0;
-    time = 0;
+    *vel = 0.0;
+    *time = 0.0;
+
     while (pos > 0) {
-        time += dt;
+        (*time) += dt;
         temp_pos = pos;
         pos = pos * 2 - prev_pos + acc * dt * dt;
         prev_pos = temp_pos;
 
-        vel += acc * dt;
+        (*vel) += acc * dt;
     }
-
-   printf("%f\n", time);
 }
 
-void velocity_verlet(double pos, double acc, double dt) {
-    double time, vel;
-    vel = 0;
-    time = 0;
-    while (pos > 0) {
-        time += dt;
-        pos += vel * dt + 0.5 * acc * dt * dt;
-        vel += acc * dt;
-    }
+void velocity_verlet(double *time, double *vel, double pos, double acc, double dt) {
+    *vel = 0.0;
+    *time = 0.0;
 
-   printf("%f\n", time);
+    while (pos > 0) {
+        (*time) += dt;
+        pos += (*vel) * dt + 0.5 * acc * dt * dt;
+        (*vel) += acc * dt;
+    }
 }
 
 int main() {
-    verlet(5.0, -10, 0.01);
-    stormer_verlet(5.0, -10, 0.01);
-    velocity_verlet(5.0, -10, 0.01);
+    double time_v;
+    double time_sv, vel_sv;
+    double time_vv, vel_vv;
+
+    verlet(&time_v, 5.0, -10, 0.01);
+    stormer_verlet(&time_sv, &vel_sv, 5.0, -10, 0.01);
+    velocity_verlet(&time_vv, &vel_vv, 5.0, -10, 0.01);
+
+    printf("Time for Verlet integration is: %lf\n",
+           time_v);
+    printf("Time and velocity for Stormer Verlet integration is: %lf, %lf\n",
+           time_sv, vel_sv);
+    printf("Time and velocity for velocity Verlet integration is: %lf, %lf\n",
+           time_vv, vel_vv);
 
     return 0;
 }
-
