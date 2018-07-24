f94e22021dce5bda08b74dbc42d19da0b530e068
Sumeet Padavala <sumeet.padavala@gmail.com>
2018-07-24 09:04:06 +0530

    Change all variables to snake case

--- contents/monte_carlo_integration/code/python/monte_carlo.py
+++ contents/monte_carlo_integration/code/python/monte_carlo.py
@@ -3,33 +3,34 @@ from random import random
 from math import pi
 
 
-def inCircle(xPos, yPos):
+def in_circle(x_pos, y_pos):
     radius = 1
     # Compute euclidian distance from origin
-    return (xPos * xPos + yPos * yPos) < (radius * radius)
+    return (x_pos * x_pos + y_pos * y_pos) < (radius * radius)
 
 
-def monteCarlo(n):
+def monte_carlo(n):
     """
     Computes PI using the monte carlo method using `n` points 
     """
-    piCount = 0
+    pi_count = 0
 
     for i in range(n):
-        pointX = random()
-        pointY = random()
-        if inCircle(pointX, pointY):
-            piCount += 1
+        x = random()
+        y = random()
+        if in_circle(x, y):
+            pi_count += 1
 
     # This is using a quarter of the unit sphere in a 1x1 box.
     # The formula is pi = (boxLength^2 / radius^2) * (piCount / n), but we
     # are only using the upper quadrant and the unit circle, so we can use
     # 4*piCount/n instead
     # piEstimate = 4*piCount/n
-    piEstimate = 4 * piCount / n
+    pi_estimate = 4 * pi_count / n
     print('Pi is {0:} ({1:.4f}% error)'.format(
-        piEstimate, (pi - piEstimate) / pi * 100))
+        pi_estimate, (pi - pi_estimate) / pi * 100))
 
 
+# If this file was run directly
 if __name__ == "__main__":
-    monteCarlo(100000)
+    monte_carlo(100000)
