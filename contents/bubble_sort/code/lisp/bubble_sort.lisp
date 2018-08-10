2226a8c87c544540d43b3d251b52187286e53157
Trashtalk <trashtalk217@gmail.com>
2018-08-10 19:59:12 +0200

    Refactored: 4 to 2 spaces and renamed things.

--- contents/bubble_sort/code/lisp/bubble_sort.lisp
+++ contents/bubble_sort/code/lisp/bubble_sort.lisp
@@ -1,18 +1,30 @@
-;;;;Bubble sort implementation
+;;;; Bubble sort implementation
 
-;;;Swaps to elements in a list (complexity: O(n))
+;;; Swaps two elements in a list (complexity: O(n))
 (defun swap (lst low high)
-    (let ((list-tail (nthcdr low lst)))
-    (rotatef (car list-tail) (elt list-tail (- high low)))
-    lst))
+  (let ((list-tail (nthcdr low lst)))
+  (rotatef (car list-tail) (elt list-tail (- high low)))
+  lst))
 
-(defun bubble_sort (lst)
-    (dotimes (m (- (length lst) 1) lst)                     ;loop
-        (dotimes (n (- (length lst) (+ 1 m)) lst)           ;loop   
-            (if (> (nth n lst) (nth (+ n 1) lst))           ;if
-            (swap lst n (+ n 1))                            ;then
-            lst))))                                         ;else
+(defun bubble-sort (lst)
+  (dotimes (m (- (length lst) 1) lst)           ;loop
+    (dotimes (n (- (length lst) (+ 1 m)) lst)   ;loop   
+      (if (> (nth n lst) (nth (+ n 1) lst))     ;if
+      (swap lst n (+ n 1))                      ;then
+      lst))))                                   ;else
 
-;;Use of an array insead of a list would be faster
-;;The built-in sort is also quicker (sort (list 5 4 3 2 1))
-(print (bubble_sort (list 5 4 3 2 1)))
+;; Use of an array instead of a list would be faster
+(print 
+  (bubble-sort (list 5 4 3 2 1)))
+(print
+  (bubble-sort (list 1 2 3 3 2 1)))
+
+;; The built-in sort is quicker, see the usage below
+;; quick test
+(assert 
+  (equal  (bubble-sort (list 5 4 3 2 1))
+          (sort '(1 2 3 4 5) #'<)))
+
+(assert 
+  (equal  (bubble-sort (list 1 2 3 3 2 1))
+          (sort '(1 2 3 3 2 1) #'<)))
