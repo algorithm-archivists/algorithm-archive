35365e8615d18b1c4363205ccfd6faadf73cc0ad
Eric Berquist <eric.berquist@gmail.com>
2018-08-12 13:57:25 -0400

    keep results in a struct rather than a pair

--- contents/verlet_integration/code/c++/verlet.cpp
+++ contents/verlet_integration/code/c++/verlet.cpp
@@ -1,8 +1,10 @@
 #include <iomanip>
 #include <iostream>
-#include <utility>
 
-using vpair = std::pair<double, double>;
+struct timestep {
+  double time;
+  double vel;
+};
 
 double verlet(double pos, double acc, double dt) {
 
@@ -19,7 +21,7 @@ double verlet(double pos, double acc, double dt) {
   return time;
 }
 
-vpair stormer_verlet(double pos, double acc, double dt) {
+timestep stormer_verlet(double pos, double acc, double dt) {
 
   double prev_pos = pos;
   double time = 0;
@@ -35,10 +37,10 @@ vpair stormer_verlet(double pos, double acc, double dt) {
     vel += acc * dt;
   }
 
-  return std::make_pair(time, vel);
+  return timestep { time, vel };
 }
 
-vpair velocity_verlet(double pos, double acc, double dt) {
+timestep velocity_verlet(double pos, double acc, double dt) {
 
   double time = 0;
   double vel = 0;
@@ -48,12 +50,10 @@ vpair velocity_verlet(double pos, double acc, double dt) {
     vel += acc * dt;
   }
 
-  return std::make_pair(time, vel);
+  return timestep { time, vel };
 }
 
 int main() {
-  double time, vel;
-
   std::cout << std::fixed << std::setprecision(8);
 
   // Note that depending on the simulation, you might want to have the
@@ -63,21 +63,21 @@ int main() {
   // you might need to also change the acceleration to be read into
   // each of these functions.
 
-  time = verlet(5.0, -10, 0.01);
+  double time = verlet(5.0, -10, 0.01);
   std::cout << "Time for Verlet integration is: " \
             << time << std::endl;
 
-  std::tie(time, vel) = stormer_verlet(5.0, -10, 0.01);
+  timestep timestep_sv = stormer_verlet(5.0, -10, 0.01);
   std::cout << "Time for Stormer Verlet integration is: " \
-            << time << std::endl;
+            << timestep_sv.time << std::endl;
   std::cout << "Velocity for Stormer Verlet integration is: " \
-            << vel << std::endl;
+            << timestep_sv.vel << std::endl;
 
-  std::tie(time, vel) = velocity_verlet(5.0, -10, 0.01);
+  timestep timestep_vv = velocity_verlet(5.0, -10, 0.01);
   std::cout << "Time for velocity Verlet integration is: " \
-            << time << std::endl;
+            << timestep_vv.time << std::endl;
   std::cout << "Velocity for velocity Verlet integration is: " \
-            << vel << std::endl;
+            << timestep_vv.vel << std::endl;
 
   return 0;
 
