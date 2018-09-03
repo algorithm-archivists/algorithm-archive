906cc0b08be57a42ca4f90d68d783e75af0261c0
Patrik Tesarik <patrik@fam-goedecke.de>
2018-09-04 00:50:31 +0200

    Figured the use of functions out

--- contents/monte_carlo_integration/code/fortran/monte_carlo_pi.f90
+++ contents/monte_carlo_integration/code/fortran/monte_carlo_pi.f90
@@ -9,17 +9,22 @@ PROGRAM monte_carlo
 IMPLICIT NONE
 
 INTEGER :: i,n
-REAL(64) :: pos_x,pos_y, r, pi_est, pi_count
+REAL(64) :: pos_x,pos_y, r, pi_est, pi_count, pi_error, pi
 LOGICAL :: in_circle
 
 ! TEST CASE WRITE(*,*) in_circle(0.5d0, 0.5d0, 1d0) ! is true
+pi = 3.1415926535d0
 n = 100000
 pi_count = 0
-      DO i=0,n
-        CALL RANDOM_NUMBER(pos_x)
-        CALL RANDOM_NUMBER(pos_y)
-        
-        IF in_circle(pos_x, pos_y, f)
-        THEN pi_count = pi_count + 1
+    DO i=0,n
+       CALL RANDOM_NUMBER(pos_x)
+       CALL RANDOM_NUMBER(pos_y)
+       
+       IF in_circle(pos_x, pos_y, f)
+       THEN pi_count = pi_count + 1
 
+    pi_est = 4 * pi_count / n
+    pi_error = 100 * (abs(pi_est - pi)/pi)
+    WRITE(*,*) 'The pi estimate is: ', pi_est
+    WRITE(*,*) 'Percent error is: ', pi_error, ' %'
 END PROGRAM monte_carlo
