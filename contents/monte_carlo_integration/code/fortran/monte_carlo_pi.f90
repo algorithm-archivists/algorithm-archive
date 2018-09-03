fb31d586ac087819ddf892fc52396749ba4f51e4
Patrik Tesarik <patrik@fam-goedecke.de>
2018-09-03 22:37:38 +0200

    added further code

--- contents/monte_carlo_integration/code/fortran/monte_carlo_pi.f90
+++ contents/monte_carlo_integration/code/fortran/monte_carlo_pi.f90
@@ -1,8 +1,13 @@
 PROGRAM monte_carlo
 IMPLICIT NONE
 
-REAL(8) :: pos, vel, acl, dt 
+REAL(8) :: pos_x,pos_Y, r
+! BOOL :: in_circle
+! Check if numbers are in circle quadrant
 
+in_circle = FALSE
+IF pos_y ** 2 + pos_x ** 2 less r ** 2
+      THEN in_circle = TRUE
 
 
 END PROGRAM monte_carlo
