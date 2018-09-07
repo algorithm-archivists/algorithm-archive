a5d48102e6c21c07a1da05379be9e2048904d91a
jasmaa <jasonmaa3955@gmail.com>
2018-09-06 20:44:19 -0400

    Debug ref and rref

--- contents/gaussian_elimination/code/python/gaussian_elimination.py
+++ contents/gaussian_elimination/code/python/gaussian_elimination.py
@@ -1,42 +1,52 @@
 import numpy as np
 
 def gaussian_elimination(A):
-    # Step 1: Go by number of pivots with end being the min number of rows or cols
-    for pivot_i in range(min(A.shape[0], A.shape[1])):
+
+    pivot_row = 0
+    
+    # Step 1: Go by column
+    for pivot_col in range(min(A.shape[0], A.shape[1])):
 
         # Step 2: Swap row with highest element in col
-        max_i = np.argmax(abs(A[pivot_i:,pivot_i])) + pivot_i
+        max_i = np.argmax(abs(A[pivot_row:,pivot_col])) + pivot_row
         
-        temp = A[pivot_i,:].copy()
-        A[pivot_i,:] = A[max_i,:]
+        temp = A[pivot_row,:].copy()
+        A[pivot_row,:] = A[max_i,:]
         A[max_i,:] = temp
 
-        if A[pivot_i, pivot_i] == 0:
-            # Skip eval on singular matrix
+        # Skip on singular matrix, not actually a pivot
+        if A[pivot_row, pivot_col] == 0:
             continue
 
-        for row in range(pivot_i+1, A.shape[0]):
+        # Steps 3 & 4: Zero out elements below pivot
+        for r in range(pivot_row+1, A.shape[0]):
             # Step 3: Get fraction
-            frac = -A[row,pivot_i] / A[pivot_i,pivot_i]
+            frac = -A[r,pivot_col] / A[pivot_row,pivot_col]
             # Step 4: Add rows
-            A[row,:] = A[row,:] + frac*A[pivot_i,:]
+            A[r,:] = A[r,:] + frac*A[pivot_row,:]
+
+        pivot_row += 1
+
 
 """
 Assumes A is already ref
 """
 def gauss_jordon_elimination(A):
-    # Find and set each pivot to one via row scaling
+    
     col = 0
+
+    # Scan for pivots
     for row in range(A.shape[0]):
-        while A[row,col] == 0:
+        while col < A.shape[1] and A[row,col] == 0:
             col += 1
-            if col >= A.shape[1]:
-                break
+            
         if col >= A.shape[1]:
             continue
+
+        # Set each pivot to one via row scaling
         A[row,:] = A[row,:] / A[row,col]
 
-        # kill rows above
+        # Zero out elements above pivot
         for r in range(row):
             A[r,:] = A[r,:] - A[r,col] * A[row,:]
 
@@ -44,32 +54,42 @@ def gauss_jordon_elimination(A):
 """
 Assumes A has a unique solution and A in ref
 """
-def back_sub(A):
+def back_substitution(A):
+    
     sol = np.zeros(A.shape[0]).T
+
+    # Go by pivots along diagonal
     for pivot_i in range(A.shape[0]-1, -1, -1):
         s = 0
         for col in range(pivot_i+1, A.shape[1]-1):
             s += A[pivot_i,col] * sol[col]
         sol[pivot_i] = (A[pivot_i,A.shape[1]-1] - s) / A[pivot_i,pivot_i]
+        
     return sol
 
 
-# MAIN
-A = np.matrix('0. 2 1 -8; 1 -2 -3 0; -1 1 2 3')
-#A = np.matrix('1. -2 -6 12; 2 4 12 -17; 1 -4 -12 22')
-print(A)
-print()
+def main():
+    #A = np.matrix('0. 2 1 -8; 1 -2 -3 0; -1 1 2 3')
+    #A = np.matrix('1. -2 -6 12 1; 2 4 12 -17 1; 1 -4 -12 22 1')
+    #A = np.matrix('1 1 1 1 1; 0 1 0 0 0; 0 0 0 0 1; 0 0 0 1 0')
+    A = np.matrix('2. 3 4 6; 1 2 3 4; 3 -4 0 10')
 
-gaussian_elimination(A)
-print("ref")
-print(A)
-print()
+    print("Original")
+    print(A, "\n")
 
-print("back sub")
-print(back_sub(A))
-print()
+    gaussian_elimination(A)
+    print("Gaussian elimination, REF")
+    print(A, "\n")
 
-gauss_jordon_elimination(A)
-print("rref")
-print(A)
-print()
+    print("Back subsitution")
+    print(back_substitution(A))
+    print()
+
+    gauss_jordon_elimination(A)
+    print("Gauss-Jordan, RREF")
+    print(A, "\n")
+
+
+if __name__ == "__main__":
+    main()
+
