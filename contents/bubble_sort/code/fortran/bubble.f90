3e86449900be9d6007506ae341ae17e20f3d7022
depate <gutluft@outlook.com>
2018-09-12 22:50:19 +0200

    init

---
+++ contents/bubble_sort/code/fortran/bubble.f90
@@ -0,0 +1,15 @@
+SUBROUTINE bubblesort(array)
+    IMPLICIT NONE
+    INTEGER :: len_array
+    REAL(8), ALLOCATABLE, DIMENSION(:,:) :: array
+
+
+    for i
+END SUBROUTINE bubblesort
+
+PROGRAM
+    IMPLICIT NONE
+    INTEGER :: cols, rows
+
+    
+END PROGRAMM 
