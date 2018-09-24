1a7ba04d74c8067a3d8d401f3021c704d032feea
depate <gutluft@outlook.com>
2018-09-25 01:12:35 +0200

    finished implementation and docs

--- contents/bubble_sort/code/fortran/bubble.f90
+++ contents/bubble_sort/code/fortran/bubble.f90
@@ -1,19 +1,40 @@
-SUBROUTINE bubblesort(array)
-    IMPLICIT NONE
-    INTEGER                              :: array_length, array_shape
-    REAL(8), ALLOCATABLE, DIMENSION(:,:) :: array
-
-    len_array = size(array)
-
-    DO 
-	
-	END DO
-END SUBROUTINE bubblesort
-
 PROGRAM main
 
-IMPLICIT NONE
-INTEGER :: cols, rows
-
+    IMPLICIT NONE
+    REAL(8), DIMENSION(10) :: A
     
-END PROGRAMM 
+
+    A = (/ 1d0, 3d0, 2d0, 4d0, 5d0, 10d0, 50d0, 7d0, 1.5d0, 0.3d0 /)
+    WRITE(*,*) 'Input vector'
+    WRITE(*,'( F6.2 )') A
+    WRITE(*,*) ' '
+    CALL bubblesort(A)
+
+    WRITE(*,*) 'Output vector'
+    WRITE(*,'(F6.2)') A
+
+CONTAINS
+
+SUBROUTINE bubblesort(array)
+    IMPLICIT NONE
+    INTEGER                              :: array_length, i, j, n
+    REAL(8)                              :: tmp
+    REAL(8), DIMENSION(:), INTENT(INOUT) :: array
+
+    array_length = size(array)
+    n = array_length
+    
+    DO i=1, n
+        DO j=1, n-1
+            IF ( array(j) > array(j+1) ) THEN
+
+                tmp        = array(j+1)
+                array(j+1) = array(j)
+                array(j)   = tmp
+                                
+            END IF
+        END DO
+    END DO
+END SUBROUTINE bubblesort
+    
+END PROGRAM main 
