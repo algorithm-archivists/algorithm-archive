b8839d2f9309c3a7b667287ce5492376460d91e1
Gorzoid <drakex321@gmail.com>
2018-10-01 23:51:23 +0100

    Use tuple style swap
    Forgot lua could do this derp.

--- contents/bubble_sort/code/lua/bubble_sort.lua
+++ contents/bubble_sort/code/lua/bubble_sort.lua
@@ -3,9 +3,7 @@ function bubble_sort(arr)
 	for i = 1,#arr do
 		for j = 1,#arr-1 do
 			if arr[j] > arr[j+1] then
-				local temp = arr[j]
-				arr[j] = arr[j+1]
-				arr[j+1] = temp
+				arr[j], arr[j+1] = arr[j], arr[j+1]
 			end
 		end
 	end
