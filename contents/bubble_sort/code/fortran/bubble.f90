7b9eb97eaafedc1b2f0445a19bc5fa945dd59345
depate <gutluft@outlook.com>
2018-10-02 12:36:46 +0200

    fix that merge thing in bubble.f90 again...

--- contents/bubble_sort/code/fortran/bubble.f90
+++ contents/bubble_sort/code/fortran/bubble.f90
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-SUBROUTINE bubblesort(array)
-    IMPLICIT NONE
-    INTEGER                              :: array_length, array_shape
-    REAL(8), ALLOCATABLE, DIMENSION(:,:) :: array
-
-    len_array = size(array)
-
-    DO 
-	
-	END DO
-END SUBROUTINE bubblesort
-
-PROGRAM main
-
-IMPLICIT NONE
-INTEGER :: cols, rows
-
-    
-END PROGRAMM 
-=======
 PROGRAM main
 
     IMPLICIT NONE
