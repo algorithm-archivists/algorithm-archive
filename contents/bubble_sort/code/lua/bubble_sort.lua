b25e17c8d2ffecd2bdefec54a919773146cd3b6d
Gorzoid <drakex321@gmail.com>
2018-10-03 16:46:35 +0100

    Swap code broken
    Probably should have tested before pushing

--- contents/bubble_sort/code/lua/bubble_sort.lua
+++ contents/bubble_sort/code/lua/bubble_sort.lua
@@ -2,7 +2,7 @@ function bubble_sort(arr)
 	for i = 1,#arr-1 do
 		for j = 1,#arr-1 do
 			if arr[j] > arr[j+1] then
-				arr[j], arr[j+1] = arr[j], arr[j+1]
+				arr[j], arr[j+1] = arr[j+1], arr[j]
 			end
 		end
 	end
