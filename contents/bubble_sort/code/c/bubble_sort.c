441a7be4eb05b80e834569fc923d5e27318a4a14
varunyo <43836307+varunyo@users.noreply.github.com>
2018-10-04 09:24:24 +0530

    made change
    a better and easy C change

--- contents/bubble_sort/code/c/bubble_sort.c
+++ contents/bubble_sort/code/c/bubble_sort.c
@@ -1,35 +1,36 @@
+/* Bubble sort code */
+ 
 #include <stdio.h>
-
-void print_range(int *array, size_t n) {
-    for (size_t i = 0; i < n; ++i) {
-        printf("%d ", array[i]);
+ 
+int main()
+{
+  int array[100], n, c, d, swap;
+ 
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+ 
+  printf("Enter %d integers\n", n);
+ 
+  for (c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+ 
+  for (c = 0 ; c < n - 1; c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
+    {
+      if (array[d] > array[d+1]) /* For decreasing order use < */
+      {
+        swap       = array[d];
+        array[d]   = array[d+1];
+        array[d+1] = swap;
+      }
     }
-    printf("\n");
-}
-
-void bubble_sort(int *array, size_t n) {
-    for (size_t i = 0; i < n; ++i) {
-        for (size_t j = 0; j < n - 1; ++j) {
-            if (array[j] > array[j + 1]) {
-                int tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-            }
-        }
-    }
-}
-
-int main() {
-    int array[] = {1, 45, 756, 4569, 56, 3, 8, 5, -10, -4};
-    size_t N = sizeof(array) / sizeof(*array);
-
-    printf("Unsorted array:\n");
-    print_range(array, N);
-
-    bubble_sort(array, N);
-
-    printf("\nSorted array:\n");
-    print_range(array, N);
-
-    return 0;
+  }
+ 
+  printf("Sorted list in ascending order:\n");
+ 
+  for (c = 0; c < n; c++)
+     printf("%d\n", array[c]);
+ 
+  return 0;
 }
