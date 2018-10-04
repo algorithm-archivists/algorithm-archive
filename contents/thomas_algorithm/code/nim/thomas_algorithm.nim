755cb2c93d2c5a8054e73ea799172468331593f6
Cyrus B <cyrusb252@gmail.com>
2018-10-03 21:41:56 -0400

    Update thomas_algorithm.nim

--- contents/thomas_algorithm/code/nim/thomas_algorithm.nim
+++ contents/thomas_algorithm/code/nim/thomas_algorithm.nim
@@ -1,40 +1,38 @@
-const x: array[3,float] = [0.0,2.0,3.0]
-const y: array[3,float] = [1.0,3.0,6.0]
-const z: array[3,float] = [4.0,5.0,0.0]
-const w: array[3,float] = [7.0,5.0,3.0]
+proc thomas_algorithm(a, b, c_in, d_in: seq[float]): seq[float] = 
 
-const n: int = len(w)
+  let n: int = len(d_in)
 
-proc thomas_algorithm(a,b,c_in,d_in: array[n,float]): array[n,float] = 
-
-    #const n: int = len(d1)
-
-    var c: array[n,float] = c_in
-    var d: array[n,float] = d_in
+  var c: seq[float] = c_in
+  var d: seq[float] = d_in
     
-    c[0] = c[0] / b[0]
-    d[0] = d[0] / b[0]
+  c[0] /= b[0]
+  d[0] /= b[0]
 
     for i in 1..n-1:
-        let scale: float = (1 / (b[i] - c[i-1] * a[i]))
+      let scale: float = (1 / (b[i] - c[i - 1] * a[i]))
 
-        c[i] = c[i] * scale
-        d[i] = (d[i] - a[i] * d[i - 1]) * scale
+      c[i] *= scale
+      d[i] = (d[i] - a[i] * d[i - 1]) * scale
 
     for i in countdown(n-2,0):
-        d[i] -= c[i]*d[i+1]
+      d[i] -= c[i] * d[i + 1]
 
     return d
             
+
+const x: seq[float] = @[0.0, 2.0, 3.0]
+const y: seq[float] = @[1.0, 3.0, 6.0]
+const z: seq[float] = @[4.0, 5.0, 0.0]
+const w: seq[float] = @[7.0, 5.0, 3.0]            
             
 echo "The system,"
 echo "[1.0 4.0 0.0][x] = [7.0]"
-echo "[2.0 3.0 5.0][x] = [5.0]"
-echo "[0.0 3.0 6.0][x] = [3.0]"
+echo "[2.0 3.0 5.0][y] = [5.0]"
+echo "[0.0 3.0 6.0][z] = [3.0]"
 
 echo "has the solution:"
 
-const soln: array[3,float] = thomas_algorithm(x,y,z,w)
+const soln: seq[float] = thomas_algorithm(x,y,z,w)
 
 for i in 0..2:
-    echo soln[i]
+  echo soln[i]
