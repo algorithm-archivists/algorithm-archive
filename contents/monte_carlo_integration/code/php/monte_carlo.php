b411169b3fa3ab47f9b358e69b69a04f4a23acae
Michal Hanajik <pudotta.pommin@gmail.com>
2018-10-04 11:22:38 +0100

    Changed code style

--- contents/monte_carlo_integration/code/php/monte_carlo.php
+++ contents/monte_carlo_integration/code/php/monte_carlo.php
@@ -3,24 +3,24 @@ declare(strict_types=1);
 
 function in_circle(float $positionX, float $positionY, float $radius = 1): bool
 {
-  return pow($positionX, 2) + pow($positionY, 2) < pow(abs($radius), 2);
+    return pow($positionX, 2) + pow($positionY, 2) < pow(abs($radius), 2);
 }
 
 function random_zero_to_one(): float
 {
-  return mt_rand(0, 100) / 100;
+    return mt_rand(0, 100) / 100;
 }
 
 function monte_carlo(int $samples, float $radius = 1): float
 {
-  $in_circle_count = 0;
+    $in_circle_count = 0;
 
-  for ($i = 0; $i < $samples; $i++) {
-    if (in_circle(random_zero_to_one(), random_zero_to_one(), $radius))
-      $in_circle_count++;
-  }
+    for ($i = 0; $i < $samples; $i++) {
+        if (in_circle(random_zero_to_one(), random_zero_to_one(), $radius))
+            $in_circle_count++;
+    }
 
-  return 4 * $in_circle_count / $samples;
+    return 4 * $in_circle_count / $samples;
 }
 
 $piEstimate = monte_carlo(100000000);
