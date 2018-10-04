5b1dd4066f87b91d5b3c6f4b0c9bc68b34515625
Nic Hartley <nxh9052@rit.edu>
2018-10-04 11:37:49 -0700

    Add code

---
+++ contents/euclidean_algorithm/code/ruby/euclidean.rb
@@ -0,0 +1,21 @@
+def gcd_mod(a, b)
+	a, b = b, a%b until b.zero?
+	a
+end
+ 
+def gcd_minus(a, b)
+	until a == b
+		if a > b
+			a -= b
+		else
+			b -= a
+		end
+	end
+	a
+end
+ 
+p gcd_mod(108, 48) #=> 12
+p gcd_mod(6003, 936) #=> 9
+ 
+p gcd_minus(108, 48) #=> 12
+p gcd_minus(6003, 936) #=> 9
