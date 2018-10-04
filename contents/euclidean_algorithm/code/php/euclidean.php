494c1d2615091c401cb40f2b05f8a21b7c400354
Michal Hanajik <pudotta.pommin@gmail.com>
2018-10-04 11:26:21 +0100

    Changed code style

--- contents/euclidean_algorithm/code/php/euclidean.php
+++ contents/euclidean_algorithm/code/php/euclidean.php
@@ -3,26 +3,26 @@ declare(strict_types=1);
 
 function euclid_sub(float $a, float $b): float
 {
-  $a = abs($a);
-  $b = abs($b);
+    $a = abs($a);
+    $b = abs($b);
 
-  while ($a !== $b) {
-    if ($a > $b) $a = $a - $b;
-    else $b = $b - $a;
-  }
+    while ($a !== $b) {
+        if ($a > $b) $a = $a - $b;
+        else $b = $b - $a;
+    }
 
-  return $a;
+    return $a;
 }
 
 function euclid_mod(float $a, float $b): float
 {
-  $a = abs($a);
-  $b = abs($b);
+    $a = abs($a);
+    $b = abs($b);
 
-  while ($b != 0)
-    list($b, $a) = [$a % $b, $b];
+    while ($b != 0)
+        list($b, $a) = [$a % $b, $b];
 
-  return $a;
+    return $a;
 }
 
 echo sprintf('Euclidean mod: %s', euclid_mod(64 * 67, 64 * 81));
