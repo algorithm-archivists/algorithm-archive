af0a9f79aed7dbdf6d5082a20ed75c042d954689
PaddyKe <34421580+PaddyKe@users.noreply.github.com>
2018-10-05 19:04:49 +0200

    added comment explaining the multiplication with four

--- contents/monte_carlo_integration/code/racket/monte_carlo.rkt
+++ contents/monte_carlo_integration/code/racket/monte_carlo.rkt
@@ -18,4 +18,10 @@
         ) n)
   )
 
+
+; In this case, the Monte Carlo algorithm returns the ratio
+; between the area of the unit circle and the area of the enclosing square
+; for one of the four quadrants of the circle.
+; To calculate Pi the result must be multiplied by four.
+; (This step is generally not required. It is only used to approximate Pi.)
 (display (* 4 (monte_carlo 1000)))
