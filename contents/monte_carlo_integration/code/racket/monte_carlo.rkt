dab76f6a57afa22150a3e545645eb3be93836d4f
PaddyKe <34421580+PaddyKe@users.noreply.github.com>
2018-10-06 19:51:57 +0200

    moved multiplication with 4 back and removed comment

--- contents/monte_carlo_integration/code/racket/monte_carlo.rkt
+++ contents/monte_carlo_integration/code/racket/monte_carlo.rkt
@@ -3,24 +3,20 @@
   (< (+ (sqr x) (sqr y)) 1)
  )
 
-(define (monte_carlo n)
-  (/ (local ((define (monte_carlo* n count)
+(define (monte_carlo_pi n)
+  (* (/ (local ((define (monte_carlo* n count)
                   (if (= n 0)
                       count
-                      (monte_carlo* (sub1 n) 
+                      (monte_carlo_pi* (sub1 n) 
                                     (if (in_circle (random) (random)) 
                                         (add1 count)
                                         count
                                     )
                       )
                   )
-               )) (monte_carlo* n 0)
-        ) n)
+               )) (monte_carlo_pi* n 0)
+        ) n) 4)
   )
 
 
-; In this case, the Monte Carlo algorithm returns the ratio
-; between the area of the unit circle and the area of the enclosing square.
-; To calculate Pi using this ratio, it has to be multiplied by four.
-; (This step is generally not required. It is only used to approximate Pi.)
-(display (* 4 (monte_carlo 1000)))
+(display  (monte_carlo_pi 1000))
