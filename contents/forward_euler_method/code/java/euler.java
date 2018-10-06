98d9ee6ae4c3b7fef1e73a2e4be15af081132e99
William103 <39882051+William103@users.noreply.github.com>
2018-10-06 09:16:05 -0700

    Followed suggestions

--- contents/forward_euler_method/code/java/euler.java
+++ contents/forward_euler_method/code/java/euler.java
@@ -1,20 +1,20 @@
 // Submitted by William103
 
-public class ForwardEuler {
+class ForwardEuler {
+	static int n = 100;
+	static double dt = 0.01;
+	static double initial = 1;
+    static double threshold = 0.01;
     public static void main(String[] args) {
-		int n = 100;
-		double dt = 0.01;
-		double initial = 1;
-		double threshold = 0.01;
-        double[] result = forwardEuler(n, dt, initial);
-		if (check(result, dt, threshold)) {
-			System.out.println("All values below threshold!");
-		} else {
-			System.out.println("Value(s) outside of threshold!");
-		}
+        double[] result = forwardEuler();
+		    if (check(result)) {
+			    System.out.println("All values below threshold!");
+		    } else {
+			    System.out.println("Value(s) outside of threshold!");
+		    }
     }
 
-	static double[] forwardEuler(int n, double dt, double initial) {
+	static double[] forwardEuler() {
 		double[] result = new double[n];
 		result[0] = initial;
 		for (int i = 1; i < n; i++) {
@@ -23,7 +23,7 @@ public class ForwardEuler {
 		return result;
 	}
 
-	static boolean check(double[] result, double dt, double threshold) {
+	static boolean check(double[] result) {
 		double solution;
 		for (int i = 0; i < result.length; i++) {
 			solution = Math.exp(-3 * i * dt);
