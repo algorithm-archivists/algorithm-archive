fd1842cca48d79fa2d1f5bef6dbdc005686c9466
depate <gutluft@outlook.com>
2018-10-07 03:40:57 +0200

    Fixed the raised issues from the review

--- contents/forward_euler_method/code/javascript/euler.js
+++ contents/forward_euler_method/code/javascript/euler.js
@@ -1,32 +1,31 @@
-function forward_euler(time_step, n) {
-    var arr = [];
-    arr[0] = 1;
-    for (var i = 1; i <= n; i++) {
-        arr[i] = arr[i - 1] - 3 * arr[i - 1] * time_step;
-    }
-    return arr;
+function forwardEuler(time_step, n) {
+  const arr = [1];
+  for (let i = 1; i <= n; i++) {
+    arr[i] = arr[i - 1] - 3 * arr[i - 1] * time_step;
+  }
+  return arr;
 }
 
-function check_euler(arr, time_step, threshold) {
-    var is_approx = true;
-    for (var i = 1; i <= arr.length; i++) {
-        var solution = Math.exp(-3 * time_step * i);
+function checkEuler(arr, time_step, threshold) {
+  const is_approx = true;
+  arr.forEach(function callback(value, i) {
+    const solution = Math.exp(-3 * time_step * i);
 
-        if (Math.abs(arr[i] - solution) > threshold) {
-            alert(arr[i], solution);
-            is_approx = false;
-        }
+    if (Math.abs(arr[i] - solution) > threshold) {
+      console.log(arr[i], solution);
+      is_approx = false;
     }
-    return is_approx;
+  });
+  return is_approx;
 }
 
 function main() {
-     time_step = 0.01;
-     threshold = 0.01;
-     n = 100;
-    var euler_result = forward_euler(time_step, n);
-    var check_result = check_euler(euler_result, time_step, threshold);
-    alert(check_result);
+  const time_step = 0.01;
+  const threshold = 0.01;
+  const n = 100;
+  var euler_result = forwardEuler(time_step, n);
+  var check_result = checkEuler(euler_result, time_step, threshold);
+  console.log(check_result);
 }
 
 main();
