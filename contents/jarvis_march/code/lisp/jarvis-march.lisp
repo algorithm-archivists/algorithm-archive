ed7ef93217d8062d439f69842fd75c01d56dd00a
Trashtalk <trashtalk217@gmail.com>
2018-10-07 20:05:44 +0200

    Replaced p-eql with built-in equalp and abstracted two functions

--- contents/jarvis_march/code/lisp/jarvis-march.lisp
+++ contents/jarvis_march/code/lisp/jarvis-march.lisp
@@ -2,25 +2,26 @@
 
 (defstruct (point (:constructor make-point (x y))) x y)
 
-(defun p-eql (p1 p2)
-  "Checks if two points are the same"
-  (and (eql (point-x p1) (point-x p2))
-    (eql (point-y p1) (point-y p2))))
+(defun extreme-of (func comp-op list)
+  "Finds the value in a list that evaluates to the highest
+  or lowest value when passed to a function"
+  (if (< (length list) 2)
+      (first list)
+      ;the comp-op is either '>' or '<'
+      (if
+        (funcall comp-op 
+          (funcall func (first list))
+          (funcall func (second list)))
+        (extreme-of func comp-op (cons (first list) (rest (rest list)))) 
+        (extreme-of func comp-op (rest list)))))
 
 (defun leftmost-point (gift)
   "Returns the leftmost point of the gift"
-  (if (< (length gift) 2)
-      (first gift)
-      (if 
-        (< 
-          (point-x (first gift)) 
-          (point-x (second gift)))
-        (leftmost-point 
-          (cons
-            (first gift)
-            (rest (rest gift))))
-        (leftmost-point
-          (rest gift)))))
+  (extreme-of #'point-x #'< gift))
+
+(defun next-point-on-hull (p1 p2 gift)
+  "finds the next point on the convex hull of a gift"
+  (extreme-of (lambda (p3) (angle p1 p2 p3)) #'> gift))
 
 (defun atan2 (y x)
   "Returns the angle based on the origin"
@@ -38,7 +39,7 @@
 
 (defun angle (p1 p2 p3)
   "Returns the angle between three points"
-  (if (or (p-eql p1 p2) (p-eql p2 p3) (p-eql p1 p3))
+  (if (or (equalp p1 p2) (equalp p2 p3) (equalp p1 p3))
       0
       (let*
         ((thetaA 
@@ -54,36 +55,6 @@
           ((< theta 0) (+ theta (* 2 pi)))
           (t theta)))))
 
-(defun leftmost-point (gift)
-  "Returns the leftmost point of a list of points"
-  (if (< (length gift) 2)
-      (first gift)
-      (if 
-        (< 
-          (point-x (first gift)) 
-          (point-x (second gift)))
-        (leftmost-point 
-          (cons
-            (first gift)
-            (rest (rest gift))))
-        (leftmost-point
-          (rest gift)))))
-
-(defun next-point-on-hull (p1 p2 gift)
-  "finds the next point on the convex hull of a gift"
-  (cond 
-    ((null gift) nil) 
-    ((null (rest gift)) (first gift)) 
-    (t 
-      (if 
-        (> 
-          (angle p1 p2 (first gift)) 
-          (angle p1 p2 (second gift))) 
-        (next-point-on-hull p1 p2 
-          (cons 
-            (first gift) 
-            (rest (rest gift)))) 
-        (next-point-on-hull p1 p2 (rest gift)))))) 
 
 (defun jarvis-march (gift)
 "finds the convex hull of any random distribution of points"
