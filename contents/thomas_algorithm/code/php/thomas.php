151ae05e8c32b83971ed3c38cebf63d28a519bae
Michal Hanajik <pudotta.pommin@gmail.com>
2018-10-07 10:53:26 +0100

    Update thomas.php
    changed sprintf to printf

--- contents/thomas_algorithm/code/php/thomas.php
+++ contents/thomas_algorithm/code/php/thomas.php
@@ -25,12 +25,12 @@ $b = [1.0, 3.0, 6.0];
 $c = [4.0, 5.0, 0.0];
 $x = [7.0, 5.0, 3.0];
 
-echo sprintf('The system,%s', PHP_EOL);
-echo sprintf('  [%s, %s, %s][x] = [%s]%s', $b[0], $c[0], 0, $x[0], PHP_EOL);
-echo sprintf('  [%s, %s, %s][y] = [%s]%s', $a[1], $b[1], $c[1], $x[1], PHP_EOL);
-echo sprintf('  [%s, %s, %s][z] = [%s]%s', 0, $a[2], $b[2], $x[2], PHP_EOL);
-echo sprintf('has the solution:%s', PHP_EOL);
+printf('The system,%s', PHP_EOL);
+printf('  [%s, %s, %s][x] = [%s]%s', $b[0], $c[0], 0, $x[0], PHP_EOL);
+printf('  [%s, %s, %s][y] = [%s]%s', $a[1], $b[1], $c[1], $x[1], PHP_EOL);
+printf('  [%s, %s, %s][z] = [%s]%s', 0, $a[2], $b[2], $x[2], PHP_EOL);
+printf('has the solution:%s', PHP_EOL);
 
 $solution = thomas_algorithm($a, $a, $c, $x, count($x));
 for ($i = 0; $i < count($solution); $i++)
-    echo sprintf('  [%s]%s', $solution[$i], PHP_EOL);
+    printf('  [%s]%s', $solution[$i], PHP_EOL);
