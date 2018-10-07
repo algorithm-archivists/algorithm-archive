4a241aa3344641edc48f334a1edca2380d483021
Michal Hanajik <pudotta.pommin@gmail.com>
2018-10-07 15:34:17 +0100

    Styles fixes and typo

--- contents/monte_carlo_integration/code/php/monte_carlo.php
+++ contents/monte_carlo_integration/code/php/monte_carlo.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 function in_circle(float $positionX, float $positionY, float $radius = 1): bool
 {
-    return pow($positionX 2) + pow($positionY, 2) < pow($radius, 2);
+    return pow($positionX, 2) + pow($positionY, 2) < pow($radius, 2);
 }
 
 function random_zero_to_one(): float
@@ -16,16 +16,18 @@ function monte_carlo(int $samples, float $radius = 1): float
     $inCircleCount = 0;
 
     for ($i = 0; $i < $samples; $i++) {
-        if (in_circle(random_zero_to_one() * $radius, random_zero_to_one() * $radius, $radius))
+        if (in_circle(random_zero_to_one() * $radius, random_zero_to_one() * $radius, $radius)) {
             $inCircleCount++;
+        }
     }
 
     return 4 * $inCircleCount / $samples;
 }
 
-$piEstimate = monte_carlo(100000000);
+$piEstimate = monte_carlo(10000000);
 $percentError = abs($piEstimate - pi()) / pi() * 100;
 
 printf('The estimate of PI is: %s', $piEstimate);
 echo PHP_EOL;
 printf('The percent error is: %s', $percentError);
+echo PHP_EOL;
