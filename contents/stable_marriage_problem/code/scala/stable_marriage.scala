26a3271769e6b6c5d189b8416a007d74bbed7f97
Ken Power <ken.power_ext@scout24.com>
2018-10-10 09:14:23 +0200

    refactor implementation details out of main

--- contents/stable_marriage_problem/code/scala/stable_marriage.scala
+++ contents/stable_marriage_problem/code/scala/stable_marriage.scala
@@ -2,8 +2,10 @@ import scala.collection.mutable._
 
 object StableMarriage {
 
-  case class Man(name: String, var preferences: List[Woman] = List()){
-    def propose():Unit = preferences match {
+  var bachelors = ListBuffer[Man]()
+
+  case class Man(name: String, var preferences: List[Woman] = List()) {
+    def propose(): Unit = preferences match {
       case woman :: remainingPreferences => {
         if (woman.prefers(this)) {
           bachelors ++= woman.fiance
@@ -17,20 +19,28 @@ object StableMarriage {
     }
   }
 
-  case class Woman(name: String, var preferences: List[Man] = List(), var fiance: Option[Man] = None){
+  case class Woman(name: String, var preferences: List[Man] = List(), var fiance: Option[Man] = None) {
     def prefers(man: Man): Boolean =
       fiance match {
         case Some(otherMan) => preferences.indexOf(man) < preferences.indexOf(otherMan)
-        case _ => true  //always prefer any man over nobody
+        case _ => true //always prefer any man over nobody
       }
   }
 
-  val a = Man("Adam")
-  val b = Man("Bart")
-  val c = Man("Colm")
-  val x = Woman("Xena")
-  val y = Woman("Yeva")
-  val z = Woman("Zara")
+  def findStableMatches(men: Man*): Unit = {
+    bachelors = men.to[ListBuffer]
+    bachelors foreach (b => b.propose())
+  }
+}
+
+object StableMarriageExample {
+
+  val a = StableMarriage.Man("Adam")
+  val b = StableMarriage.Man("Bart")
+  val c = StableMarriage.Man("Colm")
+  val x = StableMarriage.Woman("Xena")
+  val y = StableMarriage.Woman("Yeva")
+  val z = StableMarriage.Woman("Zara")
 
   a.preferences = List(y, x, z)
   b.preferences = List(y, z, x)
@@ -39,12 +49,16 @@ object StableMarriage {
   y.preferences = List(c, a, b)
   z.preferences = List(a, c, b)
 
-  var bachelors = ListBuffer(a, b, c)
 
   def main(args: Array[String]): Unit = {
 
-    bachelors foreach (b => b.propose())
+    StableMarriage.findStableMatches(a, b, c)
 
-    List(x, y, z).foreach(w => Console.println(w.name + " is married to " + w.fiance.getOrElse(Man("Nobody")).name))
+    List(x, y, z).foreach(
+      w => Console.println(
+        w.name
+          + " is married to "
+          + w.fiance.getOrElse(StableMarriage.Man("Nobody")).name))
   }
+
 }
