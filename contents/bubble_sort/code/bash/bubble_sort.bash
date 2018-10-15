d5c9fbbb154746081713e072c5fcee11a8b6812f
Ariana <1729ariana@gmail.com>
2018-10-15 23:57:46 +0800

    Space/tab inconsistancy

--- contents/bubble_sort/code/bash/bubble_sort.bash
+++ contents/bubble_sort/code/bash/bubble_sort.bash
@@ -9,13 +9,13 @@ bubble_sort() {
     (( len = ${#arr[@]} ))
 
     for ((i = 0; i <= len - 1; i++)); do
-	    for ((j = 0; j <= len - 2; j++)); do
-	        if (( arr[j] > arr[(( j + 1 ))] )); then
-		        (( tmp = arr[(( j + 1 ))] ))
-		        (( arr[(( j + 1 ))] = arr[j] ))
-		        (( arr[j] = tmp ))
-	        fi
-	    done
+        for ((j = 0; j <= len - 2; j++)); do
+            if (( arr[j] > arr[(( j + 1 ))] )); then
+                (( tmp = arr[(( j + 1 ))] ))
+                (( arr[(( j + 1 ))] = arr[j] ))
+                (( arr[j] = tmp ))
+            fi
+        done
     done
     echo ${arr[*]}
 }
@@ -24,4 +24,3 @@ arr=(1 45 756 4569 56 3 8 5 -10 -4)
 echo "Unsorted array: ${arr[*]}"
 tmp=$(bubble_sort "${arr[@]}")
 echo "Sorted array: ${tmp[*]}"
-
