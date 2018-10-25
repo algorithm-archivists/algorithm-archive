8b9c2d12262dcd7142ce49cbe7361b228c5faf91
Sklan <Sklan@users.noreply.github.com>
2018-10-25 21:03:17 +0530

    Create verlet.kt

---
+++ contents/verlet_integration/code/kotlin/verlet.kt
@@ -0,0 +1,55 @@
+data class VerletValues(val time: Double, val vel: Double)
+
+fun verlet(_position: Double, acceleration: Double, dt: Double): Double {
+    var position = _position  // Since function parameter are val and can't be modified
+    var previousPosition = position
+    var time = 0.0
+
+    while (position > 0) {
+        time += dt
+        val nextPosition = position * 2 - previousPosition + acceleration * dt * dt
+        previousPosition = position
+        position = nextPosition
+    }
+    return time
+}
+
+fun stormer_verlet(_position: Double, acceleration: Double, dt: Double): VerletValues {
+    var position = _position
+    var previousPosition = position
+    var time = 0.0
+    var velocity = 0.0
+    while (position > 0) {
+        time += dt
+        val nextPosition = position * 2 - previousPosition + acceleration * dt * dt
+        previousPosition = position
+        position = nextPosition
+        velocity += acceleration * dt
+    }
+    return VerletValues(time, velocity)
+}
+
+fun velocity_verlet(_position: Double, acceleration: Double, dt: Double): VerletValues {
+    var position = _position
+    var time = 0.0
+    var velocity = 0.0
+    while (position > 0) {
+        time += dt
+        position += velocity * dt + 0.5 * acceleration * dt * dt
+        velocity += acceleration * dt
+    }
+    return VerletValues(time, velocity)
+}
+
+fun main(args: Array<String>) {
+    val verletTime = verlet(5.0, -10.0, 0.01)
+    println("Time for Verlet integration is: $verletTime")
+
+    val stormerVerlet = stormer_verlet(5.0, -10.0, 0.01)
+    println("Time for Stormer Verlet integration is: " + stormerVerlet.time)
+    println("Velocity for Stormer Verlet integration is: " + stormerVerlet.vel)
+
+    val velocityVerlet = velocity_verlet(5.0, -10.0, 0.01)
+    println("Time for velocity Verlet integration is: " + velocityVerlet.time)
+    println("Velocity for velocity Verlet integration is: " + velocityVerlet.vel)
+}
