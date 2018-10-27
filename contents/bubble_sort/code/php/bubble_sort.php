329409cc100c0d0a885a5b961b15911f6782f0bd
Michal Hanajik <pudotta.pommin@gmail.com>
2018-10-27 15:47:55 +0100

    PHP unify code style for bubble, bogo and tree traversal (#515)

--- contents/bubble_sort/code/php/bubble_sort.php
+++ contents/bubble_sort/code/php/bubble_sort.php
@@ -1,21 +1,25 @@
 <?php
+declare(strict_types=1);
 
 function bubble_sort(array $arr): array
 {
-  for ($i = 0, $length = count($arr); $i < $length; $i++) {
-    for ($j = 1; $j < $length; $j++) {
-      if ($arr[$j - 1] > $arr[$j]) {
-        $tmp = $arr[$j - 1];
-        $arr[$j - 1] = $arr[$j];
-        $arr[$j] = $tmp;
-      }
+    for ($i = 0, $length = count($arr); $i < $length; $i++) {
+        for ($j = 1; $j < $length; $j++) {
+            if ($arr[$j - 1] > $arr[$j]) {
+                $tmp = $arr[$j - 1];
+                $arr[$j - 1] = $arr[$j];
+                $arr[$j] = $tmp;
+            }
+        }
     }
-  }
-  return $arr;
+
+    return $arr;
 }
 
 $unsorted = [1, 2, 6, 47, 4, 9, 3, 7, 8, 23, 15];
 $bubble_sorted = bubble_sort($unsorted);
 
-echo sprintf('Unsorted: %s%s', implode(',', $unsorted), PHP_EOL);
-echo sprintf('Sorted: %s%s', implode(',', $bubble_sorted), PHP_EOL);
+printf('Unsorted: %s', implode(',', $unsorted));
+echo PHP_EOL;
+printf('Sorted: %s', implode(',', $bubble_sorted));
+echo PHP_EOL;
