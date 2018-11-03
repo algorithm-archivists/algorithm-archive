e309e36faaa68cbbc02cf7d50286c12e14d9fdad
Ariana <1729ariana@gmail.com>
2018-11-03 15:34:10 +0800

    Didnt realize crippled indentation somehow

--- contents/cooley_tukey/code/javascript/fft.js
+++ contents/cooley_tukey/code/javascript/fft.js
@@ -1,115 +1,90 @@
-// This code example makes use of complex.js.
-// You can install it with `npm install complex.js`.
+const complex = require('./complex');
 
-const complex = require('complex.js');
-
-function dft(x) {
-    const len = x.length;
-    const X = []
-
-    for (let i = 0; i < len; ++i) {
-	X[i] = complex.Complex(0,0);
-
-	for (let j = 0; j < len; ++j) {
-		X[i] = X[i].add(complex.Complex(0, -2.0 * Math.PI * i * j / len).exp().mul(x[j]));
-	}
+function dft(X) {
+  const len = X.length;
+  const fft = []
+  for (let i = 0; i < len; ++i) {
+    fft[i] = complex.Complex(0,0);
+    for (let j = 0; j < len; ++j) {
+      fft[i]=fft[i].add(complex.Complex(0, -2.0 * Math.PI * i * j / len).exp().mul(X[j]));
     }
+  }
+  return fft;
+}
 
+function cooleyTukey(X) {
+  const len = X.length;
+  if (len <= 1) {
     return X;
+  }
+  const hlen = Math.floor(len/2); //so we dont have to redo this many times
+
+  const Xeven = [];
+  const Xodd = [];
+  for (let i = 0; i < hlen; ++i) {
+    Xodd[i] = X[2 * i + 1];
+    Xeven[i] = X[2 * i];
+  }
+  const even = cooleyTukey(Xeven);
+  const odd = cooleyTukey(Xodd);
+  const fft = [];
+  for (let i = 0; i < hlen; ++i) {
+    fft[i] = complex.Complex(0, -2 * Math.PI * i / len).exp().mul(odd[i]).add(even[i]);
+    fft[i+hlen] = complex.Complex(0, -2 * Math.PI * i / len).exp().mul(odd[i]).mul(-1).add(even[i]);
+  }
+  return fft;
 }
 
-function cooleyTukey(x) {
-    const len = x.length;
-    if (len <= 1) {
-	return x;
+function bitReverse(X) {
+  const len = X.length;
+  const lglen = Math.floor(Math.log2(len)) - 1;
+  for (let i = 0; i < len; ++i) {
+    let n = i;
+    let a = i;
+    let count = lglen;
+    for (n >>= 1; n > 0; n >>= 1) {
+      a = (a << 1) | (n & 1);
+      --count;
     }
-    const hlen = Math.floor(len/2); //so we dont have to redo this many times
-    
-    let even = [];
-    let odd = [];
-    for (let i = 0; i < hlen; ++i) {
-	odd[i] = x[2 * i + 1];
-	even[i] = x[2 * i];
+    n = (a << count) & ((2 << lglen) - 1);
+    if (n > i) {
+      const tmp = X[i];
+      X[i] = X[n];
+      X[n] = tmp;
     }
-
-    even = cooleyTukey(even);
-    odd = cooleyTukey(odd);
-
-    const X = [];
-    for (let i = 0; i < hlen; ++i) {
-	X[i] = complex.Complex(0, -2 * Math.PI * i / len).exp().mul(odd[i]).add(even[i]);
-	X[i+hlen] = complex.Complex(0, -2 * Math.PI * i / len).exp().mul(odd[i]).mul(-1).add(even[i]);
-    }
-
-    return X;
+  }
+  return X;
 }
 
-function bitReverse(x) {
-    const len = x.length;
-    const lglen = Math.floor(Math.log2(len)) - 1;
+function iterativeCooleyTukey(X) {
+  X = bitReverse(X);
+  const len = X.length;
 
-    for (let i = 0; i < len; ++i) {
-	let n = i;
-	let a = i;
-	let count = lglen;
-
-	for (n >>= 1; n > 0; n >>= 1) {
-	    a = (a << 1) | (n & 1);
-	    --count;
-	}
-	n = (a << count) & ((2 << lglen) - 1);
-
-	if (n > i) {
-	    const tmp = x[i];
-	    x[i] = x[n];
-	    x[n] = tmp;
-	}
+  for (let i = 1; i <= Math.floor(Math.log2(len)); ++i) {
+    const stride = 1 << i;
+    const hstride = 1 << (i - 1);
+    const w = complex.Complex(0, -2 * Math.PI / stride).exp();
+    for (let j = 0; j < len; j += stride) {
+      let v = complex.Complex(1,0);
+      for (let k = 0; k < hstride; ++k) {
+        X[k + j + hstride] = X[k + j].sub(X[k + j + hstride].mul(v));
+        X[k + j] = X[k + j].mul(2).sub(X[k + j + hstride]);
+        v = v.mul(w);
+      }
     }
-
-    return x;
+  }
+  return X;
 }
 
-function iterativeCooleyTukey(x) {
-    x = bitReverse(x);
-    const len = x.length;
-
-    for (let i = 1; i <= Math.floor(Math.log2(len)); ++i) {
-	const stride = 1 << i;
-	const hstride = 1 << (i - 1);
-	const w = complex.Complex(0, -2 * Math.PI / stride).exp();
-
-	for (let j = 0; j < len; j += stride) {
-	    let v = complex.Complex(1,0);
-
-	    for (let k = 0; k < hstride; ++k) {
-		x[k + j + hstride] = x[k + j].sub(x[k + j + hstride].mul(v));
-		x[k + j] = x[k + j].mul(2).sub(x[k + j + hstride]);
-		v = v.mul(w);
-	    }
-	}
-    }
-
-    return x;
+let m=[];
+for(let i=0;i<64;++i) {
+  m[i]=complex.Complex(1/(i+1),0);
 }
-
-const x = [];
-for (let i = 0; i < 64; ++i) {
-    x[i] = complex.Complex(Math.random(),0);
-}
-
-const C = cooleyTukey(x);
-const D = dft(x);
-const I = iterativeCooleyTukey(x);
-
-const bound = 10**-12;
-for (let i = 0; i < 64; ++i) {
-    if (D[i].sub(C[i]).abs() < bound && D[i].sub(I[i]).abs() < bound){
-	console.log("X[" + i + "] = " + C[i].toString());
-    } else {
-	console.log("FFT first differs at index " + i);
-	console.log("Cooley Tukey : " + C[i].toString());
-	console.log("Normal dft : " + D[i].toString());
-	console.log("Iterative Cooley Tukey : " + I[i].toString());
-	break;
-    }
+let C = cooleyTukey(m);
+let D = dft(m);
+let I = iterativeCooleyTukey(m);
+for(let i=0;i<64;++i) {
+  console.log(C[i]);
+  console.log(D[i]);
+  console.log(I[i]);
 }
