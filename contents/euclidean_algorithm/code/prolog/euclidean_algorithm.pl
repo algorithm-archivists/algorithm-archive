48258ee4e5eb8c6a80833c66115bd1ec67d687f7
PaddyKe <34421580+PaddyKe@users.noreply.github.com>
2018-11-30 22:07:47 +0100

    add euclidean algorithm in Prolog

---
+++ contents/euclidean_algorithm/code/prolog/euclidean_algorithm.pl
@@ -0,0 +1,12 @@
+euclid_sub(A, A, A) :- !.
+euclid_sub(A, B, Result) :- A1 is abs(A), B1 is abs(B),
+                            A1 >  B1, !, T is A1 - B1, euclid_sub(T, B, Result).
+euclid_sub(A, B, Result) :- A1 is abs(A), B1 is abs(B),
+                            A1 =< B1, !, T is B1 - A1, euclid_sub(A, T, Result).
+
+euclid_mod(A, 0, A) :- !.
+euclid_mod(A, B, Result) :- A1 is abs(A), B1 is abs(B),
+                            M is mod(A1, B1), euclid_mod(B, M, Result).
+
+test_euclid_sub(Result) :- euclid_sub(4288, 5184, Result). % Result: 64
+test_euclid_mod(Result) :- euclid_mod(1536, 9856, Result). % Result: 128 
