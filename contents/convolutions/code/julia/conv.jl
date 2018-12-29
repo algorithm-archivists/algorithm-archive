dfcb5c63709061d7428540e6f219573dedb92afd
Gorzoid <5949829+Gorzoid@users.noreply.github.com>
2018-12-29 17:26:22 +0000

    Fixed indentation

--- contents/convolutions/code/julia/conv.jl
+++ contents/convolutions/code/julia/conv.jl
@@ -4,11 +4,10 @@ function conv(signal1::AbstractVector{<:Complex}, signal2::Vector{<:Complex})
     [sum(
         signal1[j] * signal2[i-(j-1)]
         for j = 1:i
-	if j in eachindex(signal1) && (i-(j-1)) in eachindex(signal2)
+        if j in eachindex(signal1) && (i-(j-1)) in eachindex(signal2)
     ) for i = 1:n]
 end
 
 function conv_fft(signal1::Vector{<:Complex}, signal2::Vector{<:Complex})
     return ifft(fft(signal1).*fft(signal2))
 end
-
