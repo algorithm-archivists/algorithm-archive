f3e18a9efb162d9f275438f176b728fbf0563cc1
Akash Dhiman <akash.d0407@gmail.com>
2019-07-07 22:48:02 +0530

    improved implementation based on suggestions

--- contents/graham_scan/code/c++/graham_scan.cpp
+++ contents/graham_scan/code/c++/graham_scan.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 struct point{
-  float x;
-  float y;
+  double x;
+  double y;
 };
 
 bool ccw(point a, point b, point c){
@@ -14,45 +14,41 @@ bool ccw(point a, point b, point c){
 std::vector<point> grahamScan(std::vector<point> points){
   //selecting lowest point as pivot
   int lowIndex = 0;
-  for (size_t i = 1; i < points.size(); i++) {
+  for(size_t i = 1; i < points.size(); i++) {
     if(points[i].y < points[lowIndex].y){
       lowIndex = i;
     }
   }
-  std::swap<point>(points[0], points[lowIndex]);
+  std::swap(points[0], points[lowIndex]);
   point pivot = points[0];
 
   //sorting points by polar angle
-  sort(points.begin()+1, points.end(), [pivot](point a, point b){
+  std::sort(points.begin()+1, points.end(), [pivot](point a, point b){
     return ccw(pivot, a, b);
   });
 
   //creating convex hull
   size_t m = 1;
-  for (size_t i = 2; i <= points.size(); i++) {
+  for(size_t i = 2; i <= points.size(); i++) {
     while(ccw(points[m - 1], points[m], points[i]) <= 0){
-      if (m > 1) {
+      if(m > 1) {
                 m--;
                 continue;
-            } else if (i == points.size()) {
+            } else if(i == points.size()) {
                 break;
             } else {
                 i++;
             }
     }
     m++;
-    std::swap<point>(points[i],points[m]);
+    std::swap(points[i], points[m]);
   }
-  std::vector<point>::const_iterator first = points.begin();
-  std::vector<point>::const_iterator last = points.begin()+m;
-  std::vector<point> hull(first, last);
-  return hull;
+  return std::vector<point>(points.begin(), points.begin() + m);
 }
 
 void print(std::vector<point> points){
-  std::cout << "points in hull are as follows: \n";
-  for (size_t i = 0; i < points.size(); i++) {
-    std::cout <<"(" << points[i].x << ", "<< points[i].y <<")\n";
+  for ( auto p : points){
+    std::cout <<"(" << p.x << ", " << p.y <<")\n";
   }
 }
 
@@ -60,7 +56,10 @@ int main(){
   std::vector<point> points = {{-5, 2}, {5, 7}, {-6, -12}, {-14, -14}, {9, 9},
                               {-1, -1}, {-10, 11}, {-6, 15}, {-6, -8}, {15, -9},
                               {7, -7}, {-2, -9}, {6, -5}, {0, 14}, {2, 8}};
+  std::cout << "original points are as follows:\n";
+  print(points);
   std::vector<point> hull = grahamScan(points);
+  std::cout << "points in hull are as follows:\n";
   print(hull);
   return 0;
 }
