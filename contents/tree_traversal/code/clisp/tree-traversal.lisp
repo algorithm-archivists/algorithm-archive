3d3cde5a2f0cc172c7d4724558939fcacbaa50cf
Trashtalk <Trashtalk217@gmail.com>
2019-07-17 16:44:31 +0200

    Added a function for creating trees and refactored a lot

--- contents/tree_traversal/code/clisp/tree-traversal.lisp
+++ contents/tree_traversal/code/clisp/tree-traversal.lisp
@@ -1,95 +1,88 @@
 ;;;; Tree traversal in Common Lisp
 
 (defstruct node data children)
-
+	 
 (defun dfs-recursive (node)
-  "A depth first approach for printing out all values in a tree"
-  (when (not (eql (node-data node) Nil))
+  "A depth first approach for printing out all values in a tree."
+  (when (not (eql (node-data node) nil))
     (print (node-data node)))
   (loop for child in (node-children node)
-    do (dfs-recursive child)))
+     do (dfs-recursive child)))
 
 (defun dfs-recursive-postorder (node)
-  "A depth first approach for printing out all values in a tree starting from the bottom"
+  "A depth first approach for printing out all values in a tree starting from the bottom."
   (loop for child in (node-children node)
-    do (dfs-recursive-postorder child))
-  (when (not (eql (node-data node) Nil))
+     do (dfs-recursive-postorder child))
+  (when (not (eql (node-data node) nil))
     (print (node-data node))))
 
 (defun dfs-recursive-inorder-btree (node)
-  "A depth first search approach for printing all values in a binary tree"
-  (cond
-    ((eql (length (node-children node)) 2)
-      (dfs-recursive-inorder-btree (first (node-children node)))
-      (print (node-data node))
-      (dfs-recursive-inorder-btree (second (node-children node))))
-    ((eql (length (node-children node)) 1)
-      (dfs-recursive-inorder-btree (first (node-children node)))
-      (print (node-data node)))
-    ((eql (length (node-children node)) 0)
-      (print (node-data node)))
+  "A depth first search approach for printing all values in a binary tree."
+  (case (length (node-children node))
+    (2
+     (dfs-recursive-inorder-btree (first (node-children node)))
+     (print (node-data node))
+     (dfs-recursive-inorder-btree (second (node-children node))))
+    (1
+     (dfs-recursive-inorder-btree (first (node-children node)))
+     (print (node-data node)))
+    (0
+     (print (node-data node)))
     (t
-      (print "Invalid binary tree"))))
+     (print "Invalid binary tree."))))
 
 (defun dfs-stack (node)
-  "A depth first approach for printing out all values in a tree using a stack"
+  "A depth first approach for printing out all values in a tree using a stack."
   (let
-    ((stack (list node)) (temp Nil))
-    (loop while (> (length stack) 0) do
-      (print (node-data (first stack)))
-      (setf temp (pop stack))
-      (loop for child in (node-children temp)
-        do (setf stack (nconc stack (list child)))))))
+      ((stack (list node))
+       (temp nil))
+    (loop while (> (length stack) 0)
+       do (print (node-data (first stack)))
+	 (setf temp (pop stack))
+	 (loop for child in (node-children temp)
+	    do (push child stack)))))
 
 (defun bfs-queue (node)
-  "A breadth first search approach for printing out all values in a tree"
-  (let ((queue (list node)) (temp Nil))
-    (loop while (> (length queue) 0) do
-      (print (node-data (first queue)))
-      (setf temp (pop queue))
-      (loop for child in (node-children temp)
-        do (setf queue (nconc queue (list child)))))))
+  "A breadth first search approach for printing out all values in a tree."
+  (let
+      ((queue (list node))
+       (temp nil))
+    (loop while (> (length queue) 0)
+       do (print (node-data (first queue)))
+	 (setf temp (pop queue))
+	 ;; If the queue is empty, the queue should be filled with the children
+	 (if (eql queue nil)
+	     (setf queue (node-children temp))
+	     (nconc queue (node-children temp))))))
 
-;a tree for testing
-(defvar root
-  (make-node :data 0 :children
-    (list
-      (make-node :data 1 :children
-        (list
-          (make-node :data 4)
-          (make-node :data 5)
-          (make-node :data 6)))
-      (make-node :data 2 :children
-        (list
-          (make-node :data 7)
-          (make-node :data 8)
-          (make-node :data 9)))
-      (make-node :data 3 :children
-        (list
-          (make-node :data 10)
-          (make-node :data 11)
-          (make-node :data 12))))))
+(defun make-tree (num-rows num-child)
+  "Creates a simple tree, where every node has 'num-child' children and is 'num-rows' deep."
+  ;; A tree with 0 rows can't be created.
+  (if (eql num-rows 1)
+      (make-node
+       :data 1
+       :children nil)
+      (make-node
+       :data num-rows
+       :children (loop for i upto (1- num-child) collect (make-tree (1- num-rows) num-child)))))
 
-;a binary tree for testing
-(defvar binary-root
-  (make-node :data 0 :children
-    (list
-      (make-node :data 1 :children
-        (list (make-node :data 2) (make-node :data 3)))
-      (make-node :data 4 :children
-        (list (make-node :data 5) (make-node :data 6))))))
+;; A tree for testing
+(defvar tree (make-tree 3 3))
 
-;should print: 0, 1, 4, 5, 6, 2, 7, 8, 9, 3, 10, 11, 12
-(dfs-recursive root)
+;; A binary tree for testing
+(defvar binary-tree (make-tree 3 2))
 
-;should print: 4, 5, 6, 1, 7, 8, 9, 2, 10, 11, 12, 3, 0
-(dfs-recursive-postorder root)
+;; Should print: 3, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1
+(dfs-recursive tree)
 
-;should print: 2, 1, 3, 0, 5, 4, 6
-(dfs-recursive-inorder-btree binary-root)
+;; Should print: 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 3
+(dfs-recursive-postorder tree)
 
-;should print: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-(dfs-stack root)
+;; Should print: 1, 2, 1, 3, 1, 2, 1
+(dfs-recursive-inorder-btree binary-tree)
 
-;should print: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-(bfs-queue root)
+;; Should print: 3, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1
+(dfs-stack tree)
+
+;; Should print: 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1
+(bfs-queue tree)
