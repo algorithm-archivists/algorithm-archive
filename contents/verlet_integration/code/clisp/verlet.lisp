e7d32e873569ae542b901b189db10ad1aa5a6259
Trashtalk <Trashtalk217@gmail.com>
2019-07-19 11:46:52 +0200

    the loop macro is incredible and safed a a couple of lines

--- contents/verlet_integration/code/clisp/verlet.lisp
+++ contents/verlet_integration/code/clisp/verlet.lisp
@@ -4,39 +4,34 @@
   "Finds the time it takes for an object to hit the ground using verlet integration."
   (loop
      with prev-pos = pos
-     with time = 0
+     for time = 0 then (incf time dt)
      while (> pos 0)
-     do (incf time dt)
      ;; The starting speed is assumed to be zero.
-       (psetf
-	    pos (+ (* pos 2) (- prev-pos) (* acc dt dt))
-	    prev-pos pos)
+     do (psetf
+	     pos (+ (* pos 2) (- prev-pos) (* acc dt dt))
+	     prev-pos pos)
      finally (return time)))
 
 (defun stormer-verlet (pos acc dt)
   "Finds the time and velocity when an object hits the ground using the stormer-verlet method."
   (loop
      with prev-pos = pos
-     with time = 0
-     with vel = 0
+     for time = 0 then (incf time dt)
+     for vel = 0 then (incf vel (* acc dt))
      while (> pos 0)
-     do (incf time dt)
      ;; Variables are changed in parallel by 'psetf', so there's no need for a temporary variable.
-       (psetf
+     do (psetf
 	    pos (+ (* pos 2) (- prev-pos) (* acc dt dt))
 	    prev-pos pos)
-       (incf vel (* acc dt))
     finally (return (list time vel))))
 
 (defun velocity-verlet (pos acc dt)
   "Finds the time and velocity when an object hist the ground using the velocity in calculations."
   (loop
-     with time = 0
-     with vel = 0
-     while (> pos 0)
-     do (incf time dt)
-       (incf pos (+ (* vel dt) (* 0.5 acc dt dt)))
-       (incf vel (* acc dt))
+     for time = 0 then (incf time dt)
+     for vel = 0 then (incf vel (* acc dt))
+     for p = pos then (incf p (+ (* vel dt) (* 0.5 acc dt dt)))
+     while (> p 0)
      finally (return (list time vel))))
 
 (format T "Time for Verlet integration: ~d~%" (verlet 5 (- 10) 0.01))
