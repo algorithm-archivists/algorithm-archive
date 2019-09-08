ed4371c3b62cd0b0a67abfe372b7098ea36d7803
Trashtalk <Trashtalk217@gmail.com>
2019-09-08 19:02:52 +0200

    removed unneccesary brackets and redid the indentation

--- contents/verlet_integration/code/clisp/verlet.lisp
+++ contents/verlet_integration/code/clisp/verlet.lisp
@@ -3,43 +3,43 @@
 (defun verlet (pos acc dt)
   "Finds the time it takes for an object to hit the ground using verlet integration."
   (loop
-     with prev-pos = pos
-     for time = 0 then (incf time dt)
-     while (> pos 0)
-     ;; The starting speed is assumed to be zero.
-     do (psetf
-	     pos (+ (* pos 2) (- prev-pos) (* acc dt dt))
-	     prev-pos pos)
-     finally (return time)))
+    with prev-pos = pos
+    for time = 0 then (incf time dt)
+    while (> pos 0)
+    ;; The starting speed is assumed to be zero.
+    do (psetf
+         pos (+ (* pos 2) (- prev-pos) (* acc dt dt))
+         prev-pos pos)
+    finally (return time)))
 
 (defun stormer-verlet (pos acc dt)
   "Finds the time and velocity when an object hits the ground using the stormer-verlet method."
   (loop
-     with prev-pos = pos
-     for time = 0 then (incf time dt)
-     for vel = 0 then (incf vel (* acc dt))
-     while (> pos 0)
-     ;; Variables are changed in parallel by 'psetf', so there's no need for a temporary variable.
-     do (psetf
-	    pos (+ (* pos 2) (- prev-pos) (* acc dt dt))
-	    prev-pos pos)
+    with prev-pos = pos
+    for time = 0 then (incf time dt)
+    for vel = 0 then (incf vel (* acc dt))
+    while (> pos 0)
+    ;; Variables are changed in parallel by 'psetf', so there's no need for a temporary variable.
+    do (psetf
+         pos (+ (* pos 2) (- prev-pos) (* acc dt dt))
+         prev-pos pos)
     finally (return (list time vel))))
 
 (defun velocity-verlet (pos acc dt)
   "Finds the time and velocity when an object hist the ground using the velocity in calculations."
   (loop
-     for time = 0 then (incf time dt)
-     for vel = 0 then (incf vel (* acc dt))
-     for p = pos then (incf p (+ (* vel dt) (* 0.5 acc dt dt)))
-     while (> p 0)
-     finally (return (list time vel))))
+    for time = 0 then (incf time dt)
+    for vel = 0 then (incf vel (* acc dt))
+    for p = pos then (incf p (+ (* vel dt) (* 0.5 acc dt dt)))
+    while (> p 0)
+    finally (return (list time vel))))
 
-(format T "Time for Verlet integration: ~d~%" (verlet 5 (- 10) 0.01))
+(format T "Time for Verlet integration: ~d~%" (verlet 5 -10 0.01))
 
-(defvar stormer-verlet-result (stormer-verlet 5 (- 10) 0.01))
+(defvar stormer-verlet-result (stormer-verlet 5 -10 0.01))
 (format T "Time for Stormer Verlet integration is: ~d~%" (first stormer-verlet-result))
 (format T "Velocity for Stormer Verlet integration is: ~d~%" (second stormer-verlet-result))
 
-(defvar velocity-verlet-result (velocity-verlet 5 (- 10) 0.01))
+(defvar velocity-verlet-result (velocity-verlet 5 -10 0.01))
 (format T "Time for velocity Verlet integration is: ~d~%" (first velocity-verlet-result))
 (format T "Velocity for velocity Verlet integration is: ~d~%" (second velocity-verlet-result))
