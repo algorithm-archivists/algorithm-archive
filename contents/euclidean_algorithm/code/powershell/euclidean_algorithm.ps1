9cd9450d864dd319f1dd5fbfd0515d00b486a757
PaddyKe <34421580+PaddyKe@users.noreply.github.com>
2019-09-28 17:07:14 +0200

    added Euclidean Algorithm in PowerShell

---
+++ contents/euclidean_algorithm/code/powershell/euclidean_algorithm.ps1
@@ -0,0 +1,30 @@
+function Sub-Euclid($a, $b) {
+    $a = [Math]::Abs($a)
+    $b = [Math]::Abs($b)
+
+    while ($a -ne $b) {
+        if ($a -gt $b) {
+            $a = $a - $b
+        } else {
+            $b = $b - $a
+        }
+    }
+    
+    return $a
+}
+
+function Mod-Euclid($a, $b) {
+    $a = [Math]::Abs($a)
+    $b = [Math]::Abs($b)
+
+    while ($b -ne 0) {
+        $tmp = $b
+        $b = $a % $b
+        $a = $tmp
+    }
+
+    return $a
+}
+
+Write-Host $(Mod-Euclid $(64 * 67) $(64 * 81))
+Write-Host $(Sub-Euclid $(128 * 12) $(128 * 77))
