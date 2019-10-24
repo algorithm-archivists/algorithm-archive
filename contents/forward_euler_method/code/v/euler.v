2024cef7e0ee2a4e355657e80f9aefc76131c19f
Julian <jb@futureplay.de>
2019-10-24 14:00:07 +0200

    switch from spaces to tabs

--- contents/forward_euler_method/code/v/euler.v
+++ contents/forward_euler_method/code/v/euler.v
@@ -1,9 +1,9 @@
 import math
 
 fn forward_euler(timestep f64, n int) []f64 {
-    mut res := [f64(0.0)].repeat(n)
-    res[0] = f64(1)
-    for x := 1; x < n; x++ {
+	mut res := [f64(0.0)].repeat(n)
+	res[0] = f64(1)
+	for x := 1; x < n; x++ {
 		res[x] = res[x-1] - 3.0*res[x-1]*timestep
 	}
 	return res
@@ -11,10 +11,10 @@ fn forward_euler(timestep f64, n int) []f64 {
 
 fn check(result []f64, threshold, timestep f64) bool {
 	mut approx := true
-	for x := 0.0; int(x) < result.len; x++ {
-		solution := math.pow(math.E, -3.0 * x * timestep)
-		if math.abs(result[int(x)]-solution) > threshold {
-            tmp := result[int(x)]
+	for x := 0; x < result.len; x++ {
+		solution := math.exp(-3.0 * f64(x) * timestep)
+		if math.abs(result[x]-solution) > threshold {
+			tmp := result[x]
 			println(tmp)
 			println(solution)
 			approx = false
@@ -25,7 +25,7 @@ fn check(result []f64, threshold, timestep f64) bool {
 
 fn main() {
 	timestep := .01
-    threshold := .01
+	threshold := .01
 	n := 100
 
 	result := forward_euler(timestep, n)
