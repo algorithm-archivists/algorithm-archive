ebfcaf7d3d2fd5f76f9907a2af60c1a55f9659ac
Arun_Suresh <31808179+asuresh213@users.noreply.github.com>
2020-03-01 16:58:57 -0800

    Added Comments
    Added some helpful comments to previous Haskell implementation of the Eucledian Algorithm

--- chapters/euclidean_algorithm/code/haskell/euclidean_example.hs
+++ chapters/euclidean_algorithm/code/haskell/euclidean_example.hs
@@ -1,25 +1,34 @@
 -- contributed by Nicole Mazzuca (ubsan)
 
+--Method 1: Euclid's original subtraction algorithm
+
 euclidSub :: Integer -> Integer -> Integer
-euclidSub a b = inner (abs a) (abs b) where
-  inner a b =
-    if a == b then
-      a
-    else if a < b then
-      euclidSub a (b - a)
-    else
-      euclidSub (a - b) b
+euclidSub a b = inner (abs a) (abs b) 
+  where
+    inner x y
+      | x == y = x                      --Exit condition for recursion (seen in next step).
+      | x < y = euclidSub x (y - x)     --Recursion with smaller value and difference b/w values as new inputs
+      | otherwise = euclidSub (x - y) y --Recursion with smaller value and difference b/w values as new inputs
+
+
+-- _____________________________________________________________________________________________
+
+--Method 2: Modern implemetation - The modulus method.
 
 euclidMod :: Integer -> Integer -> Integer
-euclidMod a b = inner (abs a) (abs b) where
-  inner a 0 = a
-  inner a b = inner b (a `mod` b)
+euclidMod a b = inner (abs a) (abs b)   
+  where
+    inner x 0 = x                       --Exit condition for recursion (seen in next step).
+    inner x y = inner y (x `mod` y)     --Recursion with b and (a mod b) as new inputs 
+                                                -- (with a, b being old inputs)
+
+--___________________________________________________________________________________________
+
+--  Examples
 
 main :: IO ()
 main = do
   let chk1 = euclidMod (64 * 67) (64 * 81)
       chk2 = euclidSub (128 * 12) (128 * 77)
-  putStrLn (show chk1)
-  putStrLn (show chk2)
-  return ()
-
+  print chk1
+  print chk2
