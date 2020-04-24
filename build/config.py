ee37483b0805ac2d36019a0c119247cf851a8dd6
Trashtalk <Trashtalk217@gmail.com>
2020-04-24 21:57:35 +0200

    cleaned up the config file

--- build/config.py
+++ build/config.py
@@ -1,25 +1,14 @@
 """ Default configuration file for aaa-py. """
 
-
 O_NAME = "website"
 CONTENTS_NAME = "contents"
 INDEX_NAME = "contents/README.md"
 SUMMARY_NAME = "SUMMARY.md"
 AAA_CLONE_PATH = "contents"
 AAA_PATH = "contents"
-AAA_ORIGIN = "git://github.com/algorithm-archivists/algorithm-archive.git"
 CONTENTS_PATH = "contents"
 AAA_README = "README.md"
 AAA_SUMMARY = "SUMMARY.md"
-AAA_REPO_PATH = "algorithm-archive-master"
-IMPORT_FILES = {
-    "SUMMARY.md": "SUMMARY.md",
-    "README.md": "README.md",
-    "contents": "contents",
-    "literature.bib": "literature.bib",
-    "book.json": "book.json",
-    "redirects.json": "redirects.json"
-}
 EXT = [
     "fenced_code",
     "codehilite",
