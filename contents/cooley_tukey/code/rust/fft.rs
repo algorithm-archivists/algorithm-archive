5a96451469a80955431eae73aaef44e2f090667c
fanninpm <fanninpm@miamioh.edu>
2020-05-06 15:05:06 -0400

    Finished FFT implementation in Rust

--- contents/cooley_tukey/code/rust/fft.rs
+++ contents/cooley_tukey/code/rust/fft.rs
@@ -5,24 +5,27 @@ use num::complex::Complex;
 use rand::prelude::*;
 use std::f64::consts::PI;
 
-fn dft(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+// This is based on the Python implementation.
+
+fn dft(x: &[Complex<f64>]) -> Vec<Complex<f64>> {
     let n = x.len();
-    // TODO: replace this with something that uses an iterable
-    let mut temp = vec![Complex::new(0.0_f64, 0.0_f64); n];
-    for i in 0..n {
-        for k in 0..n {
-            temp[i] += x[k]
-                * (Complex::new(0.0_f64, -2.0_f64) * PI * (i as f64) * (k as f64) / (n as f64))
-                    .exp();
-        }
-    }
-    temp
+    (0..n)
+        .map(|i| {
+            (0..n)
+                .map(|k| {
+                    x[k] * (Complex::new(0.0_f64, -2.0_f64) * PI * (i as f64) * (k as f64)
+                        / (n as f64))
+                        .exp()
+                })
+                .sum()
+        })
+        .collect::<Vec<_>>()
 }
 
-fn cooley_tukey(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+fn cooley_tukey(x: &[Complex<f64>]) -> Vec<Complex<f64>> {
     let n = x.len();
     if n <= 1 {
-        return x.clone();
+        return x.to_owned();
     }
     let even = cooley_tukey(&x.iter().step_by(2).cloned().collect::<Vec<_>>());
     let odd = cooley_tukey(&x.iter().skip(1).step_by(2).cloned().collect::<Vec<_>>());
@@ -37,14 +40,13 @@ fn cooley_tukey(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
     temp
 }
 
-fn bit_reverse(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+fn bit_reverse(x: &[Complex<f64>]) -> Vec<Complex<f64>> {
     let n = x.len();
     let mut temp = vec![Complex::new(0.0_f64, 0.0_f64); n];
     for k in 0..n {
         let b: usize = (0..((n as f64).log2() as usize))
-            .into_iter()
-            .filter(|i| (k << i & 1) != 0)
-            .map(|i| 1 << (((n as f64).log2()) as usize) - 1 - i)
+            .filter(|i| k >> i & 1 != 0)
+            .map(|i| 1 << ((((n as f64).log2()) as usize) - 1 - i))
             .sum();
         temp[k] = x[b];
         temp[b] = x[k];
@@ -52,7 +54,7 @@ fn bit_reverse(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
     temp
 }
 
-fn iterative_cooley_tukey(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+fn iterative_cooley_tukey(x: &[Complex<f64>]) -> Vec<Complex<f64>> {
     let n = x.len();
 
     let mut new_x = bit_reverse(x);
@@ -60,13 +62,14 @@ fn iterative_cooley_tukey(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
     for i in 1..=((n as f64).log2() as usize) {
         let stride = 2_u128.pow(i as u32);
         let w = (Complex::new(0.0_f64, -2.0_f64) * PI / (stride as f64)).exp();
-        for j in (0..n).into_iter().step_by(stride as usize) {
+        for j in (0..n).step_by(stride as usize) {
             let mut v = Complex::new(1.0_f64, 0.0_f64);
             for k in 0..((stride / 2) as usize) {
-                new_x[k + j + ((stride / 2) as usize)] =
-                    new_x[k + j] - v * new_x[k + j + ((stride / 2) as usize)];
-                new_x[k + j] -=
-                    new_x[k + j + ((stride / 2) as usize)].clone() - new_x[k + j].clone();
+                let k_plus_j = new_x[k + j];
+                let k_plus_j_plus_half_stride = new_x[k + j + ((stride / 2) as usize)];
+                new_x[k + j + ((stride / 2) as usize)] = k_plus_j - v * k_plus_j_plus_half_stride;
+                let k_plus_j_plus_half_stride_2 = new_x[k + j + ((stride / 2) as usize)];
+                new_x[k + j] -= k_plus_j_plus_half_stride_2 - k_plus_j;
                 v *= w;
             }
         }
@@ -77,19 +80,18 @@ fn iterative_cooley_tukey(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
 
 fn main() {
     let mut x = Vec::with_capacity(64);
+    let mut rng = thread_rng();
     for _i in 0..64 {
-        let real: f64 = random();
+        let real = rng.gen_range(0.0_f64, 1.0_f64);
         x.push(Complex::new(real, 0.0_f64));
     }
-    let y = cooley_tukey(&x);
-    let z = iterative_cooley_tukey(&x);
-    let t = dft(&x);
+    let y = cooley_tukey(&x.clone());
+    let z = iterative_cooley_tukey(&x.clone());
+    let t = dft(&x.clone());
 
     println!(
         "{}",
-        y.iter()
-            .zip(z.into_iter())
-            .all(|i| (i.0 - i.1).norm() < 1.0)
+        y.iter().zip(z.iter()).all(|i| (i.0 - i.1).norm() < 1.0)
     );
     println!(
         "{}",
