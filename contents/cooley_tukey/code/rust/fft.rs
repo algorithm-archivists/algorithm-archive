6dd00c5585b64cceeb4d4a618f7fbf7f053b89b4
fanninpm <fanninpm@miamioh.edu>
2020-05-06 12:46:32 -0400

    Implement FFT in Rust
    This code is incomplete right now. (It hasn't resolved all compiler
    warnings/errors, and I haven't put it through Clippy yet.)

---
+++ contents/cooley_tukey/code/rust/fft.rs
@@ -0,0 +1,100 @@
+extern crate num;
+extern crate rand;
+
+use num::complex::Complex;
+use rand::prelude::*;
+use std::f64::consts::PI;
+
+fn dft(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+    let n = x.len();
+    // TODO: replace this with something that uses an iterable
+    let mut temp = vec![Complex::new(0.0_f64, 0.0_f64); n];
+    for i in 0..n {
+        for k in 0..n {
+            temp[i] += x[k]
+                * (Complex::new(0.0_f64, -2.0_f64) * PI * (i as f64) * (k as f64) / (n as f64))
+                    .exp();
+        }
+    }
+    temp
+}
+
+fn cooley_tukey(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+    let n = x.len();
+    if n <= 1 {
+        return x.clone();
+    }
+    let even = cooley_tukey(&x.iter().step_by(2).cloned().collect::<Vec<_>>());
+    let odd = cooley_tukey(&x.iter().skip(1).step_by(2).cloned().collect::<Vec<_>>());
+
+    let mut temp = vec![Complex::new(0.0_f64, 0.0_f64); n];
+    for k in 0..(n / 2) {
+        temp[k] = even[k]
+            + (Complex::new(0.0_f64, -2.0_f64) * PI * (k as f64) / (n as f64)).exp() * odd[k];
+        temp[k + n / 2] = even[k]
+            - (Complex::new(0.0_f64, -2.0_f64) * PI * (k as f64) / (n as f64)).exp() * odd[k];
+    }
+    temp
+}
+
+fn bit_reverse(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+    let n = x.len();
+    let mut temp = vec![Complex::new(0.0_f64, 0.0_f64); n];
+    for k in 0..n {
+        let b: usize = (0..((n as f64).log2() as usize))
+            .into_iter()
+            .filter(|i| (k << i & 1) != 0)
+            .map(|i| 1 << (((n as f64).log2()) as usize) - 1 - i)
+            .sum();
+        temp[k] = x[b];
+        temp[b] = x[k];
+    }
+    temp
+}
+
+fn iterative_cooley_tukey(x: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+    let n = x.len();
+
+    let mut new_x = bit_reverse(x);
+
+    for i in 1..=((n as f64).log2() as usize) {
+        let stride = 2_u128.pow(i as u32);
+        let w = (Complex::new(0.0_f64, -2.0_f64) * PI / (stride as f64)).exp();
+        for j in (0..n).into_iter().step_by(stride as usize) {
+            let mut v = Complex::new(1.0_f64, 0.0_f64);
+            for k in 0..((stride / 2) as usize) {
+                new_x[k + j + ((stride / 2) as usize)] =
+                    new_x[k + j] - v * new_x[k + j + ((stride / 2) as usize)];
+                new_x[k + j] -=
+                    new_x[k + j + ((stride / 2) as usize)].clone() - new_x[k + j].clone();
+                v *= w;
+            }
+        }
+    }
+
+    new_x
+}
+
+fn main() {
+    let mut x = Vec::with_capacity(64);
+    for _i in 0..64 {
+        let real: f64 = random();
+        x.push(Complex::new(real, 0.0_f64));
+    }
+    let y = cooley_tukey(&x);
+    let z = iterative_cooley_tukey(&x);
+    let t = dft(&x);
+
+    println!(
+        "{}",
+        y.iter()
+            .zip(z.into_iter())
+            .all(|i| (i.0 - i.1).norm() < 1.0)
+    );
+    println!(
+        "{}",
+        y.iter()
+            .zip(t.into_iter())
+            .all(|i| (i.0 - i.1).norm() < 1.0)
+    );
+}
