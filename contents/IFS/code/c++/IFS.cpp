f20b318924e9f7c41b45e4fcbd438a0137af35f4
Vincent Zalzal <vincent.zalzal@gmail.com>
2020-05-21 21:30:08 -0400

    Simpler version, no template

--- contents/IFS/code/c++/IFS.cpp
+++ contents/IFS/code/c++/IFS.cpp
@@ -11,40 +11,45 @@ Point operator+(Point lhs, Point rhs) { return {lhs.x + rhs.x, lhs.y + rhs.y}; }
 Point operator*(double k, Point pt) { return {k * pt.x, k * pt.y}; }
 Point operator*(Point pt, double k) { return k * pt; }
 
-template <typename RandEngine>
-std::vector<Point> chaosGame(
-    int numOutputPoints,
-    const std::vector<Point>& inputPoints,
-    RandEngine& engine) {
+using PointVector = std::vector<Point>;
 
+std::default_random_engine& rng() {
+  // Initialize static pseudo-random engine with non-deterministic random seed
+  static std::default_random_engine randEngine(std::random_device{}());
+  return randEngine;
+}
+
+double drand() {
+  return std::uniform_real_distribution<double>(0.0, 1.0)(rng());
+}
+
+Point chooseRandPoint(const PointVector& points) {
+  auto index =
+      std::uniform_int_distribution<std::size_t>(0, points.size() - 1)(rng());
+  return points[index];
+}
+
+PointVector chaosGame(int numOutputPoints, const PointVector& inputPoints) {
   // Choose first point randomly
-  std::uniform_real_distribution<double> doubleDist(0.0, 1.0);
-  Point curPoint = {doubleDist(engine), doubleDist(engine)};
-
-  // Prepare local function to choose randomly from input points
-  std::uniform_int_distribution<std::size_t> intDist(0, inputPoints.size() - 1);
-  auto randInputPoint = [&] { return inputPoints[intDist(engine)]; };
+  Point curPoint = {drand(), drand()};
 
   // For each output point, compute midpoint to random input point
-  std::vector<Point> outputPoints(numOutputPoints);
+  PointVector outputPoints(numOutputPoints);
   for (auto& outPoint : outputPoints) {
     outPoint = curPoint;
-    curPoint = 0.5 * (curPoint + randInputPoint());
+    curPoint = 0.5 * (curPoint + chooseRandPoint(inputPoints));
   }
 
   return outputPoints;
 }
 
 int main() {
-  // Initialize pseudo-random engine with non-deterministic random seed
-  std::default_random_engine randEngine(std::random_device{}());
-
   // This will generate a Sierpinski triangle with a chaos game of n points for
   // an initial triangle with three points on the vertices of an equilateral
   // triangle.
-  std::vector<Point> inputPoints = {
+  PointVector inputPoints = {
       {0.0, 0.0}, {0.5, std::sqrt(0.75)}, {1.0, 0.0}};
-  auto outputPoints = chaosGame(10000, inputPoints, randEngine);
+  auto outputPoints = chaosGame(10000, inputPoints);
 
   // It will output the file sierpinski.dat, which can be plotted after
   std::ofstream ofs("sierpinski.dat");
