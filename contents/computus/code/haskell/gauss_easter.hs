d75f37fbc8d67eefbec3bd2650ee5d887d614d91
Jérémie Gillet <jie.gillet@gmail.com>
2020-05-27 11:48:29 +0900

    Adding type annotations and format with ormolu

--- contents/computus/code/haskell/gauss_easter.hs
+++ contents/computus/code/haskell/gauss_easter.hs
@@ -2,60 +2,51 @@ data Mode = Servois | Easter
 
 computus :: Mode -> Int -> String
 computus mode year =
-    case mode of
-        Servois ->  
-            -- Value for Servois' table
-            show $ (21 + d) `mod` 31
-
-        Easter ->
-            -- Determination of the correct month for Easter
-            if 22+ d + f > 31
-                then "April " ++ show (d + f - 9)
-                else "March " ++ show (22 + d + f)
+  case mode of
+    Servois ->
+      -- Value for Servois' table
+      show $ (21 + d) `mod` 31
+    Easter ->
+      -- Determination of the correct month for Easter
+      if 22 + d + f > 31
+        then "April " ++ show (d + f - 9)
+        else "March " ++ show (22 + d + f)
   where
     -- Year's position on the 19 year metonic cycle
-    a = year `mod` 19
-    
+    a = year `mod` 19 :: Int
     -- Century index
-    k = year `div` 100
-
+    k = year `div` 100 :: Int
     -- Shift of metonic cycle, add a day offset every 300 years
-    p = (13 + 8 * k) `div`  25 
-
+    p = (13 + 8 * k) `div` 25 :: Int
     -- Correction for non-observed leap days
-    q = k `div` 4
-
+    q = k `div` 4 :: Int
     -- Correction to starting point of calculation each century
-    m = (15 - p + k - q) `mod` 30
-
+    m = (15 - p + k - q) `mod` 30 :: Int
     -- Number of days from March 21st until the full moon
-    d = (19 * a + m) `mod` 30
-
+    d = (19 * a + m) `mod` 30 :: Int
     -- Finding the next Sunday
     -- Century-based offset in weekly calculation
-    n = (4 + k - q) `mod` 7
-
+    n = (4 + k - q) `mod` 7 :: Int
     -- Correction for leap days
-    b = year `mod` 4
-    c = year `mod` 7
-
+    b = year `mod` 4 :: Int
+    c = year `mod` 7 :: Int
     -- Days from d to next Sunday
-    e = (2 * b + 4 * c + 6 * d + n) `mod` 7
-
+    e = (2 * b + 4 * c + 6 * d + n) `mod` 7 :: Int
     -- Historical corrections for April 26 and 25
-    f = if (d == 29 && e == 6) || (d == 28 && e == 6 && a > 10)
-        then -1
-        else e
+    f =
+      if (d == 29 && e == 6) || (d == 28 && e == 6 && a > 10)
+        then -1 :: Int
+        else e :: Int
 
 -- Here, we will output the date of the Paschal full moon
 -- (using Servois notation), and Easter for 2020-2030
 main :: IO ()
-main = do 
-    let years = [2020..2030]
-        servoisNumbers = map (computus Servois) years
-        easterDates = map (computus Easter) years
-    putStrLn "The following are the dates of the Paschal full moon (using Servois notation) and the date of Easter for 2020-2030 AD:"
-    putStrLn "Year\tServois number\tEaster"
-    let conc y s e = show y ++ "\t" ++ s ++ "\t\t" ++ e
-    mapM_ putStrLn $ zipWith3 conc years servoisNumbers easterDates
-
+main = do
+  let years = [2020 .. 2030] :: [Int]
+      servoisNumbers = map (computus Servois) years :: [String]
+      easterDates = map (computus Easter) years :: [String]
+  putStrLn "The following are the dates of the Paschal full moon (using Servois notation) and the date of Easter for 2020-2030 AD:"
+  putStrLn "Year\tServois number\tEaster"
+  let conc :: Int -> String -> String -> String
+      conc y s e = show y ++ "\t" ++ s ++ "\t\t" ++ e
+  mapM_ putStrLn $ zipWith3 conc years servoisNumbers easterDates
