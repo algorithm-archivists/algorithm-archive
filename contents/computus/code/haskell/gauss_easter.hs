b58aebbd881e211d1e553cfcdd4dc9adcaffdec9
Jérémie Gillet <jie.gillet@gmail.com>
2020-05-30 10:32:43 +0900

    Type signature on seperate line

--- contents/computus/code/haskell/gauss_easter.hs
+++ contents/computus/code/haskell/gauss_easter.hs
@@ -12,39 +12,42 @@ computus mode year =
         then "April " ++ show (d + f - 9)
         else "March " ++ show (22 + d + f)
   where
+    a, b, c, d, e, f, k, m, n, p, q :: Int
     -- Year's position on the 19 year metonic cycle
-    a = year `mod` 19 :: Int
+    a = year `mod` 19
     -- Century index
-    k = year `div` 100 :: Int
+    k = year `div` 100
     -- Shift of metonic cycle, add a day offset every 300 years
-    p = (13 + 8 * k) `div` 25 :: Int
+    p = (13 + 8 * k) `div` 25
     -- Correction for non-observed leap days
-    q = k `div` 4 :: Int
+    q = k `div` 4
     -- Correction to starting point of calculation each century
-    m = (15 - p + k - q) `mod` 30 :: Int
+    m = (15 - p + k - q) `mod` 30
     -- Number of days from March 21st until the full moon
-    d = (19 * a + m) `mod` 30 :: Int
+    d = (19 * a + m) `mod` 30
     -- Finding the next Sunday
     -- Century-based offset in weekly calculation
-    n = (4 + k - q) `mod` 7 :: Int
+    n = (4 + k - q) `mod` 7
     -- Correction for leap days
-    b = year `mod` 4 :: Int
-    c = year `mod` 7 :: Int
+    b = year `mod` 4
+    c = year `mod` 7
     -- Days from d to next Sunday
-    e = (2 * b + 4 * c + 6 * d + n) `mod` 7 :: Int
+    e = (2 * b + 4 * c + 6 * d + n) `mod` 7
     -- Historical corrections for April 26 and 25
     f =
       if (d == 29 && e == 6) || (d == 28 && e == 6 && a > 10)
-        then -1 :: Int
-        else e :: Int
+        then -1
+        else e
 
 -- Here, we will output the date of the Paschal full moon
 -- (using Servois notation), and Easter for 2020-2030
 main :: IO ()
 main = do
-  let years = [2020 .. 2030] :: [Int]
-      servoisNumbers = map (computus Servois) years :: [String]
-      easterDates = map (computus Easter) years :: [String]
+  let years :: [Int]
+      years = [2020 .. 2030]
+      servoisNumbers, easterDates :: [String]
+      servoisNumbers = map (computus Servois) years
+      easterDates = map (computus Easter) years
   putStrLn "The following are the dates of the Paschal full moon (using Servois notation) and the date of Easter for 2020-2030 AD:"
   putStrLn "Year\tServois number\tEaster"
   let conc :: Int -> String -> String -> String
