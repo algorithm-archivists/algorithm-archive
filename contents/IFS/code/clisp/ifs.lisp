f233beb9d6c585bc54426789ac73941a45afd0e0
Trashtalk <Trashtalk217@gmail.com>
2020-07-04 13:11:29 +0200

    cleaned up the chaos-game function and added some helpfull comments

--- contents/IFS/code/clisp/ifs.lisp
+++ contents/IFS/code/clisp/ifs.lisp
@@ -2,27 +2,25 @@
 
 (defstruct (point (:constructor make-point (x y))) x y)
 
-;; not a very good function some tail-end recursion would be kinda sexy here
-(defun chaos-game (n shape-points)
+(defun chaos-game (iterations shape-points)
   (loop
-    with point = (make-point (random 1.0) (random 1.0))
-    with result = (list)
-    for i from 1 to n do
-    (push point result)
-    (let ((rand-point (aref shape-points (random (length shape-points)))))
-      (setf point
-        (make-point
-          (* 0.5 (+ (point-x rand-point) (point-x point)))
-          (* 0.5 (+ (point-y rand-point) (point-y point))))))
-    finally (return result)))
+    repeat iterations
+    for rand-point = (svref shape-points (random (length shape-points)))
+    for point = (make-point (random 1.0) (random 1.0)) ; starting point
+    then (make-point
+           (* 0.5 (+ (point-x rand-point) (point-x point)))
+           (* 0.5 (+ (point-y rand-point) (point-y point)))) ; every subsequent point
+    collect point))
 
 (defparameter *shape-points*
   (map
     'vector
     (lambda (e) (apply #'make-point e))
-    `((0 0) (0.5 ,(sqrt 0.75)) (1 0)))) ; this is basically magic
+    ;; the backquote allows us to selectively evaluate (sqrt 0.75) with the comma
+    `((0 0) (0.5 ,(sqrt 0.75)) (1 0))))
 
+;; output the data to the "out.dat" file
 (with-open-file (out "out.dat" :direction :output :if-exists :supersede)
   (flet ((format-point (p)
            (format nil "~f~c~f" (point-x p) #\tab (point-y p)))) ; this is a bit ugly, but it works
-    (format out "~{~a~%~}" (map 'list #'format-point (chaos-game 1000 *shape-points*)))))
+    (format out "~{~a~%~}" (map 'list #'format-point (chaos-game 1000000 *shape-points*)))))
