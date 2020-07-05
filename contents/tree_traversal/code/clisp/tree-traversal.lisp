bc74f7a06168f909de124578404363696d91f8c3
Trashtalk <Trashtalk217@gmail.com>
2020-07-05 18:57:18 +0200

    picked the simple solution, refactored code slightly

--- contents/tree_traversal/code/clisp/tree-traversal.lisp
+++ contents/tree_traversal/code/clisp/tree-traversal.lisp
@@ -4,16 +4,16 @@
 
 (defun dfs-recursive (node)
   "A depth first approach for printing out all values in a tree."
-  (when (not (eql (node-data node) nil))
+  (when (node-data node)
     (format t "~a " (node-data node)))
-  (loop for child in (node-children node)
-     do (dfs-recursive child)))
+  (loop for child in (node-children node) do
+    (dfs-recursive child)))
 
 (defun dfs-recursive-postorder (node)
   "A depth first approach for printing out all values in a tree starting from the bottom."
-  (loop for child in (node-children node)
-     do (dfs-recursive-postorder child))
-  (when (not (eql (node-data node) nil))
+  (loop for child in (node-children node) do
+    (dfs-recursive-postorder child))
+  (when (node-data node)
     (format t "~a "  (node-data node))))
 
 (defun dfs-recursive-inorder-btree (node)
@@ -33,27 +33,27 @@
 
 (defun dfs-stack (node)
   "A depth first approach for printing out all values in a tree using a stack."
-  (let
-      ((stack (list node))
-       (temp nil))
-    (loop while (> (length stack) 0)
-       do (format t "~a " (node-data (first stack)))
-	 (setf temp (pop stack))
-	 (loop for child in (node-children temp)
-	    do (push child stack)))))
+  (loop
+    with stack = (list node)
+    with temp = nil
+    while (> (length stack) 0) do
+    (format t "~a " (node-data (first stack)))
+    (setf temp (pop stack))
+    (loop for child in (node-children temp) do
+      (push child stack))))
 
 (defun bfs-queue (node)
   "A breadth first search approach for printing out all values in a tree."
-  (let
-      ((queue (list node))
-       (temp nil))
-    (loop while (> (length queue) 0)
-       do (format t "~a " (node-data (first queue)))
-	     (setf temp (pop queue))
-	     ;; If the queue is empty, the queue should be filled with the children nodes.
-	     (if (eql queue nil)
-	         (setf queue (node-children temp))
-	         (nconc queue (node-children temp))))))
+  (loop
+    with queue = (list node)
+    with temp = nil
+    while (> (length queue) 0) do
+    (format t "~a " (node-data (first queue)))
+    (setf temp (pop queue))
+    ;; If the queue is empty, the queue should be filled with the children nodes.
+    (if (eql queue nil)
+        (setf queue (node-children temp))
+        (nconc queue (node-children temp)))))
 
 (defun make-tree (num-rows num-child)
   "Creates a simple tree, where every node has 'num-child' children and is 'num-rows' deep."
@@ -72,22 +72,22 @@
 ;; A binary tree for testing
 (defvar binary-tree (make-tree 3 2))
 
-;; Should print: 3, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1
+;; Should print: 3 2 1 1 1 2 1 1 1 2 1 1 1
 (dfs-recursive tree)
 (format t "~%")
 
-;; Should print: 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 3
+;; Should print: 1 1 1 2 1 1 1 2 1 1 1 2 3
 (dfs-recursive-postorder tree)
 (format t "~%")
 
-;; Should print: 1, 2, 1, 3, 1, 2, 1
+;; Should print: 1 2 1 3 1 2 1
 (dfs-recursive-inorder-btree binary-tree)
 (format t "~%")
 
-;; Should print: 3, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1
+;; Should print: 3 2 1 1 1 2 1 1 1 2 1 1 1
 (dfs-stack tree)
 (format t "~%")
 
-;; Should print: 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1
+;; Should print: 3 2 2 2 1 1 1 1 1 1 1 1 1
 (bfs-queue tree)
 (format t "~%")
