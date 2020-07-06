f4a577031d04fdb660b61fc66940c94ab6ef0d87
Trashtalk <Trashtalk217@gmail.com>
2020-07-06 20:27:33 +0200

    fixed the wrong test and added in a macro

--- contents/thomas_algorithm/code/clisp/thomas.lisp
+++ contents/thomas_algorithm/code/clisp/thomas.lisp
@@ -1,23 +1,25 @@
 ;;;; Thomas algorithm implementation in Common Lisp
 
+(defmacro divf (place divisor)
+  "Divides the value at place by divisor"
+  `(setf ,place (/ ,place ,divisor)))
+
+(defun helper (v1 v2 v3 row)
+  (- (svref v1 row) (* (svref v2 row) (svref v3 (1- row)))))
+
 (defun thomas (diagonal-a diagonal-b diagonal-c last-column)
   "Returns the solutions to a tri-diagonal matrix non-destructively"
+  ;; We have to copy the inputs to ensure non-destructiveness
   (let ((a (copy-seq diagonal-a))
          (b (copy-seq diagonal-b))
          (c (copy-seq diagonal-c))
          (d (copy-seq last-column)))
-    (setf (svref c 0) (/ (svref c 0) (svref b 0)))
-    (setf (svref d 0) (/ (svref d 0) (svref b 0)))
+    (divf (svref c 0) (svref b 0))
+    (divf (svref d 0) (svref b 0))
     (loop
       for i from 1 upto (1- (length a)) do
-      (setf
-        (svref c i)
-        (/ (svref c i) (- (svref b i) (* (svref a i) (svref c (1- i))))))
-      (setf
-        (svref d i)
-        (/
-          (- (svref d i) (* (svref a i) (svref d (1- i))))
-          (- (svref b i) (* (svref a i) (svref c (1- i)))))))
+      (divf (svref c i) (helper b a c i))
+      (setf (svref d i) (/ (helper d a d i) (helper b a c i))))
     (loop
       for i from (- (length a) 2) downto 0 do
       (decf (svref d i) (* (svref c i) (svref d (1+ i)))))
@@ -28,4 +30,5 @@
 (defparameter diagonal-c #(4 5 0))
 (defparameter last-column #(7 5 3))
 
-(format t "~{~f ~}" (coerce (thomas diagonal-a diagonal-b diagonal-c last-column) 'list))
+;; should print 0.8666667 1.5333333 -0.26666668
+(format t "~{~f ~}~%" (coerce (thomas diagonal-a diagonal-b diagonal-c last-column) 'list))
