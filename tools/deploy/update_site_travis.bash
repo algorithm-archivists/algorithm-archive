b5455b689ab988a183d4edb6d104913c9a3c0efa
Eric Berquist <eric.berquist@gmail.com>
2020-07-08 09:24:34 -0400

    Update Travis for HonKit build

--- tools/deploy/update_site_travis.bash
+++ tools/deploy/update_site_travis.bash
@@ -33,14 +33,15 @@ git config user.email "travis@travis-ci.org"
 
 GH_REPO_REF="github.com/${DOCS_REPO_OWNER}/${DOCS_REPO_NAME}.git"
 
-# Assume the book has already been built, and was moved to `build/`
+# Assume the book has already been built, and was moved to `_book/`
 # inside the same directory as this script.
+book_build_dir=_book
 
-if [ -d build ]; then
+if [ -d "${book_build_dir}" ]; then
     echo "${bold}Cloning the website repo...${normal}"
     git clone -b "${DOCS_BRANCH_NAME}" https://git@"${GH_REPO_REF}"
     rm -rf ./"${DOCS_REPO_NAME}"/*
-    cp -a ./build/* ./"${DOCS_REPO_NAME}"
+    cp -a ./"${book_build_dir}"/* ./"${DOCS_REPO_NAME}"
     pushd ./"${DOCS_REPO_NAME}"
     echo "www.algorithm-archive.org" > CNAME
     echo "${bold}Adding changes...${normal}"
