1955ee47798ba6a755b0e5f6b906a9d6e886525f
Jess 3Jane <me@jess.coffee>
2020-08-25 22:10:37 -0400

    Comment jarvis march implementation

--- contents/jarvis_march/code/rust/jarvis_march.rs
+++ contents/jarvis_march/code/rust/jarvis_march.rs
@@ -1,28 +1,43 @@
 type Point = (i32, i32);
 
-fn ccw(p1: &Point, p2: &Point, p3: &Point) -> bool {
-    (p3.1 - p1.1) * (p2.0 - p1.0)
-        >= (p2.1 - p1.1) * (p3.0 - p1.0)
+/// Checks if point B is counter-clockwise of point C relative to point A
+///
+/// This is a very simple cross product of the vectors ab and ac
+/// but we've replaced the normal subtraction with a comparison
+/// which tells us if the result would have been negative (i.e.
+/// the angle is counter clockwise)
+fn ccw(a: &Point, b: &Point, c: &Point) -> bool {
+    (c.1 - a.1) * (b.0 - a.0)
+        >= (b.1 - a.1) * (c.0 - a.0)
 }
 
 fn jarvis_march<'a>(points: &'a [Point]) -> Vec<&'a Point> {
     if points.len() == 0 { return Vec::new(); }
 
+    // The most negative point *must* be on the hull so
+    // we use it as a starting point
     let mut hull = vec![points.iter().min().unwrap()];
 
     loop {
         let last_point = hull.last().unwrap();
 
+        // Loop over possible points and find the one furthest to the left of the 
+        // last point added on the hull
         let mut endpoint = &points[0];
         for i in 1..points.len() {
+            // In this case when we say "left" we really mean the point which is 
+            // counter clockwise of all other points relative to the last point
+            // on the hull
             if endpoint == *last_point || !ccw(&points[i], last_point, endpoint) {
                 endpoint = &points[i];
             }
         }
 
+        // If we've looped back around to the first point in the hull the hull is now closed
         if hull[0] == endpoint {
             return hull;
         } else {
+            // Otherwise push this point onto the hull and keep going
             hull.push(endpoint);
         }
     }
