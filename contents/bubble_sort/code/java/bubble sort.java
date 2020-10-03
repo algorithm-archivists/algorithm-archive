6126a0c34f4b9acd950fdbcbb2df510d3e794225
Archit099 <65014238+Archit099@users.noreply.github.com>
2020-10-03 13:54:06 -0600

    Update bubble sort.java

--- contents/bubble_sort/code/java/bubble sort.java
+++ contents/bubble_sort/code/java/bubble sort.java
@@ -1,35 +1,34 @@
-public class BubbleSortExample {  
-    static void bubbleSort(int[] arr) {  
-        int n = arr.length;  
-        int temp = 0;  
-         for(int i=0; i < n; i++){  
-                 for(int j=1; j < (n-i); j++){  
-                          if(arr[j-1] > arr[j]){  
-                                 //swap elements  
-                                 temp = arr[j-1];  
-                                 arr[j-1] = arr[j];  
-                                 arr[j] = temp;  
-                         }  
-                          
-                 }  
-         }  
-  
-    }  
-    public static void main(String[] args) {  
-                int arr[] ={3,60,35,2,45,320,5};  
-                 
-                System.out.println("Array Before Bubble Sort");  
-                for(int i=0; i < arr.length; i++){  
-                        System.out.print(arr[i] + " ");  
-                }  
-                System.out.println();  
-                  
-                bubbleSort(arr);//sorting array elements using bubble sort  
-                 
-                System.out.println("Array After Bubble Sort");  
-                for(int i=0; i < arr.length; i++){  
-                        System.out.print(arr[i] + " ");  
-                }  
-   
-        }  
-}  
+import java.util.Scanner;
+
+class BubbleSortExample {
+  public static void main(String []args) {
+    int num, i, j, temp;
+    Scanner input = new Scanner(System.in);
+ 
+    System.out.println("Enter the number of integers to sort:");
+    num = input.nextInt();
+ 
+    int array[] = new int[num];
+ 
+    System.out.println("Enter " + num + " integers: ");
+ 
+    for (i = 0; i < num; i++) 
+      array[i] = input.nextInt();
+ 
+    for (i = 0; i < ( num - 1 ); i++) {
+      for (j = 0; j < num - i - 1; j++) {
+        if (array[j] > array[j+1]) 
+        {
+           temp = array[j];
+           array[j] = array[j+1];
+           array[j+1] = temp;
+        }
+      }
+    }
+ 
+    System.out.println("Sorted list of integers:");
+ 
+    for (i = 0; i < num; i++) 
+      System.out.println(array[i]);
+  }
+}
