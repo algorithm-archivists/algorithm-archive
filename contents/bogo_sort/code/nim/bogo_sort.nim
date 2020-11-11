0ca5ac6fd82f4cfc0f8d8340fb0d3c29c680b612
Eric Berquist <eric.berquist@gmail.com>
2020-11-11 18:51:13 -0500

    Make Bogosort in Nim more idiomatic (#783)

--- contents/bogo_sort/code/nim/bogo_sort.nim
+++ contents/bogo_sort/code/nim/bogo_sort.nim
@@ -3,24 +3,23 @@ import random
 randomize()
 
 proc print_array(a: openArray[int]) =
-    for n in 0 .. len(a)-1:
-        echo a[n]
+  for n in 0 .. len(a)-1:
+    echo a[n]
 
-proc is_sorted(a: openArray[int]): bool =
-    for n in 1 .. len(a)-1:
-        if a[n] > a[n-1]:
-            return false
-    
-    return true
+func is_sorted(a: openArray[int]): bool =
+  result = true
+  for n in 1 .. len(a)-1:
+    if a[n] > a[n-1]:
+      result = false
+      break
 
 proc bogo_sort(a: var openArray[int]) =
-    while not is_sorted(a):
-        shuffle(a)
-        
+  while not is_sorted(a):
+    shuffle(a)
 
-var x: array[10,int] =  [32,32,64,16,128,8,256,4,512,2]
-
-print_array(x)
-bogo_sort(x)
-echo "\n"
-print_array(x)
+when isMainModule:
+  var x = [32, 32, 64, 16, 128, 8, 256, 4, 512, 2]
+  print_array(x)
+  bogo_sort(x)
+  echo "\n"
+  print_array(x)
