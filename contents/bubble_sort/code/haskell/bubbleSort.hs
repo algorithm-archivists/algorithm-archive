17cf93e62f409350151a463212b955cf801a4ba0
James Schloss <jrs.schloss@gmail.com>
2021-02-04 17:51:33 -0500

    removing sorting and searching chapters, for now (#799)

--- contents/bubble_sort/code/haskell/bubbleSort.hs
+++
@@ -1,6 +0,0 @@
-bubbleSort :: (Ord a) => [a] -> [a]
-bubbleSort x = (!!length x) $ iterate bubble x
-  where bubble (x:y:r)
-          | x <= y    = x : bubble (y:r)
-          | otherwise = y : bubble (x:r)
-        bubble x = x
