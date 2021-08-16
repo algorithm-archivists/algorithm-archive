bc3583f97fd0133fab1976b4f489ad3fb5cb5020
Ridham Zalawadia <zalavadiaridham@gmail.com>
2021-08-16 14:52:30 -0700

    Updated list comprehension code to numpy code. Changed if statements to not use escape character

--- contents/convolutions/code/python/2d_convolution.py
+++ contents/convolutions/code/python/2d_convolution.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math
+from contextlib import suppress
 
 
 def convolve_linear(signal, filter, output_size):
@@ -10,8 +10,7 @@ def convolve_linear(signal, filter, output_size):
         for j in range(output_size[1]):
             for k in range(max(0, i-filter.shape[0]), i+1):
                 for l in range(max(0, j-filter.shape[1]), j+1):
-                    if k < signal.shape[0] and i-k < filter.shape[0] \
-                            and l < signal.shape[1] and j-l < filter.shape[1]:
+                    with suppress(IndexError):
                         sum += signal[k, l] * filter[i-k, j-l]
             out[i, j] = sum
             sum = 0
@@ -25,14 +24,12 @@ def create_gaussian_kernel(kernel_size):
     # The center must be offset by 0.5 to find the correct index
     center = kernel_size*0.5 + 0.5
 
-    sigma = math.sqrt(0.1*kernel_size)
+    sigma = np.sqrt(0.1*kernel_size)
 
-    for i in range(kernel_size):
-        for j in range(kernel_size):
-            kernel[i, j] = math.exp(-((i-center+1) ** 2
-                                      + (j-center+1) ** 2) / (2*(sigma**2)))
+    def kernel_function(x, y):
+        return np.exp(-((x-center+1)**2 + (y-center+1)**2)/(2*sigma**2))
 
-
+    kernel = np.fromfunction(kernel_function, (kernel_size, kernel_size))
     return kernel / np.linalg.norm(kernel)
 
 
@@ -42,14 +39,15 @@ def create_sobel_operators():
 
     return Sx, Sy
 
+def sum_matrix_dimensions(mat1, mat2):
+    return (mat1.shape[0] + mat2.shape[0], 
+            mat1.shape[1], mat2.shape[1])
 
 def compute_sobel(signal):
     Sx, Sy = create_sobel_operators()
 
-    Gx = convolve_linear(signal, Sx, tuple(map(sum,
-                                               zip(signal.shape, Sx.shape))))
-    Gy = convolve_linear(signal, Sy, tuple(map(sum,
-                                               zip(signal.shape, Sy.shape))))
+    Gx = convolve_linear(signal, Sx, sum_matrix_dimensions(signal, Sx)))
+    Gy = convolve_linear(signal, Sy, sum_matrix_dimensions(signal, Sy))
 
     return np.sqrt(np.power(Gx, 2) + np.power(Gy, 2))
 
@@ -58,11 +56,11 @@ def create_circle(image_resolution, grid_extents, radius):
     out = np.zeros((image_resolution, image_resolution))
 
     for i in range(image_resolution):
-        x_position = (i * grid_extents / image_resolution) \
-                      - 0.5 * grid_extents
+        x_position = ((i * grid_extents / image_resolution)
+                      - 0.5 * grid_extents)
         for j in range(image_resolution):
-            y_position = (j * grid_extents / image_resolution) \
-                          - 0.5 * grid_extents
+            y_position = ((j * grid_extents / image_resolution)
+                          - 0.5 * grid_extents)
             if x_position ** 2 + y_position ** 2 <= radius ** 2:
                 out[i, j] = 1.0
 
@@ -75,21 +73,20 @@ def main():
     x = np.random.rand(100, 100)
 
     # Gaussian signals
-    y = [[math.exp(-(((i-50)/100) ** 2 +
-                     ((j-50)/100) ** 2) / .01) for j in range(100)]
-         for i in range(100)]
+    def create_gaussian_signals(i, j):
+        return np.exp(-(((i-50)/100) ** 2 +
+                        ((j-50)/100) ** 2) / .01)
+    y = np.fromfunction(create_gaussian_signals, (100, 100))
 
     # Normalization is not strictly necessary, but good practice
-    x = x / np.linalg.norm(x)
-    y = y / np.linalg.norm(y)
+    x /= np.linalg.norm(x)
+    y /= np.linalg.norm(y)
 
     x = np.array([[5.0, 3.0], [4.0, 2.0]])
     y = np.array([[0.2, 0.4], [0.6, 0.3]])
 
     # full convolution, output will be the size of x + y
-    full_linear_output = convolve_linear(x, y,
-                                         tuple(map(sum,
-                                                   zip(x.shape, y.shape))))
+    full_linear_output = convolve_linear(x, y,)
 
     # simple boundaries
     simple_linear_output = convolve_linear(x, y, x.shape)
@@ -106,14 +103,13 @@ def main():
     large_kernel = create_gaussian_kernel(25)
 
     small_kernel_output = convolve_linear(circle, small_kernel,
-                                          tuple(map(sum,
-                                                    zip(circle.shape,
-                                                        small_kernel.shape))))
+                                          sum_matrix_dimensions(circle,
+                                                                small_kernel))
 
     large_kernel_output = convolve_linear(circle, large_kernel,
-                                          tuple(map(sum,
-                                                    zip(circle.shape,
-                                                        large_kernel.shape))))
+                                          sum_matrix_dimensions(circle,
+                                                                large_kernel))
+                                            
 
     np.savetxt("small_kernel.dat", small_kernel_output)
     np.savetxt("large_kernel.dat", large_kernel_output)
