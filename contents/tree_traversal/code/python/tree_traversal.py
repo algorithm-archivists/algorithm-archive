57d11735a17cd6d7dc4153d95c843c9ccb9e2fb6
Ishaan Verma <ishaan56verma@gmail.com>
2021-09-03 22:45:07 +0530

    standardize python output

--- contents/tree_traversal/code/python/tree_traversal.py
+++ contents/tree_traversal/code/python/tree_traversal.py
@@ -3,7 +3,6 @@ class Node:
         self.data = None
         self.children = []
 
-
 def create_tree(node, num_row, num_child):
     node.data = num_row
 
@@ -17,7 +16,7 @@ def create_tree(node, num_row, num_child):
 
 def dfs_recursive(node):
     if node.data != None:
-        print(node.data)
+        print(node.data, end=' ')
 
     for child in node.children:
         dfs_recursive(child)
@@ -28,20 +27,20 @@ def dfs_recursive_postorder(node):
         dfs_recursive_postorder(child)
 
     if node.data != None:
-        print(node.data)
+        print(node.data, end=' ')
 
 
 # This assumes only 2 children, but accounts for other possibilities
 def dfs_recursive_inorder_btree(node):
-    if (len(node.children) == 2):
+    if len(node.children) == 2:
         dfs_recursive_inorder_btree(node.children[0])
-        print(node.data)
+        print(node.data, end=' ')
         dfs_recursive_inorder_btree(node.children[1])
-    elif (len(node.children) == 1):
+    elif len(node.children) == 1:
         dfs_recursive_inorder_btree(node.children[0])
-        print(node.data)
-    elif (len(node.children) == 0):
-        print(node.data)
+        print(node.data, end=' ')
+    elif len(node.children) == 0:
+        print(node.data, end=' ')
     else:
         print("Not a binary tree!")
 
@@ -53,7 +52,7 @@ def dfs_stack(node):
     temp = None
 
     while len(stack) > 0:
-        print(stack[-1].data)
+        print(stack[-1].data, end=' ')
         temp = stack.pop()
 
         for child in temp.children:
@@ -67,7 +66,7 @@ def bfs_queue(node):
     temp = None
 
     while len(queue) > 0:
-        print(queue[0].data)
+        print(queue[0].data, end=' ')
         temp = queue.pop(0)
 
         for child in temp.children:
@@ -75,24 +74,29 @@ def bfs_queue(node):
 
 
 def main():
-    tree = create_tree(Node(), 3, 3)
+    tree = create_tree(Node(), 2, 3)
 
-    print("Recursive:")
+    print("[#] Recursive DFS:")
     dfs_recursive(tree)
+    print()
 
-    print("Recursive Postorder:")
+    print("[#] Recursive Postorder DFS:")
     dfs_recursive_postorder(tree)
+    print()
 
-    print("Stack:")
+    print("[#] Stack-based DFS :")
     dfs_stack(tree)
+    print()
 
-    print("Queue:")
+    print("[#] Queue-based BFS:")
     bfs_queue(tree)
+    print()
 
     binary_tree = create_tree(Node(), 3, 2)
     
-    print("Recursive Inorder Binary Tree:")
+    print("[#] Recursive Inorder Binary Tree:")
     dfs_recursive_inorder_btree(binary_tree)
+    print()
 
 if __name__ == '__main__':
     main()
