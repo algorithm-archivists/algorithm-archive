f395357d671cbaa56d03660abac885355f539b4f
Ishaan Verma <ishaan56verma@gmail.com>
2021-09-08 23:30:48 +0530

    standardize csharp output

--- contents/tree_traversal/code/csharp/Program.cs
+++ contents/tree_traversal/code/csharp/Program.cs
@@ -7,23 +7,27 @@ namespace TreeTraversal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TreeTraversal");
-            var tree = new Tree(3, 3);
-            Console.WriteLine("DFSRecursive:");
+            var tree = new Tree(2, 3);
+            Console.WriteLine("[#] Recursive DFS:");
             tree.DFSRecursive();
-            Console.WriteLine("DFSStack:");
-            tree.DFSStack();
-            Console.WriteLine("BFSQueue:");
-            tree.BFSQueue();
-            Console.WriteLine("DFSRecursivePostorder");
-            tree.DFSRecursivePostorder();
+            Console.WriteLine();
 
-            // Uncommenting the following 2 lines will result in an exception thrown because at least one Node of the Tree has more than 2 children and therefor a DFSRecursiveInorderBinary doesn't work.
-            // Console.WriteLine("DFSRecursiveInorder (fail)");
-            // tree.DFSRecursiveInorderBinary();
+            Console.WriteLine("[#] Recursive Postorder DFS:");
+            tree.DFSRecursivePostorder();
+            Console.WriteLine();
+
+            Console.WriteLine("[#] Stack-based DFS:");
+            tree.DFSStack();
+            Console.WriteLine();
+
+            Console.WriteLine("[#] Queue-based BFS:");
+            tree.BFSQueue();
+            Console.WriteLine();
+
             tree = new Tree(3, 2);
-            Console.WriteLine("DFSRecursiveInorder (succeed)");
+            Console.WriteLine("[#] Recursive Inorder DFS for Binary Tree:");
             tree.DFSRecursiveInorderBinary();
+            Console.WriteLine();
         }
     }
 }
