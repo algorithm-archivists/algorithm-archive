cba2d7026138ec320c18e6a4128bd684daa140d4
Ayman Lafaz <ayman.lafaz@um5r.ac.ma>
2021-10-12 01:17:42 +0100

    spacing code properly for readability

--- contents/convolutions/1d/code/python/1d_convolution.py
+++ contents/convolutions/1d/code/python/1d_convolution.py
@@ -6,24 +6,28 @@ def convolve_cyclic(signal, filter_array):
     output_size = max(len(signal), len(filter_array))
     out = np.zeros(output_size)
     s = 0
+
     for i in range(output_size):
         for j in range(output_size):
             if(mod1(i - j, output_size) < len(filter_array)):
                 s += signal[mod1(j - 1, output_size)] * filter_array[mod1(i - j, output_size)]
         out[i] = s
         s = 0
+
     return out
 
 
 def convolve_linear(signal, filter_array, output_size):
     out = np.zeros(output_size)
     s = 0
+
     for i in range(output_size):
         for j in range(max(0, i - len(filter_array)), i + 1):
             if j < len(signal) and (i - j) < len(filter_array):
                 s += signal[j] * filter_array[i - j]
         out[i] = s
         s = 0
+
     return out
 
 # sawtooth functions for x and y
