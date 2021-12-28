e244b589bc460348b504b3782382361c52fe5c1a
James Schloss <jrs.schloss@gmail.com>
2021-12-28 17:50:33 +0100

    small fixes to nd code

--- contents/box_muller/code/julia/box_muller_nd.jl
+++ contents/box_muller/code/julia/box_muller_nd.jl
@@ -42,56 +42,63 @@ end
 # This function reads in a pair of input points and performs the Cartesian
 # Box--Muller transform
 function cartesian_box_muller(input_pts, sigma, mu)
-    r = sqrt(-2 * log(input_pts[1]))
-    theta = 2 * pi * input_pts[2]
 
-    return [sigma * r * cos(theta) + mu,
-            sigma * r * sin(theta) + mu]
+    output = zeros(length(input_pts)) 
+    components = zeros(length(input_pts)) 
+
+    components[end] = sqrt(-2 * log(input_pts[end]))
+    output[:] .= components[end]
+
+    if length(output) > 1
+        components[end-1] = 2*pi*input_pts[end-1]
+
+        for i = 1:length(output)-2
+            components[i] = pi*input_pts[i]
+        end
+
+        for i = 2:length(output)
+            output[i-1] *= cos(components[i-1])
+            output[i:end] .*= sin(components[i-1])
+        end 
+
+    else
+        #output[1] = -1*sign(input_pts[1])*output[1]
+    end 
+
+    return sigma .* output .+ mu
 
 end
 
-# This function reads in a pair of input points and performs the Cartesian
-# Box--Muller transform
-function polar_box_muller(input_pts, sigma, mu)
-    r_0 = input_pts[1]^2 + input_pts[2]^2
+function main(n; dims = 2, sigma = 1, mu = 0)
 
-    # this method is only valid for points within the unit circle
-    if r_0 == 0 || r_0 > 1
-        return [NaN, NaN]
+    if n^(1/dims) != ceil(Int, n^(1/dims))
+        n = ceil(Int, n^(1/dims))^dims
     end
 
-    return [sigma * input_pts[1] * sqrt(-2 * log(r_0) / r_0) + mu,
-            sigma * input_pts[2] * sqrt(-2 * log(r_0) / r_0) + mu]
-
-end
-
-function main(n)
-    cartesian_grid = create_grid(n, [0,1])
-    polar_grid = create_grid(n, [-1,1])
-    cartesian_rand = create_rand_dist(n, [0,1])
-    polar_rand = create_rand_dist(n, [-1,1])
+    cartesian_grid = zeros(n,dims)
+    cartesian_rand = zeros(n,dims)
+    if dims == 1
+        cartesian_grid = create_grid(n, [0,1]; dims = dims)
+        cartesian_rand = create_rand_dist(n, [0,1]; dims = dims)
+    else
+        cartesian_grid = create_grid(n, [0,1]; dims = dims)
+        cartesian_rand = create_rand_dist(n, [0,1]; dims = dims)
+    end
 
     cartesian_grid_output = similar(cartesian_grid)
-    polar_grid_output = similar(polar_grid)
     cartesian_rand_output = similar(cartesian_rand)
-    polar_rand_output = similar(polar_rand)
 
+    println(size(cartesian_grid)[1])
     for i = 1:size(cartesian_grid)[1]
         cartesian_grid_output[i,:] .= cartesian_box_muller(cartesian_grid[i,:],
-                                                           1, 0)
-        polar_grid_output[i,:] .= polar_box_muller(polar_grid[i,:], 1,0)
+                                                           sigma, mu)
         cartesian_rand_output[i,:] .= cartesian_box_muller(cartesian_rand[i,:],
-                                                           1, 0)
-        polar_rand_output[i,:] .= polar_box_muller(polar_rand[i,:], 1, 0)
+                                                           sigma, mu)
     end
 
     writedlm("cartesian_grid_output.dat", cartesian_grid_output)
-    writedlm("polar_grid_output.dat", polar_grid_output)
     writedlm("cartesian_rand_output.dat", cartesian_rand_output)
-    writedlm("polar_rand_output.dat", polar_rand_output)
 
     writedlm("cartesian_grid.dat", cartesian_grid)
-    writedlm("polar_grid.dat", polar_grid)
     writedlm("cartesian_rand.dat", cartesian_rand)
-    writedlm("polar_rand.dat", polar_rand)
 end
