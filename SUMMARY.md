b59d6217d77e2167d679005ca3bd0f10d4adf40b
James Schloss <jrs.schloss@gmail.com>
2022-04-07 16:01:23 +0200

    changing summary and adding attempt at checking Gaussian distribution

--- SUMMARY.md
+++ SUMMARY.md
@@ -20,7 +20,6 @@
         * [Multiplication as a Convolution](contents/convolutions/multiplication/multiplication.md)
         * [Convolutions of Images (2D)](contents/convolutions/2d/2d.md)
         * [Convolutional Theorem](contents/convolutions/convolutional_theorem/convolutional_theorem.md)
-    * [Probability Distributions](contents/probability/distributions/distributions.md)
     * [Box Muller Transform](contents/box_muller/box_muller.md)
         * [How costly is rejection sampling?](contents/box_muller/box_muller_rejection.md)
     * [Probability Distributions](contents/probability_distributions/distributions.md)
--- contents/box_muller/code/julia/box_muller.jl
+++ contents/box_muller/code/julia/box_muller.jl
@@ -1,4 +1,5 @@
-using DelimitedFiles
+using DelimitedFiles, LinearAlgebra
+using Test
 
 function create_grid(n, endpoints)
 
@@ -42,8 +43,8 @@ function cartesian_box_muller(input_pts, sigma, mu)
     r = sqrt(-2 * log(input_pts[1]))
     theta = 2 * pi * input_pts[2]
 
-    return [sigma * r * cos(theta) + mu,
-            sigma * r * sin(theta) + mu]
+    return [sigma * r * cos(theta) + mu[1],
+            sigma * r * sin(theta) + mu[2]]
 
 end
 
@@ -57,12 +58,53 @@ function polar_box_muller(input_pts, sigma, mu)
         return [NaN, NaN]
     end
 
-    return [sigma * input_pts[1] * sqrt(-2 * log(r_0) / r_0) + mu,
-            sigma * input_pts[2] * sqrt(-2 * log(r_0) / r_0) + mu]
+    return [sigma * input_pts[1] * sqrt(-2 * log(r_0) / r_0) + mu[1],
+            sigma * input_pts[2] * sqrt(-2 * log(r_0) / r_0) + mu[2]]
 
 end
 
+function is_gaussian(input_pts; bounds = [-1 1; -1 1], dx = 0.1,
+                     sigma = 1, mu = [0,0], threshold = 0.1)
+    histogram = zeros(ceil(Int,(bounds[1,2]-bounds[1,1])/dx),
+                      ceil(Int,(bounds[2,2]-bounds[2,1])/dx))
+
+    for i = 1:size(input_pts)[1]
+        input_x = input_pts[i, 1]
+        input_y = input_pts[i, 2]
+        if !(isnan(input_x) || isnan(input_y))
+
+            bin = CartesianIndex(ceil(Int, (input_x - bounds[1,1]) / dx),
+                                 ceil(Int, (input_y - bounds[2,1]) / dx))
+
+            if bin[1] <= size(histogram)[1] && bin[1] > 0 &&
+               bin[2] <= size(histogram)[2] && bin[2] > 0
+                histogram[bin] += 1
+            end
+        end
+    end
+
+    n = sum(histogram)
+    normalize!(histogram)
+
+    rms = 0
+    for i = 1:size(histogram)[1]
+        x = bounds[1,1] + i*dx
+        for j = 1:size(histogram)[2]
+            y = bounds[2,1] + j*dx
+            gaussian_value = exp(-(((x+mu[1])^2)/(2*sigma^2) +
+                                   ((y+mu[2])^2)/(2*sigma^2)))
+            rms += (gaussian_value - histogram[i,j])^2
+        end
+    end
+
+    return sqrt(rms/n) < threshold
+end
+
 function main(n)
+
+    # This casts the input onto the nearest square for the cartesian grids
+    n = Int(ceil(sqrt(n))^2)
+
     cartesian_grid = create_grid(n, [0,1])
     polar_grid = create_grid(n, [-1,1])
     cartesian_rand = create_rand_dist(n, [0,1])
@@ -73,13 +115,33 @@ function main(n)
     cartesian_rand_output = similar(cartesian_rand)
     polar_rand_output = similar(polar_rand)
 
+    # going through each pair of points and using the x,y coordinates in
+    # their respective functions
     for i = 1:size(cartesian_grid)[1]
-        cartesian_grid_output[i,:] .= cartesian_box_muller(cartesian_grid[i,:],
-                                                           1, 0)
-        polar_grid_output[i,:] .= polar_box_muller(polar_grid[i,:], 1,0)
-        cartesian_rand_output[i,:] .= cartesian_box_muller(cartesian_rand[i,:],
-                                                           1, 0)
-        polar_rand_output[i,:] .= polar_box_muller(polar_rand[i,:], 1, 0)
+        cartesian_grid_output[i,:] .= 
+            cartesian_box_muller(cartesian_grid[i,:], 1, [0,0])
+
+        polar_grid_output[i,:] .= polar_box_muller(polar_grid[i,:], 1, [0,0])
+
+        cartesian_rand_output[i,:] .=
+            cartesian_box_muller(cartesian_rand[i,:], 1, [0,0])
+
+        polar_rand_output[i,:] .= polar_box_muller(polar_rand[i,:], 1, [0,0])
+    end
+
+    @testset "histogram tests of Box--Muller Gaussianness" begin
+        @test is_gaussian(cartesian_grid_output;
+                          bounds = [-3 3; -3 3], dx = 0.3,
+                          sigma = 1, mu = [0,0])
+        @test is_gaussian(cartesian_rand_output;
+                          bounds = [-3 3; -3 3], dx = 0.3,
+                          sigma = 1, mu = [0,0])
+        @test is_gaussian(polar_grid_output;
+                          bounds = [-3 3; -3 3], dx = 0.3,
+                          sigma = 1, mu = [0,0])
+        @test is_gaussian(polar_rand_output;
+                          bounds = [-3 3; -3 3], dx = 0.3,
+                          sigma = 1, mu = [0,0])
     end
 
     writedlm("cartesian_grid_output.dat", cartesian_grid_output)
